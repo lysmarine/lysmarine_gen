60cb576ca017b68e021ac8e42fc09f184ed0ed50
Frederic Guilbault <fred@0464.ca>
2019-02-08 11:49:04 -0500

    Remove variables in the xygrib build script

--- stage5/03-xygrib/02-run.sh
+++ stage5/03-xygrib/02-run.sh
@@ -2,6 +2,9 @@
 install -d "${ROOTFS_DIR}/usr/share/applications/"
 install files/XyGrib.desktop "${ROOTFS_DIR}/usr/share/applications/"
 
+install -d "${ROOTFS_DIR}/usr/local/share/openGribs/XyGrib/data/img/"
+install files/logo_grib.jpg "${ROOTFS_DIR}/usr/local/share/openGribs/XyGrib/data/img/"
+
 on_chroot << EOF
 # For Ubuntu/Debian build and install new version of XyGrib from archive file on https://opengribbs.org
 # DomH, 28/08/2018 (modified 21/08/2018, 03/10/2018)
@@ -16,45 +19,40 @@ DataDir="/usr/local/share/openGribs/XyGrib"
 DownloadDir=$(pwd)
 
 
-# Test if this is a new version
-NewRelease=$(wget -nv -qO- https://api.github.com/repos/opengribs/XyGrib/releases/latest | grep -m 1 'tarball_url' | cut -dv -f2 | cut -d'"' -f1)
 
-# Download archive
-wget https://github.com/opengribs/XyGrib/archive/v$NewRelease.tar.gz
-
-# Extract files from archive
-tar xf v$NewRelease.tar.gz
-ExtractNameDir=$(tar -tf  v$NewRelease.tar.gz | head -n 1 | cut -d/ -f1)
-
-
+wget https://github.com/opengribs/XyGrib/archive/v1.2.4.tar.gz
+tar xf v1.2.4.tar.gz
+mv XyGrib-1.2.4 XyGribSrc
 # create build directory
-mkdir -p $ExtractNameDir/build
+mkdir -p XyGribSrc/build
 
 # Build and install XyGrib new version
-cd $ExtractNameDir/build
-cmake -DCMAKE_INSTALL_PREFIX=$InstallDir ..
+cd XyGribSrc/build
+cmake -DCMAKE_INSTALL_PREFIX= ..
+echo "==cmakedone============="
+
 make 1>Xygrib.std 2>Xygrib.err
-string=$(grep $BuildDir"/CMakeFiles 0" ./Xygrib.std)
-if [[ $string = *"/usr/bin/cmake -E cmake_progress_start"* ]]; then
-        make install
-        rm $AppDir"/XyGrib"
-        mv $DefaultInstallDir"/XyGrib/XyGrib" $AppDir"/"
-        mkdir -p $DataDir
-        rm -rf $DataDir"/data"
-        mv $DefaultInstallDir"/XyGrib/data" $DataDir"/"
-        echo "Successfull new build installed"
-        cd $DownloadDir
-        rm -rf $ExtractNameDir
-        if [ ! -f /home/pi/.local/share/applications/openGrib/XyGrib/data ]; then
-                mkdir -p /home/pi/.local/share/applications/openGrib/XyGrib/data
-        fi
-        mv /usr/local/share/openGrib/XyGrib/data/colors /home/pi/.local/share/applications/openGrib/XyGrib/data
-        ln -s /home/pi/.local/share/applications/openGrib/XyGrib/data/colors /usr/local/share/openGribs/XyGrib/data/colors
-        rm -rf /XyGrib
-else
-        less $BuildDir"/Xygrib.err"
-        printf "\nFolder "$ExtractNameDir" and subfolders not removed\n"
-fi
+echo "==============="
+cat Xygrib.err
+echo "==============="
+
+echo "make Done, now running make install "
+
+make install
+rm -rf /usr/local/bin/XyGrib
+mv /XyGrib/XyGrib /usr/local/bin/
+mkdir -p /usr/local/share/openGribs/XyGrib
+rm -rf /usr/local/share/openGribs/XyGrib/data
+mv /XyGrib/data /usr/local/bin/
+echo "Successfull new build installed"
+
+      mkdir -p /home/pi/.local/share/applications/openGrib/XyGrib/data
+
+#mv /usr/local/share/openGrib/XyGrib/data/colors /home/pi/.local/share/applications/openGrib/XyGrib/data
+#ln -s /home/pi/.local/share/applications/openGrib/XyGrib/data/colors /usr/local/share/openGribs/XyGrib/data/colors
+rm -rf /XyGrib
+rm -rf /XyGribSrc
+rm -rf /XyGribv1.2.4.tar.gz
 
 echo "Done building XyGrib"
 EOF
