821fa8331c1eaabfcbc7ff8d2cef3c4e31c7c8a6
Frederic Guilbault <fred@0464.ca>
2019-02-08 11:41:12 -0500

    Fix signalk install script

--- stage3/01-signalk/01-run.sh
+++ stage3/01-signalk/01-run.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -e
-
+install -m 755 -o 1000 -g 1000  -d "${ROOTFS_DIR}/home/pi/.signalk"
+install -m 644 -o 1000 -g 1000   files/defaults.json  "${ROOTFS_DIR}/home/pi/.signalk/defaults.json"
+install -m 644 -o 1000 -g 1000   files/package.json   "${ROOTFS_DIR}/home/pi/.signalk/package.json"
+install -m 644 -o 1000 -g 1000   files/settings.json  "${ROOTFS_DIR}/home/pi/.signalk/settings.json"
+install -m 755 -o 1000 -g 1000   files/signalk-server "${ROOTFS_DIR}/home/pi/.signalk/signalk-server"
 on_chroot << EOF
 npm install --loglevel error -g --unsafe-perm signalk-server
 EOF
 
-install -d "${ROOTFS_DIR}/home/pi/.signalk"
-install -m 644 -o 1000 -g 1000  -v files/defaults.json  "${ROOTFS_DIR}/home/pi/.signalk/defaults.json"
-install -m 644 -o 1000 -g 1000  -v files/package.json   "${ROOTFS_DIR}/home/pi/.signalk/package.json"
-install -m 644 -o 1000 -g 1000  -v files/settings.json  "${ROOTFS_DIR}/home/pi/.signalk/settings.json"
-install -m 755 -o 1000 -g 1000  -v files/signalk-server "${ROOTFS_DIR}/home/pi/.signalk/signalk-server"
 
 install -m 644 -v files/signalk.service "${ROOTFS_DIR}/etc/systemd/system/signalk.service"
 install -m 644 -v files/signalk.socket  "${ROOTFS_DIR}/etc/systemd/system/signalk.socket"
 
-ln -sf "${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/signalk.service" "${ROOTFS_DIR}/etc/systemd/system/signalk.service"
-ln -sf "${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/signalk.socket" "${ROOTFS_DIR}/etc/systemd/system/signalk.socket"
+ln -sf "${ROOTFS_DIR}/etc/systemd/system/signalk.service" "${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/signalk.service"
+ln -sf "${ROOTFS_DIR}/etc/systemd/system/signalk.socket"  "${ROOTFS_DIR}/etc/systemd/system/multi-user.target.wants/signalk.socket"
