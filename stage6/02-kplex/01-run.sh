124ea8f8b08373a99adcdbd5ceb3218da6046d87
Frederic Guilbault <fred@0464.ca>
2019-02-10 15:47:29 -0500

    Clean kplex after install

--- stage6/02-kplex/01-run.sh
+++ stage6/02-kplex/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-
+install -m 755 -o 1000 -g 1000   files/kplex.conf "${ROOTFS_DIR}/home/pi/.kplex.conf"
 on_chroot << EOF
 wget http://www.stripydog.com/download/kplex_1.4-1_armhf.deb
 dpkg -i kplex_1.4-1_armhf.deb
-touch /home/pi/.kplex.conf
+rm kplex_1.4-1_armhf.deb
 EOF
