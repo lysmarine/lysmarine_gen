c38b2f0f04be2445db3d99f79f25ef49fd7eb668
Frederic Guilbault <fred@0464.ca>
2019-02-13 17:06:12 -0500

    add openploter POC (it's known to still be broken atm)

--- stage6/03-signalk2/00-run.sh
+++ stage6/03-signalk2/00-run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
 on_chroot << EOF
+  cd /home/pi/.config/
+  git clone https://github.com/SignalK/signalk-server-node.git
+  cd signalk-server-node
+  npm install --unsafe-perm
 
-cd /home/pi/.config/
-git clone https://github.com/SignalK/signalk-server-node.git
-cd signalk-server-node
-npm install
+  cd ..
+  chown -R pi:pi signalk-server-node
 
 EOF
+
+install -o 1000 -g 1000 files/security.json "${ROOTFS_DIR}/home/pi/.signalk/security.json"
