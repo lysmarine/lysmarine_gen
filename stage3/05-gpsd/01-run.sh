a49dc8b912113feb2b728176b8358fcb3190fc72
Frederic Guilbault <fred@0464.ca>
2019-02-17 18:46:28 -0500

    Automatic connection of USB GPSs

--- stage3/05-gpsd/01-run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-on_chroot <<EOF
-echo 'START_DAEMON="true"' > /etc/default/gpsd
-echo 'USBAUTO="true"' >> /etc/default/gpsd
-echo 'DEVICES="/dev/ttyUSB0"' >> /etc/default/gpsd
-echo 'GPSD_OPTIONS=" -n "' >> /etc/default/gpsd
-EOF
