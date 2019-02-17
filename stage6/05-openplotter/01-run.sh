df3ce1211fb36e6b9ac8b1f41b6b3baf01a081f7
Frederic Guilbault <fred@0464.ca>
2019-02-17 18:51:49 -0500

    Move openplotter to /opt

--- stage6/05-openplotter/01-run.sh
+++ stage6/05-openplotter/01-run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
-install -d -o 1000 -g 1000 "${ROOTFS_DIR}/home/pi/.config/openplotter"
+install -d  "${ROOTFS_DIR}/opt/openplotter/"
 on_chroot << EOF
 git clone https://github.com/sailoog/openplotter.git
 cd openplotter
 git checkout 4845ae7faf8b38324f104eced9068eaadfb851fa ; # The v1.2.0 commit. ;
 
-cp -r ./*  /home/pi/.config/openplotter/
-chown -R pi:pi /home/pi/.config/openplotter
+cp -r ./*  /opt/openplotter/
+
 cd ..
 rm -rf openplotter
 
 EOF
-install -d -o 1000 -g 1000 "${ROOTFS_DIR}/home/pi/.local/share/applications/openplotter/"
+install -d -o 1000 -g 1000 "${ROOTFS_DIR}/home/pi/.local/share/applications/"
 install -o 1000 -g 1000 files/openplotter.desktop "${ROOTFS_DIR}/home/pi/.local/share/applications/"
