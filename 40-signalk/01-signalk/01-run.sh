a04568162a4ff96f202e08d64445d8ffbcb03078
Frederic Guilbault <fred@0464.ca>
2019-03-08 22:18:05 -0500

    move signalk server to it's own user

--- 40-signalk/01-signalk/01-run.sh
+++ 40-signalk/01-signalk/01-run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash -e
-install -m 755 -o 1000 -g 1000  -d "${ROOTFS_DIR}/home/pi/.signalk"
-install -m 644 -o 1000 -g 1000   files/defaults.json  "${ROOTFS_DIR}/home/pi/.signalk/defaults.json"
-install -m 644 -o 1000 -g 1000   files/package.json   "${ROOTFS_DIR}/home/pi/.signalk/package.json"
-install -m 644 -o 1000 -g 1000   files/settings.json  "${ROOTFS_DIR}/home/pi/.signalk/settings.json"
-install -m 755 -o 1000 -g 1000   files/signalk-server "${ROOTFS_DIR}/home/pi/.signalk/signalk-server"
+on_chroot << EOF
+adduser --home /home/signalk --uid 1001 --disabled-password --disabled-login signalk
+EOF
+
+install -m 755 -o 1001 -g 1001  -d "${ROOTFS_DIR}/home/signalk/.signalk"
+install -m 644 -o 1001 -g 1001   files/defaults.json  "${ROOTFS_DIR}/home/signalk/.signalk/defaults.json"
+install -m 644 -o 1001 -g 1001   files/package.json   "${ROOTFS_DIR}/home/signalk/.signalk/package.json"
+install -m 644 -o 1001 -g 1001   files/settings.json  "${ROOTFS_DIR}/home/signalk/.signalk/settings.json"
+install -m 755 -o 1001 -g 1001   files/signalk-server "${ROOTFS_DIR}/home/signalk/.signalk/signalk-server"
 on_chroot << EOF
 npm install --loglevel error -g --unsafe-perm signalk-server
 EOF
-
-
 install -m 644 -v files/signalk.service "${ROOTFS_DIR}/etc/systemd/system/signalk.service"
 install -m 644 -v files/signalk.socket  "${ROOTFS_DIR}/etc/systemd/system/signalk.socket"
 
