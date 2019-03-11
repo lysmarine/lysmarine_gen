ce620c2b8da6aa00952b66e56e02604f8b4712a7
Frederic Guilbault <fred@0464.ca>
2019-03-11 16:31:38 -0400

    Add splash screen

--- 20-boot/01-splashScreen/01-run.sh
+++ 20-boot/01-splashScreen/01-run.sh
@@ -2,8 +2,12 @@
 
 cat  files/appendToConfig.txt >> "${ROOTFS_DIR}/boot/config.txt"
 
-cat  files/motd > "${ROOTFS_DIR}/etc/motd"
+install -v -m0755 files/showLysmarineLogo.sh "${ROOTFS_DIR}/bin/splashScreen"
+install -v files/ascii_logo.txt "${ROOTFS_DIR}/etc/motd"
+install -v files/splash.png "${ROOTFS_DIR}/usr/share/splash.png"
+install -v files/splashscreen.service "${ROOTFS_DIR}/etc/systemd/system/splashscreen.service"
 
 on_chroot << EOF
    apt-get remove -y plymouth
+   systemctl enable splashscreen.service
 EOF
---
+++ 20-boot/01-splashScreen/files/showLysmarineLogo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+chvt 8
+clear
+fbi -once -timeout 20 -noverbose -nocomments -noedit /usr/share/splash.png
+clear
