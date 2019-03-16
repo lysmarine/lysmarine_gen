19e75684e811c5252f69e1a33dc7c45c0fd8bada
Frederic Guilbault <fred@0464.ca>
2019-03-16 14:05:54 -0400

    Move stages around

--- 48-openplotter/05-openplotter/01-run.sh
+++ 48-openplotter/05-openplotter/01-run.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -e
 install -d  "${ROOTFS_DIR}/opt/openplotter/"
+
 on_chroot << EOF
+  pip install setuptools
+  pip install requests ujson websocket websocket-client paho-mqtt geomag
+EOF
+
+on_chroot << EOF
+cd /opt/
 git clone https://github.com/sailoog/openplotter.git
 cd openplotter
-git checkout 4845ae7faf8b38324f104eced9068eaadfb851fa ; # The v1.2.0 commit. ;
-
-cp -r ./*  /opt/openplotter/
-
-cd ..
-rm -rf openplotter
+git checkout v2.x.x ; # We should use a fix commit hash to have reproductive builds
 
 EOF
+
 install -d -o 1000 -g 1000 "${ROOTFS_DIR}/home/pi/.local/share/applications/"
 install -o 1000 -g 1000 files/openplotter.desktop "${ROOTFS_DIR}/home/pi/.local/share/applications/"
