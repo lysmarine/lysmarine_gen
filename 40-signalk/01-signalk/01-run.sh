1f1f11f940cc58d59e7d20038a553777e9deb4d9
Frederic Guilbault <fred@0464.ca>
2019-03-16 14:06:14 -0400

    Add missing deps for signalk

--- 40-signalk/01-signalk/01-run.sh
+++ 40-signalk/01-signalk/01-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 on_chroot << EOF
-adduser --home /home/signalk --uid 1001 --disabled-password --disabled-login signalk
+  adduser --home /home/signalk --uid 1001 --disabled-password --disabled-login signalk
 EOF
 
 install -m 755 -o 1001 -g 1001  -d "${ROOTFS_DIR}/home/signalk/.signalk"
@@ -8,9 +8,11 @@ install -m 644 -o 1001 -g 1001   files/defaults.json  "${ROOTFS_DIR}/home/signal
 install -m 644 -o 1001 -g 1001   files/package.json   "${ROOTFS_DIR}/home/signalk/.signalk/package.json"
 install -m 644 -o 1001 -g 1001   files/settings.json  "${ROOTFS_DIR}/home/signalk/.signalk/settings.json"
 install -m 755 -o 1001 -g 1001   files/signalk-server "${ROOTFS_DIR}/home/signalk/.signalk/signalk-server"
+
 on_chroot << EOF
-npm install --loglevel error -g --unsafe-perm signalk-server
+  npm install --loglevel error -g --unsafe-perm signalk-server
 EOF
+
 install -m 644 -v files/signalk.service "${ROOTFS_DIR}/etc/systemd/system/signalk.service"
 install -m 644 -v files/signalk.socket  "${ROOTFS_DIR}/etc/systemd/system/signalk.socket"
 
