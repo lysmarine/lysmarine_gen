386b3797f21919bac4648c8f66e8b97395cef91a
Frederic Guilbault <fred@0464.ca>
2019-03-18 18:50:44 -0400

    Add missing deps for openplotter + switch to v2.x.x

--- 48-openplotter/05-openplotter/01-run.sh
+++ 48-openplotter/05-openplotter/01-run.sh
@@ -3,15 +3,14 @@ install -d  "${ROOTFS_DIR}/opt/openplotter/"
 
 on_chroot << EOF
   pip install setuptools
-  pip install requests ujson websocket websocket-client paho-mqtt geomag
+  pip install requests ujson websocket websocket-client paho-mqtt geomag     pyudev serial smbus      websocket websocket-client python-can
 EOF
 
 on_chroot << EOF
-cd /opt/
-git clone https://github.com/sailoog/openplotter.git
-cd openplotter
-git checkout v2.x.x ; # We should use a fix commit hash to have reproductive builds
-
+  cd /opt/
+  git clone https://github.com/sailoog/openplotter.git
+  cd openplotter
+  git checkout v2.x.x ; # We should use a fix commit hash to have reproductive builds
 EOF
 
 install -d -o 1000 -g 1000 "${ROOTFS_DIR}/home/pi/.local/share/applications/"
