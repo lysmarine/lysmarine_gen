2f396a9f443b39239817b1c5549080951e622c04
Frederic Guilbault <fred@0464.ca>
2019-03-24 19:31:07 -0400

    +1

--- 20-boot/01-splashScreen/01-run.sh
+++ 20-boot/01-splashScreen/01-run.sh
@@ -2,7 +2,7 @@
 
 cat  files/appendToConfig.txt >> "${ROOTFS_DIR}/boot/config.txt"
 
-install -v -m0755 files/showLysmarineLogo.sh "${ROOTFS_DIR}/bin/splashScreen"
+install -v -m0755 files/splashScreen "${ROOTFS_DIR}/bin/splashScreen"
 install -v files/ascii_logo.txt "${ROOTFS_DIR}/etc/motd"
 install -v files/splash.png "${ROOTFS_DIR}/usr/share/splash.png"
 install -v -m644 files/splashscreen.service "${ROOTFS_DIR}/etc/systemd/system/splashscreen.service"
