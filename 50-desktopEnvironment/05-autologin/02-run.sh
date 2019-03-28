fea148cf3c50af5159fed385a4161d0e7d70d9bc
Frederic Guilbault <fred@0464.ca>
2019-03-28 14:46:44 -0400

    Disable screensaver

--- 50-desktopEnvironment/05-autologin/02-run.sh
+++ 50-desktopEnvironment/05-autologin/02-run.sh
@@ -2,6 +2,7 @@
 install -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install files/autologin.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d"
 install -o 1000 -g 1000 files/profile "${ROOTFS_DIR}/home/pi/.profile"
+install files/x11vnc.service "${ROOTFS_DIR}/etc/systemd/system/x11vnc.service"
 
 on_chroot << EOF
 systemctl enable getty@tty1.service
