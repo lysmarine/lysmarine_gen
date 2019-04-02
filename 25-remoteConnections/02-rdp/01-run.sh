873c41d3466da2b8c8494dd8f409ad6e40f78f41
Frederic Guilbault <fred@0464.ca>
2019-04-02 08:49:54 -0400

    Move remote connection services to their own stage

---
+++ 25-remoteConnections/02-rdp/01-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+install files/Xwrapper.config "${ROOTFS_DIR}/etc/X11/Xwrapper.config"
+
+on_chroot <<EOF
+systemctl disable xrdp.service
+systemctl disable xrdp-sesman.service
+
+EOF
