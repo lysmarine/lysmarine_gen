c6e3bf28529f03dc4dbbb388e2ef031bba4f06c6
Frederic Guilbault <fred@0464.ca>
2019-04-02 16:46:22 -0400

    inline documentation

--- 50-desktopEnvironment/00-install-packages/03-run.sh
+++ 50-desktopEnvironment/00-install-packages/03-run.sh
@@ -1,5 +1,5 @@
 
 on_chroot << EOF
 chown 1000:1000 /home/pi/.local
-chown 1000:1000 /home/pi/.local/share #luakit change it to root.
+chown 1000:1000 /home/pi/.local/share # luakit change it to root So we must give it back to pi.
 EOF
