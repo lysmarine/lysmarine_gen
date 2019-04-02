210edb0efeb4eda4dae39b45d3d6d8fcdc7c6eaa
Frederic Guilbault <fred@0464.ca>
2019-04-02 12:03:49 -0400

    Disable RDP for now as it conflict with x11vnc

--- 25-remoteConnections/02-rdp/01-run.sh
+++ 25-remoteConnections/02-rdp/01-run.sh
@@ -3,7 +3,7 @@
 install files/Xwrapper.config "${ROOTFS_DIR}/etc/X11/Xwrapper.config"
 
 on_chroot <<EOF
-systemctl disable xrdp.service
-systemctl disable xrdp-sesman.service
+# systemctl disable xrdp.service
+# systemctl disable xrdp-sesman.service
 
 EOF
