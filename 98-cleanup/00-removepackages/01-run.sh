70cc5149a93974f8cc830c9e59460075d5543b74
Frederic Guilbault <fred@0464.ca>
2019-04-03 15:59:42 -0400

    Clean more stuff

--- 98-cleanup/00-removepackages/01-run.sh
+++ 98-cleanup/00-removepackages/01-run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash -e
 
-
 on_chroot << EOF
-apt remove -y xterm
+
+rm -rf /home/border # ???
+
+apt-get purge -y xterm
+apt-get purge -y vim
+apt-get purge -y plymouth
+
+rm -f /usr/share/applications/vim.desktop
+rm -f /usr/share/applications/PyCrust.desktop
+rm -f /usr/share/applications/XRCed.desktop
 
 rm -rf /home/pi/Public  /home/pi/Templates /home/pi/Videos /home/pi/Music /home/pi/Pictures /home/pi/Documents /home/pi/Desktop
 EOF
