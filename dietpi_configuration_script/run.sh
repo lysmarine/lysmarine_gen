0d67250bedff95f484e87e983f8fa1ea978f0c2b
Frederic Guilbault <fred@0464.ca>
2019-05-13 17:01:22 -0400

    Apply changes to the dietpi build process for lysmarine needs.

--- dietpi_configuration_script/run.sh
+++ dietpi_configuration_script/run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-
-# systemctl disable dietpi-boot.service
-# systemctl enable dietpi-preboot.service
-# systemctl enable dietpi-fs_partition_resize.service
-# systemctl disable dietpi-ramdisk.service
+ systemctl enable dietpi-fs_partition_resize.service
+ systemctl enable dietpi-ramlog.service
+ systemctl enable dietpi-ramdisk.service
+ systemctl enable dietpi-preboot.service
+ systemctl disable dietpi-boot.service
+ systemctl disable dietpi-postboot.service
 # systemctl disable dietpi-kill_ssh.service
-# systemctl enable dietpi-ramlog.service
-# systemctl disable dietpi-postboot.service
 # systemctl disable dietpi-wifi-monitor.service
-#
+
+systemctl unmask systemd-logind.service
+
 cp ./dietpi.txt /boot/
+
+cp ./firstBoot.sh /
+chmod a+x /firstBoot.sh
+
+sed -i 's/#ADD_POINT/\/firstBoot.sh/g' /home/dietpi/.profile
