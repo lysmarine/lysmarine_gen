59957c97065c8db4c53d0c7a4378dbdf4bcff8e4
Frederic Guilbault <fred@0464.ca>
2019-05-13 16:58:52 -0400

    Re enable auto login for lysmarine

--- lysmarine/55-autologin/run.sh
+++ lysmarine/55-autologin/run.sh
@@ -6,5 +6,4 @@ install -v $FILE_FOLDER/autologin.conf "/etc/systemd/system/getty@tty1.service.d
 install -v -o 1000 -g 1000 $FILE_FOLDER/profile "/home/dietpi/.profile"
 
 
-
-#systemctl enable getty@tty1.service
+systemctl enable getty@tty1.service
