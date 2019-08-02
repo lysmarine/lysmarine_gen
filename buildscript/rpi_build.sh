89892547d5c823f1332fdc93ef0ce41dc3bd3bdb
Frederic Guilbault <fred@0464.ca>
2019-08-02 17:42:35 -0300

    Reupload lismarine and dietpi at each mount.

--- buildscript/rpi_build.sh
+++ buildscript/rpi_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 {
 log () {
-          echo -e "\e[32m["$(date +'%T' )"]  \e[1m $1 \e[0m " #| tee -a "${LOG_FILE}"
+        echo "";
+        echo -e "\e[32m["$(date +'%T' )"]  \e[1m $1 \e[0m " #| tee -a "${LOG_FILE}"
 }
 
 
@@ -30,13 +31,13 @@ mkdir -p ./release/$thisArch
 
 # Check dependency Needed to shrink the image size at the end.
 if [ ! -f ./cache/pishrink.sh ] ; then
-        log "Downloading pishrink"
+        log "Downloading pishrink."
         cd ./cache
         wget https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
         chmod +x pishrink.sh
         cd ..
 else
-        log "Using pishrink from cache"
+        log "Using pishrink from cache."
 
 fi
 
@@ -44,7 +45,7 @@ fi
 
 # Check dietPi repo cache.
 if [ ! -d ./cache/DietPi/.git ] ; then
-        log "Downloading dietPi repository"
+        log "Downloading dietPi repository."
         cd ./cache
         git clone $dietPiRepo
         cd ./DietPi
@@ -52,7 +53,7 @@ if [ ! -d ./cache/DietPi/.git ] ; then
         cd ../../
 
 else
-        log "Updating dietPi repository"
+        log "Updating dietPi repository."
         cd ./cache/DietPi/
         git checkout $dietPiBranch
         git pull
@@ -64,13 +65,13 @@ fi
 
 # Download or copy the official image from cache
 if [ ! -f ./cache/$thisArch/$imageName ]; then
-        log "Downloading image from internet"
+        log "Downloading official Dietpi image from internet."
         wget -P ./cache/$thisArch/  $imageSource/$zipName
         7z e -o./cache/$thisArch/   ./cache/$thisArch/$zipName
-        rm ./cache/$thisSbc/$zipName ./cache/$thisSbc/hash.txt ./cache/$thisSbc/README.txt
+        rm ./cache/$thisArch/$zipName ./cache/$thisArch/hash.txt ./cache/$thisArch/README.txt
 
 else
-        log "Using image from cache"
+        log "Using official Dietpi image from cache."
 
 fi
 
@@ -78,59 +79,52 @@ fi
 
 # expend dietpi image
 if [ ! -f ./cache/$thisArch/$imageName-rdy2build ]; then
-        log "Getting DietPi image ready to run in qemu and build "
+        log "Getting DietPi image ready to run in qemu and build. "
         cp -fv ./cache/$thisArch/$imageName ./cache/$thisArch/$imageName-rdy2build
+
+        # Mounting image disk (but not the partitions yet)
+        log "Mounting Dietpi image."
+        IFS=$'\n' #to split lines into array
+        partitions=($(kpartx -sav ./cache/$thisArch/$imageName-rdy2build |  cut -d" " -f3))
+        partQty=${#partitions[@]}
+        echo $partQty partitions detected.
+
+        log "Resizing root partition."
         truncate -s "5G" ./cache/$thisArch/$imageName-rdy2build
-        partQty=$(sudo fdisk -l ./cache/$thisArch/$imageName-rdy2build | grep -e "^./cache/$thisArch/$imageName-rdy2build"|wc -l  )
         parted ./cache/$thisArch/$imageName-rdy2build --script "resizepart $partQty 100%" ;
-        # cp -fv ./cache/$thisArch/$imageName-rdy2build ./work/$thisArch/$imageName
 
-        # Mount the image and make the binds required to chroot.
-        log "Mount Dietpi image"
-        IFS=$'\n'
-        kpartx -sa ./work/$thisArch/$imageName
+        log "Mounting Dietpi partitions."
+        if [ $partQty == 2 ] ; then
+                mount -v /dev/mapper/${partitions[1]} ./work/$thisArch/rootfs/
+                mount -v /dev/mapper/${partitions[0]} ./work/$thisArch/rootfs/boot/
 
-        loop=$(losetup -j ./work/$thisArch/$imageName |  cut -d":" -f1 | sed "s/\/dev\/loop//g" )
-        loop=$(echo "${loop[*]}" | sort -nr | head -n1)
-
-        if [  -L "/dev/mapper/loop${loop}p2" ] ; then
-                mount /dev/mapper/loop${loop}p2 ./work/$thisArch/rootfs/
-                mount /dev/mapper/loop${loop}p1 ./work/$thisArch/rootfs/boot/
+        elif [ $partQty == 1 ] ; then
+                mount -v /dev/mapper/${partitions[0]} ./work/$thisArch/rootfs/
 
         else
-                mount /dev/mapper/loop${loop}p1 ./work/$thisArch/rootfs/
+                log "ERROR: unsuported amount of partitions."
+                exit 1
         fi
 
-        # Resize the root file system to fill the new drive size;
-        log "Resize the root file system to fill the new drive size WITHOUT DEV PROC SYS"
-        echo $partQty partitions detected.
-        resize2fs /dev/mapper/loop${loop}p$partQty
-
-        # Copy the lysmarine and dietpi config files in the mounted rootfs
-        log "copying lysmarine and dietpi_configuration_script on the image"
-        cp -r ../lysmarine ./work/$thisArch/rootfs/
-        chmod 0775 ./work/$thisArch/rootfs/lysmarine/*.sh
-        chmod 0775 ./work/$thisArch/rootfs/lysmarine/*/*.sh
-
-        cp -rvf ./cache/DietPi/rootfs/*    ./work/$thisArch/rootfs/
-        cp -rvf ./cache/DietPi/dietpi      ./work/$thisArch/rootfs/
+        log "Resize the root file system to fill the new drive size."
+        resize2fs /dev/mapper/${partitions[ $(($partQty - 1)) ]}
 
         # Fix the no-dns problem due to the fact that services are not started.
         mv ./work/$thisArch/rootfs/etc/resolv.conf ./work/$thisArch/rootfs/etc/resolv.conf.lysmarinebak
         cp -vf /etc/resolv.conf ./work/$thisArch/rootfs/etc/resolv.conf
 
-        #debootstrap
         log "Debootstraping."
         debootstrap --arch armhf --foreign --no-check-gpg --include $DBOOTinclude buster ./work/$thisArch/rootfs/ $DBOOTmirror
         cp -v /usr/bin/qemu-arm-static "./work/$thisArch/rootfs/usr/bin"
 
-        #Unmount.
 	log "Unmount Dietpi image"
         umount ./work/$thisArch/bootfs
         umount ./work/$thisArch/rootfs
+        kpartx -d ./cache/$thisArch/$imageName-rdy2build
 
 else
         log "Using Ready to build image from cache"
+
 fi
 
 
@@ -138,32 +132,50 @@ fi
 cp -fv ./cache/$thisArch/$imageName-rdy2build ./work/$thisArch/$imageName
 
 
+log "Mounting Dietpi image."
+IFS=$'\n' #to split lines into array
+partitions=($(kpartx -sav ./work/$thisArch/$imageName |  cut -d" " -f3))
+partQty=${#partitions[@]}
+echo $partQty partitions detected.
 
-# Mount the image and make the binds required to chroot.
-log "Mount Dietpi image"
-IFS=$'\n'
-kpartx -sa ./work/$thisArch/$imageName
 
-loop=$(losetup -j ./work/$thisArch/$imageName |  cut -d":" -f1 | sed "s/\/dev\/loop//g" )
-loop=$(echo "${loop[*]}" | sort -nr | head -n1)
 
-if [  -L "/dev/mapper/loop${loop}p2" ] ; then
-        mount /dev/mapper/loop${loop}p2 ./work/$thisArch/rootfs/
-        mount /dev/mapper/loop${loop}p1 ./work/$thisArch/rootfs/boot/
+log "Mounting Dietpi partitions."
+if [ $partQty == 2 ] ; then
+        mount -v /dev/mapper/${partitions[1]} ./work/$thisArch/rootfs/
+        mount -v /dev/mapper/${partitions[0]} ./work/$thisArch/rootfs/boot/
+
+elif [ $partQty == 1 ] ; then
+        mount -v /dev/mapper/${partitions[0]} ./work/$thisArch/rootfs/
 
 else
-        mount /dev/mapper/loop${loop}p1 ./work/$thisArch/rootfs/
+        log "ERROR: unsuported amount of partitions."
+        exit 1
 fi
 
 mount --bind /dev  ./work/$thisArch/rootfs/dev/
-#mount --bind /dev ./work/$thisArch/rootfs/dev/pts
+mount --bind /dev  ./work/$thisArch/rootfs/dev/pts
 mount --bind /sys  ./work/$thisArch/rootfs/sys/
 mount --bind /proc ./work/$thisArch/rootfs/proc/
+
+
+resize2fs /dev/mapper/${partitions[ $(($partQty - 1)) ]}
+
+
+
+# Copy the lysmarine and dietpi config files in the mounted rootfs
+log "copying lysmarine and dietpi_configuration_script on the image"
+cp -r ../lysmarine ./work/$thisArch/rootfs/
+chmod 0775 ./work/$thisArch/rootfs/lysmarine/*.sh
+chmod 0775 ./work/$thisArch/rootfs/lysmarine/*/*.sh
+
+cp -rvf ./cache/DietPi/rootfs/*    ./work/$thisArch/rootfs/
+cp -rvf ./cache/DietPi/dietpi      ./work/$thisArch/rootfs/
 
 cp -vf /etc/resolv.conf ./work/$thisArch/rootfs/etc/resolv.conf
 mv ./work/$thisArch/rootfs/etc/resolv.conf ./work/$thisArch/rootfs/etc/resolv.conf.lysmarinebak
 
-
+cp -v /usr/bin/qemu-arm-static "./work/$thisArch/rootfs/usr/bin"
 
 
 
@@ -177,7 +189,9 @@ echo "You are now in the chroot environement.";
 echo "Start the build script with by pasting the following line in the terminal:";
 echo "";
 echo "export ARCH=$thisArch ;cd /lysmarine; ./build.sh 00 10 50 51 55 98; exit"
-echo "";
+echo "export ARCH=RPi-ARMv6 ;cd /lysmarine; ./build.sh 00 10 20 21 22 26 27 30 31 32 50 51 52 55 98 ";
+echo "export ARCH=RPi-ARMv6 ;cd /lysmarine; ./build.sh ";
+
 echo "========================================================================="
 echo "";echo "";
 
@@ -194,14 +208,15 @@ mv ./work/$thisArch/rootfs/etc/resolv.conf.lysmarinebak ./work/$thisArch/rootfs/
 
 
 # Unmount the image
+log "Unmounting partitions"
 umount ./work/$thisArch/rootfs/dev/
 umount ./work/$thisArch/rootfs/sys/
 umount ./work/$thisArch/rootfs/proc/
-#  umount $ROOTFS/dev/pts
+umount ./work/$thisArch/dev/pts
 umount ./work/$thisArch/bootfs
 umount ./work/$thisArch/rootfs
 kpartx -d ./work/$thisArch/$imageName
-log "Image UNmounted"
+
 
 
 
@@ -214,6 +229,9 @@ log "Image UNmounted"
 cp -v ./work/$thisArch/$imageName  ./release/$thisArch/LysMarine_$thisArch-0.9.0.img
 log "DONE."
 
-# echo "cp -fv ./work/$thisArch/$imageName ./cache/$thisArch/$imageName-rdy2build" 
+
+
+echo "Pro Tip"
+echo "cp -v ./work/$thisArch/$imageName ./cache/$thisArch/$imageName-rdy2build"
 
 }; exit
