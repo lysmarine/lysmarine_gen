29bf06049bcf566c1ac07c1cb7fef5fbfa5aeb03
Frederic Guilbault <fred@0464.ca>
2019-11-25 09:15:26 -0500

    Code cleanup in stahe 31

--- lysmarine/31-kplex/run.sh
+++ lysmarine/31-kplex/run.sh
@@ -1,17 +1,9 @@
 #!/bin/bash -e
 
-cp $FILE_FOLDER/.kplex.conf "/home/pi/"
-chmod 0755 /home/pi/.kplex.conf
-chown 1000:1000 /home/pi/.kplex.conf
-echo $ARCH
+install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/.kplex.conf "/home/pi/.kplex.conf"
 
-if [ "$ARCH" == "NativePC-BIOS-x86_64" ] ; then
-        wget http://www.stripydog.com/download/kplex_1.4-1_amd64.deb
-        dpkg -i kplex_1.4-1_amd64.deb
-        rm kplex_1.4-1_amd64.deb
-else
-        wget http://www.stripydog.com/download/kplex_1.4-1_armhf.deb
-        dpkg -i kplex_1.4-1_armhf.deb
-        rm kplex_1.4-1_armhf.deb
-fi
+wget http://www.stripydog.com/download/kplex_1.4-1_armhf.deb
+dpkg -i kplex_1.4-1_armhf.deb
+rm kplex_1.4-1_armhf.deb
+
 systemctl enable kplex
