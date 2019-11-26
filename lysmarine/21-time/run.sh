8f4371aef64b1eb93b5a4d1ee4c770f3a837607e
Frederic Guilbault <fred@0464.ca>
2019-11-25 19:08:27 -0500

    GPS time in chrony won't be supported in 0.9, signalk will set the time

--- lysmarine/21-time/run.sh
+++ lysmarine/21-time/run.sh
@@ -3,7 +3,3 @@
 apt-get install -y -q chrony
 
 systemctl disable systemd-timesyncd
-
-echo "" >>  /etc/chrony/chrony.conf
-echo "# For data comming from signalk" >>  /etc/chrony/chrony.conf
-echo "refclock SHM 0 offset 0.5 delay 0.2 refid NMEA" >>  /etc/chrony/chrony.conf
