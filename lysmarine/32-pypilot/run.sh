0f90cad22774e2c07af76da1d06f726beb1294f2
Frederic Guilbault <fred@0464.ca>
2019-11-28 08:18:37 -0500

    Code cleaning of stage 32

--- lysmarine/32-pypilot/run.sh
+++ lysmarine/32-pypilot/run.sh
@@ -1,44 +1,36 @@
 #!/bin/bash -e
-apt-get install -y python-setuptools python-gps python-serial libpython-dev \
+apt-get install -y -q python-setuptools python-gps python-serial libpython-dev \
 python-numpy python-scipy swig python-pillow python-flask python-socketio \
 python-pip python-pylirc wiringpi python-flask python-gevent-websocket \
 python-wxgtk3.0 python-opengl
 
-
-mkdir -p "/home/pi/.pypilot"
-cp $FILE_FOLDER/signalk.conf "/home/pi/.pypilot/"
-chown 1000:1000 /home/pi/.pypilot/signalk.conf
-
-
-cp $FILE_FOLDER/pypilot.desktop "/usr/share/applications/"
-cp $FILE_FOLDER/pypilot_webapp.desktop "/usr/share/applications/"
-
-
 pip install wheel
 pip install pyglet ujson PyOpenGL PyWavefront flask_socketio
 
-
-
-
-echo " RTIMULib2 : "
+echo "Install RTIMULib2 as a (dep of Pypilot) : "
 git clone --depth=1 https://github.com/seandepagnier/RTIMULib2
-cd RTIMULib2/Linux/python
+pushd RTIMULib2/Linux/python
 python setup.py install
+popd
+rm -rf RTIMULib2
 
-cd ../../
-rm -rf /RTIMULib2
-
-echo " Pypilot : "
-cd /
+echo "Install Pypilot : "
+pushd /
 git clone --depth=1 https://github.com/pypilot/pypilot.git
 git clone --depth=1 https://github.com/pypilot/pypilot_data.git
 cp -rv pypilot_data/* pypilot
 rm -rf pypilot_data
 
-cd pypilot
+pushd ./pypilot
 python setup.py build
 python setup.py install
-cd scripts/debian/
+pushd ./scripts/debian/
 cp -r etc/systemd/system/* /etc/systemd/system/
-cd /
+popd; popd
 rm -rf pypilot
+popd
+
+install -d -v -o 1000 -g 1000 /home/pi/.pypilot
+install    -v -o 1000 -g 1000 $FILE_FOLDER/signalk.conf "/home/pi/.pypilot/"
+install    -v                 $FILE_FOLDER/pypilot.desktop "/usr/share/applications/"
+install    -v                 $FILE_FOLDER/pypilot_webapp.desktop "/usr/share/applications/"
