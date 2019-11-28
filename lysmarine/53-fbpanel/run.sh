4d0a22d41cac1a81b280aacdfa682bdf3f0c0541
Frederic Guilbault <fred@0464.ca>
2019-11-28 09:04:29 -0500

    Use nativefer for desktop webapp

--- lysmarine/53-fbpanel/run.sh
+++ lysmarine/53-fbpanel/run.sh
@@ -1,15 +1,6 @@
 #!/bin/bash -e
 apt-get install -y gsimplecal pavucontrol libgtk2.0-dev
 
-install    -o 1000 -g 1000 $FILE_FOLDER/panelbg.png "/usr/share/panelbg.png"
-
-
-install -d -o 1000 -g 1000 "/home/pi/.config/fbpanel"
-install    -o 1000 -g 1000 $FILE_FOLDER/default "/home/pi/.config/fbpanel/default"
-
-install -d  -o 1000 -g 1000 /home/pi/.config/openbox
-echo 'fbpanel &' >> /home/pi/.config/openbox/autostart
-
 git clone https://github.com/FredericGuilbault/fbpanel
 pushd ./fbpanel
 git checkout lysmarine_flavour
@@ -17,6 +8,11 @@ git checkout lysmarine_flavour
 ./configure --prefix=/usr/
 make
 make install
-
 popd
 rm -rf fbpanel
+
+install -d -o 1000 -g 1000 "/home/pi/.config/fbpanel"
+install    -o 1000 -g 1000 $FILE_FOLDER/default "/home/pi/.config/fbpanel/default"
+
+install -d  -o 1000 -g 1000 /home/pi/.config/openbox
+echo 'fbpanel -p default &' >> /home/pi/.config/openbox/autostart
---
+++ lysmarine/57-nativefier/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get -y -q install nodejs
+
+npm install nativefier -g --unsafe-perm
+
+install -v -o 1000 -g 1000 $FILE_FOLDER/freeboard-sk.png "/home/pi/.local/share/icons/freeboard-sk.png"
+
+nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instance \
+--name "SignalK"   --icon /home/pi/.local/share/icons/signalk.png \
+"http://localhost:80" /opt/
+
+nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instance \
+ --name "Freeboard-sk" --icon /home/pi/.local/share/icons/freeboard-sk.png \
+ "http://localhost/@signalk/freeboard-sk/" /opt/
+
+nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instance \
+ --name "SpeedSample" --icon /usr/share/icons/gnome/256x256/apps/utilities-system-monitor.png \
+ "http://localhost:4998" /opt/
+
+ nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instance \
+  --name "Tuktuk" --icon /home/pi/.icons/Flat-Remix-Dark/apps/scalable/android-studio.svg \
+  "http://localhost:4999" /opt/
