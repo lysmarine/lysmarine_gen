a073da0b95b7b073362e5f65f62d9ee590b0eb2e
Frederic Guilbault <fred@0464.ca>
2019-11-28 08:59:13 -0500

    Fix servicefile perms

--- lysmarine/55-autologin/run.sh
+++ lysmarine/55-autologin/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 install -v -d              "/etc/systemd/system/getty@tty1.service.d"
-install -v                 $FILE_FOLDER/autologin.conf "/etc/systemd/system/getty@tty1.service.d"
+install -v -m644           $FILE_FOLDER/autologin.conf "/etc/systemd/system/getty@tty1.service.d"
 install -v -o 1000 -g 1000 $FILE_FOLDER/profile "/home/pi/.profile"
 
 systemctl enable getty@tty1.service
