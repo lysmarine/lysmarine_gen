4afa02c16a25d2932dbc7135da9c9e60c2e288ca
Frederic Guilbault <fred@0464.ca>
2019-12-29 12:06:36 -0500

    have only one wpa_supplicant running

--- lysmarine/22-networkInterfaces/run.sh
+++ lysmarine/22-networkInterfaces/run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -e
 
 # network manager
-apt-get install -y network-manager network-manager-gnome
-apt-get install -y util-linux procps hostapd iproute2 iw dnsmasq iptables
+apt-get install -y network-manager
+apt-get install -y git util-linux procps hostapd iproute2 iw dnsmasq iptables
 
 #acces point
 git clone --depth=1 https://github.com/oblique/create_ap
-cd create_ap
+pushd create_ap
 make install
-cd ../
+popd
 
 cp $FILE_FOLDER/create_ap.conf /etc/
 rm -rf create_ap
+
+systemctl disable dhcpcd.service
+systemctl disable wpa_supplicant.service
