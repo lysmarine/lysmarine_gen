8e685ce7bbeee7062bbbff7aac43d982b46155d9
Frederic Guilbault <fred@0464.ca>
2020-01-09 15:29:28 -0500

    Cleanup stage 50

--- lysmarine/50-desktopEnvironment/run.sh
+++ lysmarine/50-desktopEnvironment/run.sh
@@ -2,32 +2,25 @@
 
 touch /etc/X11/Xwrapper.config
 
-apt-get install -y \
-gstreamer1.0-x gstreamer1.0-omx gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-libav alsa-utils \
-nodejs libavahi-compat-libdnssd-dev  git \
-pcmanfm \
-openbox xbacklight lxappearance gmrun xdotool xsettingsd gnome-themes-standard \
-xserver-xorg xinit xserver-xorg-video-fbdev \
-xserver-xorg-video-fbturbo \
-libgtk2-perl \
-leafpad gsimplecal \
-pavucontrol \
-cpanminus perl-base dialog \
-lxterminal \
-luakit \
-conky-all \
-network-manager-gnome
 
 
-apt-get install -y chromium-browser
+apt-get install -q -y \
+gstreamer1.0-x gstreamer1.0-omx gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
+gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-libav alsa-utils \
+libavahi-compat-libdnssd-dev git openbox xbacklight lxappearance gmrun xdotool \
+xsettingsd gnome-themes-standard xserver-xorg xinit xserver-xorg-video-fbdev \
+xserver-xorg-video-fbturbo libgtk2-perl \
+gsimplecal pavucontrol cpanminus perl-base dialog \
+lxterminal network-manager-gnome
 
 
-install -d -o pi -g pi /home/pi/.local
-install -d -o pi -g pi /home/pi/.local/share
 
-chown pi:pi /home/pi/.local
-chown pi:pi /home/pi/.local/share # luakit change it to root So we must give it back to pi.
+install -d -o 1000 -g 1000 /home/pi/.local
+install -d -o 1000 -g 1000 /home/pi/.local/share
+
+
 
 # Openbox
+install -d -o 1000 -g 1000 "/home/pi/.config"
 install -d -o 1000 -g 1000 "/home/pi/.config/openbox"
 install -o 1000 -g 1000  -v $FILE_FOLDER/autostart     "/home/pi/.config/openbox/"
