00095f4b970e0d5282293d00f86f7e13ef86210d
Frederic Guilbault <fred@0464.ca>
2020-01-10 18:50:37 -0500

    Make onboard load it's conf only once.

--- lysmarine/52-onboard/run.sh
+++ lysmarine/52-onboard/run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -e
-apt-get install -y onboard dconf-editor dbus-x11
+apt-get install -y onboard dconf-cli dbus-x11
 
 install -d "/usr/share/onboard/"
 install -v $FILE_FOLDER/onboard.dconf "/usr/share/onboard/"
 install -v $FILE_FOLDER/a11y.dconf "/usr/share/onboard/"
 
 install -d -o 1000 -g 1000 /home/pi/.config/openbox/
-echo "sleep 1 ; dconf load /org/onboard/ < /usr/share/onboard/onboard.dconf &" >> /home/pi/.config/openbox/autostart
-echo "sleep 1 ; dconf load / < /usr/share/onboard/a11y.dconf &" >> /home/pi/.config/openbox/autostart
+echo "dconf load /org/onboard/ < /usr/share/onboard/onboard.dconf &" >> /home/pi/.config/openbox/autostart
+echo "dconf load / < /usr/share/onboard/a11y.dconf &" >> /home/pi/.config/openbox/autostart
+
+echo "sed -i 's/^dconf\ /#&/' /home/pi/.config/openbox/autostart; " >> /home/pi/.config/openbox/autostart
+echo "sed -i 's/^sed\ /#&/'   /home/pi/.config/openbox/autostart; " >> /home/pi/.config/openbox/autostart
