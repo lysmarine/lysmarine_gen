f37abbcf13ca2db6bc642dd24b90c9e801b23bb7
Frederic Guilbault <fred@0464.ca>
2020-01-11 14:07:46 -0500

    packages cleanup

--- lysmarine/50-desktopEnvironment/run.sh
+++ lysmarine/50-desktopEnvironment/run.sh
@@ -7,10 +7,10 @@ touch /etc/X11/Xwrapper.config
 apt-get install -q -y \
 gstreamer1.0-x gstreamer1.0-omx gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
 gstreamer1.0-plugins-bad gstreamer1.0-alsa gstreamer1.0-libav alsa-utils \
-libavahi-compat-libdnssd-dev git openbox xbacklight lxappearance gmrun xdotool \
+libavahi-compat-libdnssd-dev git openbox xbacklight lxappearance gmrun \
 xsettingsd gnome-themes-standard xserver-xorg xinit xserver-xorg-video-fbdev \
 xserver-xorg-video-fbturbo libgtk2-perl \
-gsimplecal pavucontrol cpanminus perl-base dialog \
+pavucontrol cpanminus perl-base dialog \
 lxterminal network-manager-gnome
 
 
@@ -24,3 +24,6 @@ install -d -o 1000 -g 1000 /home/pi/.local/share
 install -d -o 1000 -g 1000 "/home/pi/.config"
 install -d -o 1000 -g 1000 "/home/pi/.config/openbox"
 install -o 1000 -g 1000  -v $FILE_FOLDER/autostart     "/home/pi/.config/openbox/"
+
+# Make som room for the rest
+apt-get clean
--- lysmarine/98-cleanup/run.sh
+++ lysmarine/98-cleanup/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
+rm -rvf /tmp/empty-cache46
+rm -fvf /usr/share/applications/vim.desktop
+rm -fvf /usr/share/applications/PyCrust.desktop
+rm -fvf /usr/share/applications/XRCed.desktop
+rm -rvf /home/pi/Public /home/pi/Templates /home/pi/Videos /home/pi/Desktop
 
-rm -f /usr/share/applications/vim.desktop
-rm -f /usr/share/applications/PyCrust.desktop
-rm -f /usr/share/applications/XRCed.desktop
-
-rm -rf /home/pi/Public  /home/pi/Templates /home/pi/Videos /home/pi/Music /home/pi/Pictures /home/pi/Documents /home/pi/Desktop
+apt-get clean
+apt remove -y greybird-gtk-theme murrine-themes rpd-icons
 apt -y autoremove
+apt-get clean
