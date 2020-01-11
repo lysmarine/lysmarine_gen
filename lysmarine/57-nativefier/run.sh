de3999fd4daf7cca4ce1f8b59eaad12195923a53
Frederic Guilbault <fred@0464.ca>
2020-01-11 14:06:31 -0500

    Remove tuktuk

--- lysmarine/57-nativefier/run.sh
+++ lysmarine/57-nativefier/run.sh
@@ -4,7 +4,9 @@ apt-get -y -q install nodejs libnss3
 
 npm install nativefier -g --unsafe-perm
 
+install -d -o 1000 -g 1000 "/home/pi/.local/share/icons"
 install -v -o 1000 -g 1000 -m 644 $FILE_FOLDER/freeboard-sk.png "/home/pi/.local/share/icons/freeboard-sk.png"
+install -v -o 1000 -g 1000 -m 644 $FILE_FOLDER/signalk.png "/home/pi/.local/share/icons/"
 
 nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instance \
 --name "SignalK"   --icon /home/pi/.local/share/icons/signalk.png \
@@ -17,10 +19,6 @@ nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instanc
 nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instance \
  --name "SpeedSample" --icon /usr/share/icons/gnome/256x256/apps/utilities-system-monitor.png \
  "http://localhost:4998" /opt/
-
- nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instance \
-  --name "Tuktuk" --icon /home/pi/.icons/Flat-Remix-Dark/apps/scalable/android-studio.svg \
-  "http://localhost:4999" /opt/
 
 nativefier -a armv7l --disable-context-menu --disable-dev-tools --single-instance \
 	--name "Pypilot_webapp" --icon /usr/share/icons/gnome/256x256/actions/go-jump.png  \
