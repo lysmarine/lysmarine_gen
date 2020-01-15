8ebe7655e987d56520ee099b9df2287ca6d5017e
Frederic Guilbault <fred@0464.ca>
2020-01-15 17:22:58 -0500

    Add user stage

--- lysmarine/18-users/run.sh
+++ lysmarine/18-users/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
+echo 'root:changeme' | chpasswd
+
 if [ $LMBUILD == raspbian ] ;then
 	userdel -r -f pi
 	rm /etc/sudoers.d/010_pi-nopasswd
@@ -8,8 +10,8 @@ if [ ! -d /home/user ] ; then
 	adduser --home /home/user --quiet --disabled-password -gecos "lysmarine" user
 fi
 echo 'user:changeme' | chpasswd
-echo 'root:changeme' | chpasswd
 
 echo -n 'lysmarine' > /etc/hostname
+echo '127.0.1.1	lysmarine' >> /etc/hosts
 
 echo "user ALL=(ALL:ALL) ALL" >> /etc/sudoers
