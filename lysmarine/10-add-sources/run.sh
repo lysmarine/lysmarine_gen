90d63cf030a7b29c5caa0e56886f1b79ecf22ec6
border <border@border-GS70-2OD>
2020-01-28 11:34:24 -0500

    code cleanup on stage 10

--- lysmarine/10-add-sources/run.sh
+++ lysmarine/10-add-sources/run.sh
@@ -3,43 +3,28 @@ if [ $LMBUILD == raspbian ] ;then
 	apt-mark hold raspberrypi-sys-mods
 fi
 
-if [ $LMBUILD == armbian-pineA64 ] ;then
-	echo 'root:raspberry' | chpasswd
-fi
-
-
-
 apt-get update  -y -q
+apt-get upgrade -y -q
 apt-get install -y -q apt-transport-https lsb-release wget gnupg
 
 
 
-DISTRO="$(lsb_release -s -c)"
+## Nodejs
+wget wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add
+echo "deb https://deb.nodesource.com/node_10.x buster main" | tee /etc/apt/sources.list.d/nodesource.list
 
-if [ $LMBUILD == debian-amd64 ] ;then
-	echo "";
-
-else
-	## Nodejs10
-	wget wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add
-	echo "deb https://deb.nodesource.com/node_10.x $DISTRO main" | tee /etc/apt/sources.list.d/nodesource.list
-
-fi
-
-
-
-## Opencpn
+## Opencpn  
+#opencpn's PPA does not provide arm64 binaries, but the multiarch packages are still usefull.
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67E4A52AC865EB40
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF0E1940624A220
 echo "deb http://ppa.launchpad.net/opencpn/opencpn/ubuntu/ eoan main" > /etc/apt/sources.list.d/opencpnsource.list
-install -m0644 -v $FILE_FOLDER/50-lysmarine "/etc/apt/preferences.d/"
+install -m0644 -v $FILE_FOLDER/50-lysmarine "/etc/apt/preferences.d/" # make opencpn's PPA prefered to free-x
 
 ## XyGrib
 wget -q -O - https://www.free-x.de/debian/oss.boating.gpg.key  | apt-key add -
-echo "deb https://www.free-x.de/debian/ $DISTRO main contrib non-free" > /etc/apt/sources.list.d/oss.list
+echo "deb https://www.free-x.de/debian/ buster main contrib non-free" > /etc/apt/sources.list.d/oss.list
 
 
 
 ## Upadate
 apt-get update  -y -q
-apt-get upgrade -y -q
