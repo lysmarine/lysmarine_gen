c30772a9dd6c31eb9abc968f3fa4f607509c0e0d
border <border@border-GS70-2OD>
2020-02-02 23:06:54 -0500

    make opencpn build with chart inbedded instead of hacking apt

--- lysmarine/95-opencpn/run.sh
+++ lysmarine/95-opencpn/run.sh
@@ -5,23 +5,17 @@ install -o 1000 -g 1000 -v $FILE_FOLDER/opencpn.conf "/home/user/.opencpn/"
 
 
 if [ $LMBUILD == armbian-pineA64 ] ;then
+	apt-mark hold opencpn
 	apt-get install -y -q build-essential cmake gettext git-core gpsd gpsd-clients libgps-dev wx-common libwxgtk3.0-dev libglu1-mesa-dev libgtk2.0-dev wx3.0-headers libbz2-dev libtinyxml-dev libportaudio2 portaudio19-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev libelf-dev libsqlite3-dev
 	git clone git://github.com/OpenCPN/OpenCPN.git
 	mkdir -p ./OpenCPN/build
 
 	pushd ./OpenCPN/build;
-		cmake -DCFLAGS="-O2 -march=native" -DBUNDLE_DOCS=OFF -DBUNDLE_TCDATA=ON -DBUNDLE_GSHHS=HIGH ../
+		cmake  -DOCPN_BUNDLE_TCDATA=ON -DOCPN_BUNDLE_GSHHS=CRUDE ../
 		make
 		make install
 	popd
 	rm -r ./OpenCPN
-
-	apt-get download opencpn-tcdata
-	dpkg -i --ignore-depends=opencpn opencpn-tcdata*
-	apt download opencpn-gshhs-*
-	dpkg -i --ignore-depends=opencpn opencpn-gshhs-*
-
-	rm opencpn-tcdata* opencpn-gshhs-*
 else
 	apt-get install -y -q opencpn
 
