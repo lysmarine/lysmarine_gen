705a6784cfdde235201e926cd03c4683a05c2e11
Frederic Guilbault <2@0464.ca>
2020-02-13 16:22:53 -0500

    Pypilot regression

--- lysmarine/32-pypilot/run.sh
+++ lysmarine/32-pypilot/run.sh
@@ -2,34 +2,39 @@
 apt-get install -y -q python-setuptools python-gps python-serial libpython-dev \
 python-numpy python-scipy swig python-pillow python-flask python-socketio \
 python-pip python-pylirc  python-flask python-gevent-websocket \
-python-wxgtk4.0 python-opengl
+python-wxgtk4.0 python-opengl python-wxtools
 
 if [ $LMBUILD == raspbian ] ;then
 	apt-get install -y -q wiringpi
 fi
 
 pip install wheel
-pip install pyglet ujson PyOpenGL PyWavefront flask_socketio
+pip install pyglet ujson PyOpenGL PyWavefront pyudev flask_socketio
 
 echo "Install RTIMULib2 as a (dep of Pypilot) : "
 git clone --depth=1 https://github.com/seandepagnier/RTIMULib2
 pushd RTIMULib2/Linux/python
-python setup.py install
+	python setup.py install
 popd
 rm -rf RTIMULib2
 
 echo "Install Pypilot : "
 pushd /
-git clone --depth=1 https://github.com/pypilot/pypilot.git
-git clone --depth=1 https://github.com/pypilot/pypilot_data.git
-cp -rv pypilot_data/* pypilot
-rm -rf pypilot_data
+	git clone https://github.com/pypilot/pypilot.git
+	pushd ./pypilot
+		git checkout db173ae4409aba2900dfd58c50bf8a409cd954e7 # Forced regression due to broken GTK GUI
+	popd 
 
-pushd ./pypilot
-python setup.py build
-python setup.py install
-pushd ./scripts/debian/
-cp -r etc/systemd/system/* /etc/systemd/system/
+	git clone --depth=1 https://github.com/pypilot/pypilot_data.git
+	cp -rv pypilot_data/* pypilot
+	rm -rf pypilot_data
+
+	pushd ./pypilot
+		python setup.py build
+		python setup.py install
+	pushd ./scripts/debian/
+	cp -r etc/systemd/system/* /etc/systemd/system/
+
 popd; popd
 rm -rf pypilot
 popd
