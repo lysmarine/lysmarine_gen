f2c34c99ae99632a842573f3b7a95f61d63ed5ac
Frederic Guilbault <2@0464.ca>
2020-03-31 23:55:19 -0400

    check cache aganst folder not file+ don't assume service exist.

--- lysmarine/22-networkInterfaces/run.sh
+++ lysmarine/22-networkInterfaces/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -e
 
 # Network manager
-apt-get install -y -q network-manager make 
+apt-get install -y -q network-manager make
 
 
 
 # Resolve lysmarine.local
-apt-get install -y -q avahi-daemon 
+apt-get install -y -q avahi-daemon
 echo -n 'lysmarine' > /etc/hostname
 echo '127.0.1.1	lysmarine' >> /etc/hosts
 echo '127.0.1.1	lysmarine.local' >> /etc/hosts
@@ -15,13 +15,13 @@ echo '127.0.1.1	lysmarine.local' >> /etc/hosts
 
 # Access Point management
 apt-get install -y -q git util-linux procps hostapd iproute2 iw dnsmasq iptables
-pushd ./stageCache 
-	if [[ ! -f create_ap ]]; then 
+pushd ./stageCache
+	if [[ ! -d create_ap ]]; then
 		git clone --depth=1 https://github.com/oblique/create_ap
 	fi
 	pushd create_ap
 		make install
-	popd		
+	popd
 popd
 
 cp $FILE_FOLDER/create_ap.conf /etc/
@@ -29,13 +29,16 @@ rm -rf create_ap
 
 
 
-##  NetworkManager provide it's own wpa_supplicant, stop the others to avoid conflicts.  
-systemctl disable dhcpcd.service
+##  NetworkManager provide it's own wpa_supplicant, stop the others to avoid conflicts.
+
+if [[ $LMOS == 'Raspbian' ]]; then
+	systemctl disable dhcpcd.service
+fi
 systemctl disable wpa_supplicant.service
 systemctl disable hostapd.service
 
 
-## Disable some useless networking serivces 
+## Disable some useless networking serivces
 systemctl disable NetworkManager-wait-online.service # if we do not boot remote user it's not needed
 systemctl disable ModemManager.service # for 2G/3G/4G
 systemctl disable pppd-dns.service # For dial-up Internet LOL
