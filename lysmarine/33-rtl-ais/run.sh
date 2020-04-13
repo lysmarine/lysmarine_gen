7709decbcb29a880f63731c1308e6c0abca2a1f0
Frederic Guilbault <2@0464.ca>
2020-04-13 11:29:46 -0400

    Add missing dep to ais

--- lysmarine/33-rtl-ais/run.sh
+++ lysmarine/33-rtl-ais/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 ## https://pysselilivet.blogspot.com/2018/06/ais-reciever-for-raspberry.html
-apt-get install -y -q git librtlsdr-dev
+apt-get install -y -q git librtlsdr-dev pkg-config
 
 
 
-## Compiling 
-pushd ./stageCache 
+## Compiling
+pushd ./stageCache
 	if [[ ! -d rtl-ais ]]; then
 		git clone --depth 1 https://github.com/dgiardini/rtl-ais/
 		pushd ./rtl-ais
@@ -13,12 +13,12 @@ pushd ./stageCache
 			make
 		popd
 	fi
-	
+
 	pushd ./rtl-ais
 		make install
 	popd
 popd
 
-## Adding service file 
+## Adding service file
 install -v -m 0644 $FILE_FOLDER/rtl-ais.service "/etc/systemd/system/"
 systemctl enable rtl-ais.service
