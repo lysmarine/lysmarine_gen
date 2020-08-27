4629bb420edea65018b5de8e98bac52fac808397
Frederic Guilbault <2@0464.ca>
2020-08-27 14:28:19 -0400

    Update opencpn sources

--- lysmarine/10-add-sources/run.sh
+++ lysmarine/10-add-sources/run.sh
@@ -11,15 +11,15 @@ wget wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key 
 echo "deb https://deb.nodesource.com/node_10.x buster main" | tee /etc/apt/sources.list.d/nodesource.list
 
 ## Opencpn  
-#opencpn's PPA does not provide arm64 binaries, but the multiarch packages are still usefull.
+#opencpn's PPA does not provide arm64 binaries, but the multiarch packages are still useful.
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67E4A52AC865EB40
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF0E1940624A220
-echo "deb http://ppa.launchpad.net/opencpn/opencpn/ubuntu/ eoan main" > /etc/apt/sources.list.d/opencpnsource.list
-install -m0644 -v $FILE_FOLDER/50-lysmarine "/etc/apt/preferences.d/" # make opencpn's PPA prefered to free-x
+echo "deb http://ppa.launchpad.net/opencpn/opencpn/ubuntu bionic main" > /etc/apt/sources.list.d/opencpn.list
+install -m0644 -v $FILE_FOLDER/50-lysmarine "/etc/apt/preferences.d/" # Prefer opencpn's PPA to free-x
 
 ## XyGrib
 wget -q -O - https://www.free-x.de/debian/oss.boating.gpg.key  | apt-key add -
-echo "deb https://www.free-x.de/debian/ buster main contrib non-free" > /etc/apt/sources.list.d/oss.list
+echo "deb https://www.free-x.de/debian/ buster main contrib non-free" > /etc/apt/sources.list.d/xygrib.list
 
 
 
