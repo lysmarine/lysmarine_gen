53f7faf06344f0e563f1bb44d34202171dad3749
Frederic Guilbault <2@0464.ca>
2020-10-05 17:50:02 -0400

    fix typos

--- lysmarine/10-add-sources/run.sh
+++ lysmarine/10-add-sources/run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash -e
 
-
-
-
 apt-get update  -y -q
 apt-get upgrade -y -q
 apt-get install -y -q apt-transport-https lsb-release wget gnupg dirmngr
 
-
-## NodejsY
+## Nodejs
 wget wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add
 echo "deb https://deb.nodesource.com/node_10.x buster main" | tee /etc/apt/sources.list.d/nodesource.list
 
@@ -24,6 +20,5 @@ wget -q -O - https://www.free-x.de/debian/oss.boating.gpg.key  | apt-key add -
 echo "deb https://www.free-x.de/debian/ buster main contrib non-free" > /etc/apt/sources.list.d/xygrib.list
 
 
-
-## Upadate
+## Update
 apt-get update  -y -q
