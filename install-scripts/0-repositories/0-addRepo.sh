2861de2d3820914b64d6f549737fe05eb845dfa9
Frederic Guilbault <2@0464.ca>
2020-10-09 15:37:33 -0400

    Rewrite stage 0

--- install-scripts/0-repositories/0-addRepo.sh
+++ install-scripts/0-repositories/0-addRepo.sh
@@ -1,27 +1,22 @@
 #!/bin/bash -e
 
+## Add repository sources
+install -m0644 -v $FILE_FOLDER/nodesource.list "/etc/apt/sources.list.d/"
+install -m0644 -v $FILE_FOLDER/opencpn.list "/etc/apt/sources.list.d/"
+install -m0644 -v $FILE_FOLDER/xygrib.list "/etc/apt/sources.list.d/"
+install -m0644 -v $FILE_FOLDER/lysmarine.list "/etc/apt/sources.list.d/"
+
+## Prefer opencpn PPA to free-x (for mainly for the opencpn package)
+install -m0644 -v $FILE_FOLDER/50-lysmarine.pref "/etc/apt/preferences.d/"
+
+## Get the signature keys
+apt-get install -y -q  wget gnupg
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67E4A52AC865EB40          # Opencpn
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF0E1940624A220          # Opencpn
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 868273edce9979e7          # lysmarine (provide: createap, rtl-ais, fbpanel)
+wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add     #NodeJs
+wget -q -O- https://www.free-x.de/debian/oss.boating.gpg.key     | apt-key add -   # XyGrib
+
+## Update && Upgrade
 apt-get update  -y -q
 apt-get upgrade -y -q
-apt-get install -y -q apt-transport-https lsb-release wget gnupg dirmngr
-
-## Nodejs
-wget wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add
-echo "deb https://deb.nodesource.com/node_10.x buster main" | tee /etc/apt/sources.list.d/nodesource.list
-
-## Opencpn  
-#opencpn's PPA does not provide arm64 binaries, but the multiarch packages are still useful.
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67E4A52AC865EB40
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF0E1940624A220
-echo "deb http://ppa.launchpad.net/opencpn/opencpn/ubuntu bionic main" > /etc/apt/sources.list.d/opencpn.list
-install -m0644 -v $FILE_FOLDER/50-lysmarine "/etc/apt/preferences.d/" # Prefer opencpn's PPA to free-x
-
-## XyGrib
-wget -q -O - https://www.free-x.de/debian/oss.boating.gpg.key  | apt-key add -
-echo "deb https://www.free-x.de/debian/ buster main contrib non-free" > /etc/apt/sources.list.d/xygrib.list
-
-## lysmarine (provide: createap, rtl-ais, fbpanel)
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 868273edce9979e7
-echo "deb http://ppa.launchpad.net/lysmarine/upstream-projects/ubuntu bionic main " > /etc/apt/sources.list.d/lysmarine.list
-
-## Update
-apt-get update  -y -q
