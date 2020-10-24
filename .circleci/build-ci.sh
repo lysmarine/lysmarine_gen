fc49f7da41119fd30845719b4e383ad43dea0b21
mgrouch <mgrushinskiy@gmail.com>
2020-10-24 05:59:22 -0400

    CI

--- .circleci/build-ci.sh
+++ .circleci/build-ci.sh
@@ -15,15 +15,15 @@ sleep 5;
 
 if [ "$EMU" = "on" ]; then
   if [ "$CONTAINER_DISTRO" = "raspbian" ]; then
-      docker run --rm --privileged multiarch/qemu-user-static:register --reset
+      docker run --rm --privileged --cap-add=ALL multiarch/qemu-user-static:register --reset
   else
-      docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+      docker run --rm --privileged --cap-add=ALL multiarch/qemu-user-static --reset -p yes
   fi
 fi
 
 WORK_DIR=$(pwd):/ci-source
 
-docker run --privileged -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
+docker run --privileged --cap-add=ALL -d -ti -e "container=docker"  -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
 DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}')
 
 docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get update
