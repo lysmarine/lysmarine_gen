892564967d357be849efe28690a3eb0cde3bb515
mgrouch <mgrushinskiy@gmail.com>
2020-10-26 12:58:58 -0400

    CI

--- .circleci/build-ci.sh
+++ .circleci/build-ci.sh
@@ -32,9 +32,9 @@ docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install \
   proot git-core live-build kpartx p7zip p7zip-full parted e2fsprogs qemu-user
 
 docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
-    "update-alternatives --set fakeroot /usr/bin/fakeroot-tcp; cd ci-source/cross-build-release; chmod -v u+w *.sh; /bin/bash -xe ./raspbian.sh "
+    "cd ci-source/cross-build-release; chmod -v u+w *.sh; /bin/bash -xe ./raspbian.sh "
 
-docker exec --privileged --security-opt="seccomp=unconfined" -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
+docker exec --privileged -ti $DOCKER_CONTAINER_ID /bin/bash -xec \
     cd /lysmarine; export LMBUILD="raspbian"; ./build.sh
 
 pwd
