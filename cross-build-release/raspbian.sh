08cd6facbfe2e0e549f4d0c7111e125d2c98b2d9
mgrouch <mgrushinskiy@gmail.com>
2020-10-26 16:28:01 -0400

    CI

--- cross-build-release/raspbian.sh
+++ cross-build-release/raspbian.sh
@@ -84,6 +84,8 @@ ls -l /run
 MK_ROOT=work/${thisArch}/rootfs
 mkdir -p ./cache/${thisArch}/stageCache
 mkdir -p /run/shm
+mkdir -p $MK_ROOT/lysmarine/stageCache
+mkdir -p $MK_ROOT/run/shm
 mount -o bind /etc/resolv.conf $MK_ROOT/etc/resolv.conf
 mount -o bind /dev $MK_ROOT/dev
 mount -o bind /sys $MK_ROOT/sys
