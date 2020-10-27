7f452432f9af55f4b8c49b42448d30b2e22707ef
mgrouch <mgrushinskiy@gmail.com>
2020-10-26 21:05:18 -0400

    CI

--- install-scripts/2-baseSystem/0-users.sh
+++ install-scripts/2-baseSystem/0-users.sh
@@ -4,11 +4,8 @@ apt-get -y -q install sudo policykit-1
 ## Force keyboard layout to be EN US by default.
 sed -i "s/XKBLAYOUT=.*/XKBLAYOUT=\"us\"/g" /etc/default/keyboard
 
-
 ## Set root password.
 echo 'root:changeme' | chpasswd
-
-
 
 ## Remove default user (if any).
 oldUser=$(cat /etc/passwd | grep 1000:1000 | cut -f1 -d:) 
@@ -19,8 +16,6 @@ else
 	echo "No default user found !"
 fi
 
-
-
 ## Add default user.
 adduser --uid 1000 --home /home/user --quiet --disabled-password -gecos "lysmarine" user
 echo 'user:changeme' | chpasswd
@@ -30,23 +25,17 @@ usermod -a -G tty user
 usermod -a -G sudo user
 usermod -a -G video user
 
-
-
 ## Create signalk user to run the server.
 if [ ! -d /home/signalk ] ; then
 	echo "Creating signalk user"
 	adduser --home /home/signalk --gecos --system --disabled-password --disabled-login signalk
 fi
 
-
-
 ## Create pypilot user to run the services.
 if [ ! -d /home/pypilot ] ; then
 	echo "Creating pypilot user"
 	adduser --home /home/pypilot --gecos --system --disabled-password --disabled-login pypilot
 fi
-
-
 
 ## Create the charts group and add users that have to write to that folder.
 if ! grep -q charts /etc/group ; then
@@ -56,19 +45,13 @@ if ! grep -q charts /etc/group ; then
 	usermod -a -G charts root;
 fi
 
-
-
 ## Create the special charts folder.
 install -v -d -m 6775 -o signalk -g charts /srv/charts; 
-
-
 
 ## Link the chart folder to home for convenience.
 if [ ! -f /home/user/charts ] ; then
 	su user -c "ln -s /srv/charts /home/user/charts;"
 fi
-
-
 
 ## Manage the permissions and privileges.
 if [[ -d /etc/polkit-1 ]]; then
@@ -87,8 +70,6 @@ echo 'PATH="/sbin:/usr/sbin:$PATH"' >> /home/user/.profile # Give user capabilit
 if [ -f /root/.not_logged_in_yet ] ;then # Disable first login script.
 	rm /root/.not_logged_in_yet
 fi
-
-
 
 ## Prevent the creation of useless home folders on first boot.
 if [ -f /etc/xdg/user-dirs.defaults ] ;then
