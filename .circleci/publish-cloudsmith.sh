f95d52bf6f2fcb99888e345a02f72fce83d320e9
mgrouch <mgrushinskiy@gmail.com>
2020-10-27 12:04:46 -0400

    CI

--- .circleci/publish-cloudsmith.sh
+++ .circleci/publish-cloudsmith.sh
@@ -9,8 +9,8 @@ DISTRO=$3
 pwd
 ls
 
-for pkg_file in cross-build-release/release/*/*.img; do
-  cloudsmith push raw $REPO/$DISTRO $pkg_file  --summary "LysMarine built by CircleCi on $(date)" --description "LysMarine BBN build"
+for pkg_file in cross-build-release/release/*/*.$EXT; do
+  cloudsmith push raw $REPO $pkg_file --summary "LysMarine built by CircleCi on $(date)" --description "LysMarine BBN build"
   RESULT=$?
   if [ $RESULT -eq 144 ]; then
      echo "skipping already deployed $pkg_file"
