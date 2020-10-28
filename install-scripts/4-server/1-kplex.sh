b1a05d4a18a6fe18c81b969133e4a3b8b569184b
mgrouch <mgrushinskiy@gmail.com>
2020-10-28 01:50:08 -0400

    CI

--- install-scripts/4-server/1-kplex.sh
+++ install-scripts/4-server/1-kplex.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 apt-get install -y -q libc6
 
+install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/.kplex.conf "/home/user/.kplex.conf"
+
 if [[ $LMARCH == 'amd64' ]]; then
 	package='kplex_1.4-1_amd64.deb'
 elif [[ $LMARCH == 'armhf' ]]; then
 	package='kplex_1.4-1_armhf.deb'
 elif [[ $LMARCH == 'arm64' ]]; then
-	apt-get install -y -q gcc-8-base libc6 libgcc1
+	apt-get install -y -q gcc-8-base:armhf libc6:armhf libgcc1:armhf
 	package='kplex_1.4-1_armhf.deb'
 fi
-
-install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/.kplex.conf "/home/user/.kplex.conf"
 
 pushd ./stageCache
 	if [[ ! -f $package ]]; then
