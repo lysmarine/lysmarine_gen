5e7df4f4583a2f77c8261dd1f33876d78257fe36
mgrouch <mgrushinskiy@gmail.com>
2020-11-02 17:50:44 -0500

    CI

--- install-scripts/0-repositories/0-addRepo.sh
+++ install-scripts/0-repositories/0-addRepo.sh
@@ -6,6 +6,7 @@ install -m0644 -v $FILE_FOLDER/opencpn.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/xygrib.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/lysmarine.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/bbn-rce.list "/etc/apt/sources.list.d/"
+install -m0644 -v $FILE_FOLDER/bbn-kplex.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/bbn.list "/etc/apt/sources.list.d/"
 
 ## Prefer opencpn PPA to free-x (for mainly for the opencpn package)
@@ -19,6 +20,7 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 868273edce9979e7       
 wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -   # NodeJs
 wget -q -O- https://www.free-x.de/debian/oss.boating.gpg.key     | apt-key add -   # XyGrib
 wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-rce/cfg/gpg/gpg.540A03461CECBA19.key | apt-key add -
+wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-kplex/cfg/gpg/gpg.B487196268D0D9B6.key | apt-key add -
 wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-repo/cfg/gpg/gpg.070C975769B2A67A.key | apt-key add -
 
 ## Update && Upgrade
