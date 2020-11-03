f65d8db37795387fd24aef9733d5e968aad98389
mgrouch <mgrushinskiy@gmail.com>
2020-11-03 07:22:16 -0500

    CI

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
+
+apt-get install -y -q libportaudio2 libgtk-3-0 wx-common wx3.1-i18n libwxbase3.1-5v5 libwxgtk3.1-gtk3-5v5 libwxgtk-media3.1-5v5 libwxgtk-webview3.1-5v5 libwxsvg3
+
 apt-get install -y -q opencpn
 
 install -o 1000 -g 1000 -d "/home/user/.opencpn"
