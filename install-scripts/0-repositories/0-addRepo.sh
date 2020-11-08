e93e4e56c69a0a04c5f420c2ed9339f75195297a
mgrouch <mgrushinskiy@gmail.com>
2020-11-07 19:35:21 -0500

    more repos for fbpanel and mopidy

--- install-scripts/0-repositories/0-addRepo.sh
+++ install-scripts/0-repositories/0-addRepo.sh
@@ -7,6 +7,8 @@ install -m0644 -v $FILE_FOLDER/xygrib.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/lysmarine.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/bbn-rce.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/bbn-kplex.list "/etc/apt/sources.list.d/"
+install -m0644 -v $FILE_FOLDER/bbn-fbpanel.list "/etc/apt/sources.list.d/"
+install -m0644 -v $FILE_FOLDER/mopidy.list "/etc/apt/sources.list.d/"
 
 ## Prefer opencpn PPA to free-x (for mainly for the opencpn package)
 #install -m0644 -v $FILE_FOLDER/50-lysmarine.pref "/etc/apt/preferences.d/"
@@ -20,6 +22,8 @@ wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 wget -q -O- https://www.free-x.de/debian/oss.boating.gpg.key     | apt-key add -   # XyGrib
 wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-rce/cfg/gpg/gpg.540A03461CECBA19.key | apt-key add -
 wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-kplex/cfg/gpg/gpg.B487196268D0D9B6.key | apt-key add -
+wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-fbpanel/cfg/gpg/gpg.89DE2CF06C6908DA.key | apt-key add -
+wget -q -O- https://apt.mopidy.com/mopidy.gpg | apt-key add -
 
 ## Update && Upgrade
 apt-get update  -y -q
