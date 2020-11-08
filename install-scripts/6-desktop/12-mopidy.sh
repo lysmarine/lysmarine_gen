ad9b67102e176b4a0e91d4dcfc0699cd6005bde0
mgrouch <mgrushinskiy@gmail.com>
2020-11-08 08:55:48 -0500

    mopidy (without spotify support)

--- install-scripts/6-desktop/12-mopidy.sh
+++ install-scripts/6-desktop/12-mopidy.sh
@@ -4,7 +4,8 @@ adduser mopidy video
 adduser mopidy audio
 
 # Install Mopidy and all dependencies:
-apt-get -y install mopidy mopidy-mpd mopidy-spotify mopidy-tunein libspotify-dev xdotool
+#apt-get -y install mopidy mopidy-mpd mopidy-spotify mopidy-tunein libspotify-dev xdotool
+apt-get -y install mopidy mopidy-mpd mopidy-tunein xdotool
 
 # Install some needed packages
 python3 -m pip install mem systems
@@ -17,3 +18,5 @@ python3 -m pip install --pre Mopidy-YouTube
 
 # Enable mopidy service
 systemctl enable mopidy
+
+install -m 644 $FILE_FOLDER/musicbox.desktop "/usr/local/share/applications/"
