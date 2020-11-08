1bc8eee334bc37feff401ae7bc1134fd5f8cabbd
mgrouch <mgrushinskiy@gmail.com>
2020-11-07 19:47:47 -0500

    more repos for fbpanel and mopidy

---
+++ install-scripts/6-desktop/12-mopidy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+adduser mopidy video
+adduser mopidy audio
+xhost +SI:localuser:mopidy
+
+# Install Mopidy and all dependencies:
+apt-get -y install mopidy mopidy-mpd mopidy-spotify mopidy-tunein
+
+# Install some needed packages
+python3 -m pip install mem systems
+
+# Install Mopidy MusicBox Web Client:
+python3 -m pip install Mopidy-MusicBox-Webclient
+
+# Install YouTube support
+python3 -m pip install --pre Mopidy-YouTube
