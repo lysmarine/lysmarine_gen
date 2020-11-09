28dae2ed50d7ea6c30fffbe88b7b65544ab40bdc
mgrouch <mgrushinskiy@gmail.com>
2020-11-09 13:06:31 -0500

    mopidy (without spotify support)

--- install-scripts/6-desktop/11-evdev-rce.sh
+++ install-scripts/6-desktop/11-evdev-rce.sh
@@ -2,7 +2,7 @@
 
 apt-get install -y -q evdev-rce
 
-echo 'uinput' | sudo tee -a /etc/modules
+echo 'uinput' | tee -a /etc/modules
 
 install -o 1000 -g 1000 -d "/home/user/.config"
 install -o 1000 -g 1000 -d "/home/user/.config/autostart"
