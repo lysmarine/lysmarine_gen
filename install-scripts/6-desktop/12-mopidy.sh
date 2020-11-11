ece30115a715f3751346f75a24665cbc4e0bd12e
mgrouch <mgrushinskiy@gmail.com>
2020-11-11 15:17:03 -0500

    -

--- install-scripts/6-desktop/12-mopidy.sh
+++ install-scripts/6-desktop/12-mopidy.sh
@@ -24,3 +24,5 @@ python3 -m pip install https://github.com/natumbri/mopidy-youtube/archive/develo
 systemctl enable mopidy
 
 install -m 644 $FILE_FOLDER/musicbox.desktop "/usr/local/share/applications/"
+
+python3 setup.py clean --all
