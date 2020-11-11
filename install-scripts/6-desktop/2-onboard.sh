d0cd63fbabf805d59022ffd6ad50e2bc9bb3436b
mgrouch <mgrushinskiy@gmail.com>
2020-11-11 07:42:23 -0500

    gtk2

--- install-scripts/6-desktop/2-onboard.sh
+++ install-scripts/6-desktop/2-onboard.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt-get install -y onboard dconf-cli dbus-x11
+apt-get install -y onboard dconf-cli dbus-x11 at-spi2-core
 
 install -d "/usr/share/onboard/"
 install -v $FILE_FOLDER/onboard.dconf "/usr/share/onboard/"
