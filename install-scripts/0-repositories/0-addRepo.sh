3970953c0e68c00939996167ca6cddcac0095b9d
mgrouch <mgrushinskiy@gmail.com>
2020-11-11 06:24:37 -0500

    gtk2

--- install-scripts/0-repositories/0-addRepo.sh
+++ install-scripts/0-repositories/0-addRepo.sh
@@ -7,6 +7,7 @@ install -m0644 -v $FILE_FOLDER/xygrib.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/lysmarine.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/bbn-rce.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/bbn-kplex.list "/etc/apt/sources.list.d/"
+install -m0644 -v $FILE_FOLDER/bbn-gtk2-repo.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/mopidy.list "/etc/apt/sources.list.d/"
 
 ## Prefer opencpn PPA to free-x (for mainly for the opencpn package)
@@ -21,6 +22,7 @@ wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 wget -q -O- https://www.free-x.de/debian/oss.boating.gpg.key     | apt-key add -   # XyGrib
 wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-rce/cfg/gpg/gpg.540A03461CECBA19.key | apt-key add -
 wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-kplex/cfg/gpg/gpg.B487196268D0D9B6.key | apt-key add -
+wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-gtk2-repo/cfg/gpg/gpg.937F9585EB75CD5A.key | apt-key add -
 wget -q -O- https://apt.mopidy.com/mopidy.gpg | apt-key add -
 
 ## Update && Upgrade
