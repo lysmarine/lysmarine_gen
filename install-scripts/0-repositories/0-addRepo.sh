36d18e2f1860481d733303addc952d679289760d
mgrouch <mgrushinskiy@gmail.com>
2020-11-13 19:47:33 -0500

    another opencpn from launchpad

--- install-scripts/0-repositories/0-addRepo.sh
+++ install-scripts/0-repositories/0-addRepo.sh
@@ -7,7 +7,7 @@ install -m0644 -v $FILE_FOLDER/xygrib.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/lysmarine.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/bbn-rce.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/bbn-kplex.list "/etc/apt/sources.list.d/"
-install -m0644 -v $FILE_FOLDER/bbn-gtk2-repo.list "/etc/apt/sources.list.d/"
+install -m0644 -v $FILE_FOLDER/bbn-opencpn.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/mopidy.list "/etc/apt/sources.list.d/"
 
 ## Prefer opencpn PPA to free-x (for mainly for the opencpn package)
@@ -18,11 +18,12 @@ apt-get install -y -q  wget gnupg
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67E4A52AC865EB40          # Opencpn
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF0E1940624A220          # Opencpn
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 868273edce9979e7          # lysmarine (provide: createap, rtl-ais, fbpanel)
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 24A4598E769C8C51          # bbn-opencpn on launchpad
+
 wget -q -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -   # NodeJs
 wget -q -O- https://www.free-x.de/debian/oss.boating.gpg.key     | apt-key add -   # XyGrib
 wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-rce/cfg/gpg/gpg.540A03461CECBA19.key | apt-key add -
 wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-kplex/cfg/gpg/gpg.B487196268D0D9B6.key | apt-key add -
-wget -q -O- https://dl.cloudsmith.io/public/bbn-projects/bbn-gtk2-repo/cfg/gpg/gpg.937F9585EB75CD5A.key | apt-key add -
 wget -q -O- https://apt.mopidy.com/mopidy.gpg | apt-key add -
 
 ## Update && Upgrade
