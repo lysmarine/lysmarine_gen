00435dab1fab442f9c502c06945ac750cc6b422b
Frederic Guilbault <2@0464.ca>
2020-11-19 22:20:55 -0500

    Minor fix

--- cross-build-release/build.sh
+++ cross-build-release/build.sh
@@ -100,7 +100,8 @@
 		xorriso -as mkisofs -V 'Debian 10.1 amd64 custom nonfree' -o "$releaseDir/lysmarine-$lmVersion-$baseOS-$cpuArch.iso" -J -J -joliet-long -cache-inodes -b isolinux/isolinux.bin -c isolinux/boot.cat -boot-load-size 4 -boot-info-table -no-emul-boot -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus "$workDir/rootfs"
 
 	elif [[ $baseOS == 'debian-vbox' ]]; then
-		echo "not yet"
+		echo "not implemented yet"
+		exit 1
 	fi
 
 	exit 0
