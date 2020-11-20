e926f193a8ef5e35ca0c048f638cc152e23c306d
mgrouch <mgrushinskiy@gmail.com>
2020-11-20 09:17:38 -0500

    cert

--- install-scripts/0-repositories/0-addRepo.sh
+++ install-scripts/0-repositories/0-addRepo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+apt-get update  -y -q
+apt-get install -y -q  wget gnupg ca-certificates
+
 ## Add repository sources
 install -m0644 -v $FILE_FOLDER/nodesource.list "/etc/apt/sources.list.d/"
 install -m0644 -v $FILE_FOLDER/opencpn.list "/etc/apt/sources.list.d/"
@@ -14,7 +17,6 @@ install -m0644 -v $FILE_FOLDER/mopidy.list "/etc/apt/sources.list.d/"
 #install -m0644 -v $FILE_FOLDER/50-lysmarine.pref "/etc/apt/preferences.d/"
 
 ## Get the signature keys
-apt-get install -y -q  wget gnupg ca-certificates
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67E4A52AC865EB40          # Opencpn
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6AF0E1940624A220          # Opencpn
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 868273EDCE9979E7          # lysmarine (provide: createap, rtl-ais, fbpanel)
