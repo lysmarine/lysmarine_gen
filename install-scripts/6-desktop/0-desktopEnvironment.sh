f09ea1c3699454cc306f7f242c7ebf46b2b2e7f7
mgrouch <mgrushinskiy@gmail.com>
2020-11-21 15:11:29 -0500

    -budgie

--- install-scripts/6-desktop/0-desktopEnvironment.sh
+++ install-scripts/6-desktop/0-desktopEnvironment.sh
@@ -5,6 +5,13 @@ install  -v $FILE_FOLDER/Xwrapper.config "/etc/X11/"
 
 apt-get -q -y install xserver-xorg-video-fbturbo
 
+# Install touchscreen drivers
+apt-get -q -y install xserver-xorg-input-libinput
+apt-get -q -y install xinput libinput-tools
+apt-get -q -y install xinput-calibrator
+
+apt-get install -q -y budgie-desktop
+
 apt-get install -q -y \
 gstreamer1.0-x gstreamer1.0-omx gstreamer1.0-plugins-base \
 gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-alsa \
@@ -12,11 +19,6 @@ gstreamer1.0-libav alsa-utils libavahi-compat-libdnssd-dev git openbox \
 xbacklight lxappearance gmrun xsettingsd xserver-xorg \
 xinit libgtk2-perl cpanminus perl-base \
 dialog lxterminal network-manager-gnome
-
-# Install touchscreen drivers
-apt-get -q -y install xserver-xorg-input-libinput
-apt-get -q -y install xinput libinput-tools
-apt-get -q -y install xinput-calibrator
 
 install -o 1000 -g 1000 -d /home/user/.local
 install -o 1000 -g 1000 -d /home/user/.local/share
