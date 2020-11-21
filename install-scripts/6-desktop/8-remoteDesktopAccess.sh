21220eb4da640e11334ef48a86739b609d6039cc
mgrouch <mgrushinskiy@gmail.com>
2020-11-21 18:40:03 -0500

    -

--- install-scripts/6-desktop/8-remoteDesktopAccess.sh
+++ install-scripts/6-desktop/8-remoteDesktopAccess.sh
@@ -2,9 +2,9 @@
 
 apt-get install -y -q x11vnc
 
-install -v -m 644             $FILE_FOLDER/vnc.service "/etc/systemd/system/vnc.service"
+install -v -m 644 $FILE_FOLDER/vnc.service "/etc/systemd/system/vnc.service"
 install -v -d -o 1000 -g 1000 "/home/user/.vnc"
-install -v    -o 1000 -g 1000 $FILE_FOLDER/passwd "/home/user/.vnc/passwd"
+install -v -o 1000 -g 1000 $FILE_FOLDER/passwd "/home/user/.vnc/passwd"
 
 systemctl enable vnc
 
