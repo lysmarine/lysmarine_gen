990830a2f72c5b8ad18d98860c3407e35429b978
mgrouch <mgrushinskiy@gmail.com>
2020-11-21 17:58:48 -0500

    -

--- install-scripts/6-desktop/2-onboard.sh
+++ install-scripts/6-desktop/2-onboard.sh
@@ -9,11 +9,11 @@ install -v $FILE_FOLDER/a11y.dconf "/usr/share/onboard/"
 ## Dconf injection on the first boot + self commenting sed replace.
 install -d -o 1000 -g 1000 /home/user/.config/openbox/
 
-echo "dconf load /org/onboard/ < /usr/share/onboard/onboard.dconf &" >> /home/user/.config/openbox/autostart
-echo "dconf load / < /usr/share/onboard/a11y.dconf &" >> /home/user/.config/openbox/autostart
+echo "dconf load /org/onboard/ < /usr/share/onboard/onboard.dconf" >> /home/user/.config/openbox/autostart
+echo "dconf load / < /usr/share/onboard/a11y.dconf" >> /home/user/.config/openbox/autostart
 
-echo "sed -i 's/^dconf\ /#&/' /home/user/.config/openbox/autostart; " >> /home/user/.config/openbox/autostart
-echo "sed -i 's/^sed\ /#&/'   /home/user/.config/openbox/autostart; " >> /home/user/.config/openbox/autostart
+echo "sed -i 's/^dconf\ /#&/' /home/user/.config/openbox/autostart" >> /home/user/.config/openbox/autostart
+echo "sed -i 's/^sed\ /#&/'   /home/user/.config/openbox/autostart" >> /home/user/.config/openbox/autostart
 
 install -d -o 1000 -g 1000 /home/user/.local/share/onboard/themes
 sed -e "s#_font></#_font>Sans</#" -e "s#Granite#Charcoal#" /usr/share/onboard/themes/Droid.theme > /home/user/.local/share/onboard/themes/Droid.theme
