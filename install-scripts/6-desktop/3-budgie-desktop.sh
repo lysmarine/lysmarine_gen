1cba7ece5ee35400f88cff140faf745dee0fc148
mgrouch <mgrushinskiy@gmail.com>
2020-11-21 13:28:26 -0500

    -budgie

--- install-scripts/6-desktop/3-budgie-desktop.sh
+++ install-scripts/6-desktop/3-budgie-desktop.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
 
-apt-get install -y libatk-adaptor libgtk2.0-dev
+apt-get install -y -q libatk-adaptor libgtk2.0-dev
 
-apt-get install -y libatk1.0-0 libcairo2 libfontconfig1 libfreetype6 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk2.0-0 \
+apt-get install -y -q libatk1.0-0 libcairo2 libfontconfig1 libfreetype6 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk2.0-0 \
   libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 librsvg2-common libx11-6
 
-apt-get install -y budgie-desktop
+apt-get install -y -q budgie-desktop
+
+apt-get install -y -q fbpanel
 
 ## Start budgie-desktop on openbox boot.
 install -o 1000 -g 1000 -d /home/user/.config/openbox
-echo 'export XDG_CURRENT_DESKTOP=Budgie:GNOME; budgie-desktop &' >>/home/user/.config/openbox/autostart
+#echo 'export XDG_CURRENT_DESKTOP=Budgie:GNOME; budgie-desktop &' >>/home/user/.config/openbox/autostart
+echo 'fbpanel &' >>/home/user/.config/openbox/autostart
 echo 'chromium --headless &' >>/home/user/.config/openbox/autostart
