037128e180615a4ece1144615031ffaeeb85cf2c
mgrouch <mgrushinskiy@gmail.com>
2020-11-22 04:36:44 -0500

    apps

--- install-scripts/6-desktop/22-sdr.sh
+++ install-scripts/6-desktop/22-sdr.sh
@@ -5,7 +5,7 @@ apt-get -y -q install hamradio-sdr
 apt-get -y -q install gqrx-sdr
 apt-get -y -q install cubicsdr
 apt-get -y -q install multimon-ng netcat
-apt-get -y -q install hamradio-sdr
+#apt-get -y -q install hamradio-sdr
 apt-get -y -q install fldigi
 apt-get -y -q install predict gpredict
 apt-get -y -q install gnuais gnuaisgui
