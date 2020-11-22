31a407f8d1af2767deed5602244bef82a4769f0a
mgrouch <mgrushinskiy@gmail.com>
2020-11-22 17:06:59 -0500

    apps

--- install-scripts/6-desktop/19-avnav.sh
+++ install-scripts/6-desktop/19-avnav.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+apt-get clean
+
 sudo apt-get -q -y install avnav xterm mpg123
 
 install -o 1000 -g 1000 -d /home/user/AvNavCharts
@@ -10,3 +12,5 @@ adduser avnav audio
 systemctl enable avnav
 
 install -m 644 $FILE_FOLDER/avnav.desktop "/usr/local/share/applications/"
+
+apt-get clean
