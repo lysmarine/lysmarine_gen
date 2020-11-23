749bb1fb9bb1facfe5d082c52407b72e81eb6c90
Frederic Guilbault <2@0464.ca>
2020-11-23 15:51:08 -0500

    Begin integration of BBN docker Container hacks for arm emulation

--- cross-build-release/build.sh
+++ cross-build-release/build.sh
@@ -91,7 +91,7 @@ EOT
 
 		# Re-squash the file system.
 		pushd "$workDir/"
-		mksquashfs squashfs-root/ ./filesystem.squashfs -comp xz -noappend -processors 8
+		mksquashfs squashfs-root/ ./filesystem.squashfs -comp xz -noappend -processors 2
 		rm -r ./squashfs-root
 		popd
 
