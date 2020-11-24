494b8dde55c3c1b7246e4801aaba8ed602f39cbd
mgrouch <mgrushinskiy@gmail.com>
2020-11-23 20:28:01 -0500

    fix

--- .circleci/publish-cloudsmith.sh
+++ .circleci/publish-cloudsmith.sh
@@ -8,14 +8,18 @@ EXT=$1
 REPO=$2
 DISTRO=$3
 
+pwd
+ls
+
 for pkg_file in cross-build-release/release/*/*.$EXT; do
   zipName=$(basename $pkg_file)
   zipDir=$(dirname $pkg_file)
   mkdir ./tmp
   chmod 755 ./tmp
   cd $zipDir || exit 255
-  xz -z -9 --threads=4 ${zipName}
-  cloudsmith push raw $REPO ${zipName}.xz --summary "LysMarine built by CircleCi on $(date)" --description "LysMarine BBN build"
+  xz -z -c -9 --threads=4 ${zipName} > ../../../tmp/${zipName}.xz
+  cd ../../..
+  cloudsmith push raw $REPO ./tmp/${zipName}.xz --summary "LysMarine built by CircleCi on $(date)" --description "LysMarine BBN build"
   RESULT=$?
   if [ $RESULT -eq 144 ]; then
     echo "skipping already deployed $pkg_file"
