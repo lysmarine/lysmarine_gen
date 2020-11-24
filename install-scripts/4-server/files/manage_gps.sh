0a7e1a77366118a27107a927c71f48c0c9263d28
mgrouch <mgrushinskiy@gmail.com>
2020-11-23 19:44:56 -0500

    permissions

--- install-scripts/4-server/files/manage_gps.sh
+++ install-scripts/4-server/files/manage_gps.sh
@@ -2,10 +2,10 @@
 {
   chmod a+r /dev/ttyLYS_gps_"$1"
   if [[ $2 == "remove" ]] ; then
-    logger "The USB device /dev/ttyLYS_gps_$1 have been disconnected"
+    logger "The USB device /dev/ttyLYS_gps_$1 has been disconnected"
     systemctl stop lysgpsd@"$1".service
   else
-    logger "This USB device is known as a GPS and will be connected to gpsd on port 2947 /dev/ttyLYS_gps_$1"
+    logger "This USB device is known as GPS and will be connected to gpsd on port 2947 /dev/ttyLYS_gps_$1"
     systemctl restart lysgpsd@"$1".service
     sleep 1
     systemctl restart signalk
