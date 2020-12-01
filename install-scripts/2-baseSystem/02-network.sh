a91209a43c0ee7cf9e7d5cff28d26a0002af2594
mgrouch <mgrushinskiy@gmail.com>
2020-11-30 23:45:33 -0500

    net

--- install-scripts/2-baseSystem/02-network.sh
+++ install-scripts/2-baseSystem/02-network.sh
@@ -3,20 +3,28 @@
 # Network manager
 apt-get install -y -q network-manager make
 
+
 # Resolve lysmarine.local
 apt-get install -y -q avahi-daemon
 install -v $FILE_FOLDER/hostname "/etc/"
 cat $FILE_FOLDER/hosts >> /etc/hosts
 
+
 # Access Point management
 apt-get install -y -q createap
+install -m0600 -v $FILE_FOLDER/lysmarine-hotspot.nmconnection "/etc/NetworkManager/system-connections/"
+systemctl disable dnsmasq
+
 
 ##  NetworkManager provide its own wpa_supplicant, stop the others to avoid conflicts.
-systemctl disable dhcpcd.service
+if service --status-all | grep -Fq 'dhcpcd'; then
+	systemctl disable dhcpcd.service
+fi
 systemctl disable wpa_supplicant.service
 systemctl disable hostapd.service
 
-## Disable some useless networking serivces
-systemctl disable NetworkManager-wait-online.service # if we do not boot remote user it's not needed
+
+## Disable some useless networking services
+systemctl disable NetworkManager-wait-online.service # if we do not boot remote user over the network this is not needed
 systemctl disable ModemManager.service # for 2G/3G/4G
 systemctl disable pppd-dns.service # For dial-up Internet LOL
