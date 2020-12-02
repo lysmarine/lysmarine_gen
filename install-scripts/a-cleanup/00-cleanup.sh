d9e37b630ed59b008f908ba4dc23211b1feb64b3
mgrouch <mgrushinskiy@gmail.com>
2020-12-01 22:37:51 -0500

    menu re-org

--- install-scripts/a-cleanup/00-cleanup.sh
+++ install-scripts/a-cleanup/00-cleanup.sh
@@ -13,13 +13,3 @@ npm cache clean --force
 # remove python pip cache
 rm -rf ~/.cache/pip
 
-## Menu cleanup (Not a good idea as they will come back on apt updates)
-rm -fv /usr/share/applications/vim.desktop
-rm -fv /usr/share/applications/htop.desktop
-rm -fv /usr/share/applications/PyCrust.desktop
-rm -fv /usr/share/applications/XRCed.desktop
-rm -fv /usr/share/applications/x11vnc.desktop
-rm -fv /usr/share/applications/org.gnome.FileRoller.desktop
-rm -fv /usr/share/applications/gmrun.desktop
-rm -fv /usr/share/applications/mopidy.desktop
-rm -fv /usr/share/applications/xinput_calibrator.desktop
