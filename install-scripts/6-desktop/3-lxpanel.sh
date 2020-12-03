422b8cb5bdb66b680cd6b8e593dfd52f2b9f6c14
Frederic Guilbault <2@0464.ca>
2020-12-03 14:27:08 -0500

    switch to unconfigurated lxpanel

---
+++ install-scripts/6-desktop/3-lxpanel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+apt-get install -y lxpanel gsimplecal menu
+
+## Install configuration files.
+install -d -o 1000 -g 1000 "/home/user/.config/lxpanel/lysmarine/panels/"
+install    -o 1000 -g 1000 $FILE_FOLDER/panel "/home/user/.config/lxpanel/lysmarine/panels/"
+
+## Start fbpanel on openbox boot.
+install -d -o 1000 -g 1000 /home/user/.config/openbox
+echo 'lxpanel -p lysmarine &' >> /home/user/.config/openbox/autostart
