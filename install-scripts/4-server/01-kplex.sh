8c17ca63bf1296bfded639830069c11e7fdc27f8
mgrouch <mgrushinskiy@gmail.com>
2020-12-04 12:00:51 -0500

    kplex

--- install-scripts/4-server/01-kplex.sh
+++ install-scripts/4-server/01-kplex.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
+
 apt-get install -y -q libc6
 
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/.kplex.conf "/home/user/.kplex.conf"
+install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/kplex-lysmarine.conf "/etc/"
+install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/kplex-lysmarine.conf "/etc/kplex.conf"
 
 apt-get install -y -q kplex
 
