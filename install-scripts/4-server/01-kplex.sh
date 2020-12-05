0c86bfa30226117d4139e7a296a550133de2d687
mgrouch <mgrushinskiy@gmail.com>
2020-12-04 20:11:57 -0500

    fix

--- install-scripts/4-server/01-kplex.sh
+++ install-scripts/4-server/01-kplex.sh
@@ -2,10 +2,10 @@
 
 apt-get install -y -q libc6
 
+apt-get install -y -q kplex
+
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/.kplex.conf "/home/user/.kplex.conf"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/kplex-lysmarine.conf "/etc/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/kplex-lysmarine.conf "/etc/kplex.conf"
-
-apt-get install -y -q kplex
 
 systemctl enable kplex
