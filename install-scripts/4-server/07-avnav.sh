4ec3a6d8a8b0771e0dfde4065aef79a2f3f94e18
mgrouch <mgrushinskiy@gmail.com>
2020-12-04 19:06:22 -0500

    avnav

--- install-scripts/4-server/07-avnav.sh
+++ install-scripts/4-server/07-avnav.sh
@@ -14,7 +14,9 @@ usermod -a -G charts avnav
 systemctl enable avnav
 
 install -m 644 $FILE_FOLDER/avnav.desktop "/usr/local/share/applications/"
+install -v -o 1000 -g 1000 -m 0644 /var/lib/avnav/avnav_server.xml "/var/lib/avnav/avnav_server.xml-orig"
 install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/avnav_server_lysmarine.xml "/var/lib/avnav/"
+install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/avnav_server_lysmarine.xml "/var/lib/avnav/avnav_server.xml"
 
 apt-get clean
 npm cache clean --force
