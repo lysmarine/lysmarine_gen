db29e2870f5cf61fe129a14f913bd23165ff787a
mgrouch <mgrushinskiy@gmail.com>
2020-12-04 23:58:24 -0500

    fix

--- install-scripts/4-server/07-avnav.sh
+++ install-scripts/4-server/07-avnav.sh
@@ -2,21 +2,22 @@
 
 apt-get clean
 
-sudo apt-get -q -y --no-install-recommends install avnav xterm mpg123
-
 install -o 1000 -g 1000 -d /home/user/AvNavCharts
 install -o 1000 -g 1000 -d /home/user/AvNavCharts/out
+install -o 1000 -g 1000 -d /var/lib/avnav
+
+sudo apt-get -q -y --no-install-recommends install avnav xterm mpg123
 
 adduser avnav audio
 
 usermod -a -G charts avnav
 
-systemctl enable avnav
-
 install -m 644 $FILE_FOLDER/avnav.desktop "/usr/local/share/applications/"
 install -v -o 1000 -g 1000 -m 0644 /var/lib/avnav/avnav_server.xml "/var/lib/avnav/avnav_server.xml-orig"
 install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/avnav_server_lysmarine.xml "/var/lib/avnav/"
 install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/avnav_server_lysmarine.xml "/var/lib/avnav/avnav_server.xml"
+
+systemctl enable avnav
 
 apt-get clean
 npm cache clean --force
