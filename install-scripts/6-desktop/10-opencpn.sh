bbf6988d861acf88e41aef309368cbcaa2a33b36
mgrouch <mgrushinskiy@gmail.com>
2020-12-05 08:30:11 -0500

    fix

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -2,7 +2,7 @@
 
 apt-get install -y -q opencpn
 
-apt-get install -y -q opencpn-plugin-celestial opencpn-plugin-launcher opencpn-plugin-calculator
+apt-get install -y -q opencpn-plugin-celestial opencpn-plugin-launcher 
 
 install -o 1000 -g 1000 -d "/home/user/.opencpn"
 install -o 1000 -g 1000 -v $FILE_FOLDER/opencpn.conf "/home/user/.opencpn/"
