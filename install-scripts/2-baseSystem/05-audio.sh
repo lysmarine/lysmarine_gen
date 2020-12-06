3a5828ac1ebc5e237da8231755c3083b2a4b9168
mgrouch <mgrushinskiy@gmail.com>
2020-12-05 21:52:52 -0500

    audio for mopidy

--- install-scripts/2-baseSystem/05-audio.sh
+++ install-scripts/2-baseSystem/05-audio.sh
@@ -4,3 +4,5 @@ apt-get -y -q install pulseaudio
 usermod -a -G audio user
 
 echo "set-default-source alsa_output.platform-bcm2835_audio.analog-mono.2.monitor" >> /etc/pulse/default.pa
+echo "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1" >> /etc/pulse/default.pa
+
