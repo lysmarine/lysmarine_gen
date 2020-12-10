5b6d4fb0409a4c696c24f9a8e7447179796ed384
Frederic Guilbault <2@0464.ca>
2020-12-10 17:13:29 -0500

    Switch to lxpanel

--- install-scripts/6-desktop/3-panel.sh
+++ install-scripts/6-desktop/3-panel.sh
@@ -3,7 +3,7 @@ apt-get install -y lxpanel gsimplecal menu
 
 ## Install configuration files.
 install -d -o 1000 -g 1000 "/home/user/.config/lxpanel/lysmarine/panels/"
-install    -o 1000 -g 1000 $FILE_FOLDER/panel "/home/user/.config/lxpanel/lysmarine/panels/"
+install    -o 1000 -g 1000 -m 0644 $FILE_FOLDER/panel "/home/user/.config/lxpanel/lysmarine/panels/"
 
 ## Start fbpanel on openbox boot.
 install -d -o 1000 -g 1000 /home/user/.config/openbox
