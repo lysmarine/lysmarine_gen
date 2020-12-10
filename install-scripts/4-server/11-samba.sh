ff3a658b741daf40a84908c8ec4e4a6e67cd4c1b
mgrouch <mgrushinskiy@gmail.com>
2020-12-10 18:10:58 -0500

    samba for accessing widows network (fix)

--- install-scripts/4-server/11-samba.sh
+++ install-scripts/4-server/11-samba.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -e
 
+echo "samba-common samba-common/workgroup string WORKGROUP" | sudo debconf-set-selections
+echo "samba-common samba-common/dhcp boolean true" | sudo debconf-set-selections
+echo "samba-common samba-common/do_debconf boolean true" | sudo debconf-set-selections
+
 apt-get -y -q install samba samba-common
 
