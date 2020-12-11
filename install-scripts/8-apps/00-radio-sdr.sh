506a206316773a2dc15f92c6445700507bf6dd80
mgrouch <mgrushinskiy@gmail.com>
2020-12-11 07:59:33 -0500

    gqrx

--- install-scripts/8-apps/00-radio-sdr.sh
+++ install-scripts/8-apps/00-radio-sdr.sh
@@ -3,10 +3,10 @@
 apt-get clean
 
 apt-get -y -q install rtl-sdr rtl-ais
-#apt-get -y -q install gqrx-sdr
 apt-get -y -q install cubicsdr
 apt-get -y -q install multimon-ng netcat
 apt-get -y -q install fldigi
 apt-get -y -q install gpredict
+apt-get -y -q install gqrx-sdr
 
 apt-get clean
