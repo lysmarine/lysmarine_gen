fc6606cadda0b603f93ef7a755992342840ca493
Frederic Guilbault <2@0464.ca>
2020-12-13 17:40:40 -0500

    Use static theme file for onboard

--- install-scripts/6-desktop/2-onboard.sh
+++ install-scripts/6-desktop/2-onboard.sh
@@ -5,7 +5,8 @@ apt-get install -y onboard dconf-cli dbus-x11 at-spi2-core acpid
 install -d "/usr/share/onboard/"
 install -v $FILE_FOLDER/onboard.dconf "/usr/share/onboard/"
 install -v $FILE_FOLDER/a11y.dconf "/usr/share/onboard/"
-
+install -d "/usr/share/onboard/themes"
+install -v -m0644 $FILE_FOLDER/Lysmarine.theme "/usr/share/onboard/themes"
 ## Dconf injection on the first boot + self commenting sed replace.
 install -d -o 1000 -g 1000 /home/user/.config/openbox/
 echo "dconf load /org/onboard/ < /usr/share/onboard/onboard.dconf &" >> /home/user/.config/openbox/autostart
