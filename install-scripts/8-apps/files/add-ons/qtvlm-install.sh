b20a115bf60538077d83aaf6c9cdabd3f40e3b0f
mgrouch <mgrushinskiy@gmail.com>
2020-12-13 02:54:35 -0500

    qtVlm

---
+++ install-scripts/8-apps/files/add-ons/qtvlm-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+# See: https://www.meltemus.com
+
+curl https://www.meltemus.com/index.php/en/download/send/9-raspberrypi/281-qtvlm-5-9-14-p2-7 > qtVlm-5.9.14-p2-rpi.tar.gz
+gzip -cd < qtVlm-5.9.14-p2-rpi.tar.gz | tar xvf -
+curl https://raw.githubusercontent.com/bareboat-necessities/my-bareboat/master/qtvlm-conf/qtVlm.ini > /home/pi/.qtVlm/qtVlm.ini
+
+# cd qtVlm
+# ./qtVlm -platform xcb
+
