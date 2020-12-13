9c97d9c79ead64d0b8dc739b1438a62d0ef4f27f
mgrouch <mgrushinskiy@gmail.com>
2020-12-13 11:50:53 -0500

    more add ons

--- install-scripts/8-apps/files/add-ons/qtvlm-install.sh
+++ install-scripts/8-apps/files/add-ons/qtvlm-install.sh
@@ -7,5 +7,18 @@ curl https://www.meltemus.com/index.php/en/download/send/9-raspberrypi/281-qtvlm
 gzip -cd < qtVlm-5.9.14-p2-rpi.tar.gz | tar xvf -
 curl https://raw.githubusercontent.com/bareboat-necessities/my-bareboat/master/qtvlm-conf/qtVlm.ini > /home/user/.qtVlm/qtVlm.ini
 
+sudo bash -c 'cat << EOF > /usr/share/raspi-ui-overrides/applications/qtvlm.desktop
+[Desktop Entry]
+Type=Application
+Name=QtVlm
+GenericName=QtVlm
+Comment=QtVlm ChartPlotter
+Exec=sh -c "cd /home/user/qtVlm; ./qtVlm -platform xcb"
+Terminal=false
+Icon=/home/user/qtVlm/icon/qtVlm_48x48.png
+Categories=Navigation;ChartPlotter
+Keywords=Navigation;ChartPlotter
+EOF'
+
 # cd qtVlm
 # ./qtVlm -platform xcb
