b170f1d77accc731a803c95749d37f1f52d2d4b9
mgrouch <mgrushinskiy@gmail.com>
2020-12-14 11:15:55 -0500

    fix

--- install-scripts/8-apps/files/add-ons/wxtoimg-install.sh
+++ install-scripts/8-apps/files/add-ons/wxtoimg-install.sh
@@ -4,4 +4,7 @@
 
 cd ~
 curl https://wxtoimgrestored.xyz/beta/wxtoimg-armhf-2.11.2-beta.deb > wxtoimg-armhf-2.11.2-beta.deb
+
+sudo apt install libxft2:armhf libasound2:armhf
+
 sudo dpkg -i wxtoimg-armhf-2.11.2-beta.deb
