716a7a91a89bae7d975f6ebc85c9cdd80d5833f5
Frederic Guilbault <2@0464.ca>
2020-12-13 19:53:43 -0500

    change gtk theme

--- install-scripts/6-desktop/1-desktopTheming.sh
+++ install-scripts/6-desktop/1-desktopTheming.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apt-get install -y git adapta-gtk-theme
+apt-get install -y git arc-theme
 
 
 git clone -q --depth=1 --single-branch --branch Abyss-Desktop-Theme-Icons-and-Folders https://github.com/rtlewis88/rtl88-Themes.git
@@ -9,14 +9,12 @@ rm -rf rtl88-Themes
 
 
 install -d -o 1000 -g 1000 -m 755 "/home/user/.config/"
-install -d -o 1000 -g 1000 -m 755 "/home/user/.config/feh/"
 install -d -o 1000 -g 1000 -m 755 "/home/user/.config/gtk-3.0/"
 install -d -o 1000 -g 1000 -m 755 "/home/user/.config/openbox/"
 install -d -o 1000 -g 1000 -m 755 "/home/user/.config/pcmanfm/"
 install -d -o 1000 -g 1000 -m 755 "/home/user/.config/pcmanfm/default/"
 install -d -o 1000 -g 1000 -m 755 "/home/user/.config/libfm/"
-install -o 1000 -g 1000  -v $FILE_FOLDER/.gtkrc-2.0    "/home/user/"
-install -o 1000 -g 1000  -v $FILE_FOLDER/water.jpg     "/home/user/.config/feh/"
+#install -o 1000 -g 1000  -v $FILE_FOLDER/.gtkrc-2.0    "/home/user/"
 install -o 1000 -g 1000  -v $FILE_FOLDER/settings.ini  "/home/user/.config/gtk-3.0/"
 install -o 1000 -g 1000  -v $FILE_FOLDER/rc.xml        "/home/user/.config/openbox/"
 install -o 1000 -g 1000  -v $FILE_FOLDER/pcmanfm.conf  "/home/user/.config/pcmanfm/default/"
