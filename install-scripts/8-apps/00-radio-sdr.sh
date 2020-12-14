35e1008755b0ef02202acbe2921babc753f4b993
mgrouch <mgrushinskiy@gmail.com>
2020-12-14 16:03:32 -0500

    more apps

--- install-scripts/8-apps/00-radio-sdr.sh
+++ install-scripts/8-apps/00-radio-sdr.sh
@@ -2,13 +2,26 @@
 
 apt-get clean
 
-apt-get -y -q install rtl-sdr rtl-ais
-apt-get -y -q install cubicsdr
 apt-get -y -q install multimon-ng netcat
+apt-get -y -q install cubicsdr
 apt-get -y -q install fldigi
 apt-get -y -q install gpredict
-apt-get -y -q install gqrx-sdr
 apt-get -y -q install previsat
+apt-get -y -q install gqrx-sdr
+apt-get -y -q install rtl-sdr rtl-ais
 apt-get -y -q install gnss-sdr
+apt-get -y -q install gnuradio
+
+apt-get -y -q install python3-wxgtk4.0 \
+    libfftw3-dev                       \
+    libasound2-dev                     \
+    portaudio19-dev                    \
+    libpulse-dev                       \
+    python3-dev                        \
+    libpython3-dev                     \
+    python3-usb                        \
+    python3-setuptools                 \
+    python3-pip
+python3 -m pip install --upgrade quisk
 
 apt-get clean
