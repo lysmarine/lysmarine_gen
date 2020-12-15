50619411c5e7ad7e2e7b1c5363d029128b303e42
mgrouch <mgrushinskiy@gmail.com>
2020-12-15 12:59:39 -0500

    anbox

--- install-scripts/8-apps/09-more.sh
+++ install-scripts/8-apps/09-more.sh
@@ -4,7 +4,7 @@ sudo apt-get -q -y install i2c-tools traceroute telnet socat gdal-bin openvpn
 
 sudo apt-get -q -y install dconf-editor gedit gnome-weather gnome-chess openpref
 
-sudo apt-get -q -y install arduino
+#sudo apt-get -q -y install arduino
 
 install -d -o 1000 -g 1000 -m 0755 "/home/user/add-ons"
 install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/add-ons/readme.txt "/home/user/add-ons/"
