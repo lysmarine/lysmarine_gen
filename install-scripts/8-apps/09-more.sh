9358e30acdecd6ebf557a24123fc6ecb74bb478d
mgrouch <mgrushinskiy@gmail.com>
2020-12-15 12:00:35 -0500

    more apps

--- install-scripts/8-apps/09-more.sh
+++ install-scripts/8-apps/09-more.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-sudo apt-get -q -y install i2c-tools traceroute telnet socat gdal-bin
+sudo apt-get -q -y install i2c-tools traceroute telnet socat gdal-bin openvpn
 
 sudo apt-get -q -y install dconf-editor gedit gnome-weather gnome-chess openpref
 
@@ -9,9 +9,12 @@ sudo apt-get -q -y install arduino
 install -d -o 1000 -g 1000 -m 0755 "/home/user/add-ons"
 install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/add-ons/readme.txt "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/deskpi-pro-install.sh "/home/user/add-ons/"
-#install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/qtvlm-install.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/text-to-speech-install.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/text-to-speech-sample.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/wxtoimg-install.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/sdrglut-install.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/pactor-install.sh "/home/user/add-ons/"
+
+if [ "$LMARCH" == 'armhf' ]; then
+  install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/qtvlm-install.sh "/home/user/add-ons/"
+fi
