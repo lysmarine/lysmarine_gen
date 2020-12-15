7868ee671907e56bef97efdb1ea270d141f7b6cb
mgrouch <mgrushinskiy@gmail.com>
2020-12-15 14:31:05 -0500

    anbox

--- install-scripts/8-apps/files/add-ons/anbox-android-img-install.sh
+++ install-scripts/8-apps/files/add-ons/anbox-android-img-install.sh
@@ -3,7 +3,8 @@
 sudo bash -c 'curl https://iot.bzh/download/public/2020/CESDemo/Anbox/Android_images/android_arm64.img > /var/lib/anbox/android.img'
 
 sudo systemctl enable anbox-container-manager.service
-sudo systemctl enable anbox-session-manager.service
-
 sudo systemctl start anbox-container-manager.service
-sudo systemctl start anbox-session-manager.service
+
+# Running:
+# anbox session-manager &
+# anbox launch --package=org.anbox.appmgr --component=org.anbox.appmgr.AppViewActivity
