b44ddf4f9e75441334d905686367b20b51402ef3
mgrouch <mgrushinskiy@gmail.com>
2020-12-14 19:55:51 -0500

    more apps

--- install-scripts/8-apps/09-more.sh
+++ install-scripts/8-apps/09-more.sh
@@ -13,6 +13,5 @@ install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/qtvlm-install.sh "/home/
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/text-to-speech-install.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/text-to-speech-sample.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/wxtoimg-install.sh "/home/user/add-ons/"
-install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/noaa-apt-install.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/sdrglut-install.sh "/home/user/add-ons/"
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/pactor-install.sh "/home/user/add-ons/"
--- install-scripts/8-apps/files/add-ons/noaa-apt-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# See: https://github.com/martinber/noaa-apt
-
-mkdir ~/noaa-apt
-cd ~/noaa-apt
-wget https://github.com/martinber/noaa-apt/releases/download/v1.3.0/noaa-apt-1.3.0-armv7-linux-gnueabihf.zip
-unzip noaa-apt-1.3.0-armv7-linux-gnueabihf.zip
