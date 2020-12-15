66575a116331597483f7dbee87e99740325baa4e
mgrouch <mgrushinskiy@gmail.com>
2020-12-14 19:10:45 -0500

    more apps

--- install-scripts/8-apps/00-radio-sdr.sh
+++ install-scripts/8-apps/00-radio-sdr.sh
@@ -16,6 +16,7 @@ apt-get -y -q install gnuaisgui
 
 install -v $FILE_FOLDER/gnuaisgui.desktop /usr/local/share/applications/
 
+# quisk
 apt-get -y -q install python3-wxgtk4.0 \
     libfftw3-dev                       \
     libasound2-dev                     \
@@ -29,5 +30,22 @@ apt-get -y -q install python3-wxgtk4.0 \
 python3 -m pip install --upgrade quisk
 # To run quisk
 # python3 -m quisk
+
+
+
+install -v $FILE_FOLDER/jnx.desktop /usr/local/share/applications/
+install -v $FILE_FOLDER/jwx.desktop /usr/local/share/applications/
+
+install -d -m 755 "/usr/local/share/jnx"
+install -d -m 755 "/usr/local/share/jwx"
+
+curl https://arachnoid.com/JNX/JNX.jar > /usr/local/share/jnx/JNX.jar
+curl https://arachnoid.com/JNX/JNX_source.tar.gz > /usr/local/share/jnx/JNX_source.tar.gz
+
+curl https://arachnoid.com/JWX/resources/JWX.jar > /usr/local/share/jwx/JWX.jar
+curl https://arachnoid.com/JWX/resources/JWX_source.tar.bz2 > /usr/local/share/jwx/JWX_source.tar.bz2
+
+apt-get -y -q install default-jdk
+apt-get -y -q install hamfax
 
 apt-get clean
