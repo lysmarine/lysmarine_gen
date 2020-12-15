6e454f4422507e7bdf5775f38a6c34e5f19f0a9f
mgrouch <mgrushinskiy@gmail.com>
2020-12-15 13:33:02 -0500

    anbox

--- install-scripts/8-apps/10-anbox.sh
+++ install-scripts/8-apps/10-anbox.sh
@@ -9,7 +9,7 @@ git clone https://github.com/anbox/anbox-modules.git
 
 pushd anbox-modules
   install -v -m 0644 anbox.conf "/etc/modules-load.d/"
-  install -v -m 0644  99-anbox.rules "/lib/udev/rules.d/"
+  install -v -m 0644 99-anbox.rules "/lib/udev/rules.d/"
   cp -rT ashmem /usr/src/anbox-ashmem-1
   cp -rT binder /usr/src/anbox-binder-1
   dkms install anbox-ashmem/1
