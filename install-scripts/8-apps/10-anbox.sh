1dce36fe9ca535763891157218b119ea18caa2be
mgrouch <mgrushinskiy@gmail.com>
2020-12-15 17:34:40 -0500

    anbox

--- install-scripts/8-apps/10-anbox.sh
+++ install-scripts/8-apps/10-anbox.sh
@@ -1,28 +1,3 @@
 #!/bin/bash -e
 
-apt-get -q -y install android-tools-adb git curl unzip
-apt-get -q -y install dkms
-apt-get -q -y install raspberrypi-kernel-headers
-
-cd ~
-git clone https://github.com/anbox/anbox-modules.git
-
-pushd anbox-modules
-  install -v -m 0644 anbox.conf "/etc/modules-load.d/"
-  install -v -m 0644 99-anbox.rules "/lib/udev/rules.d/"
-  cp -rT ashmem /usr/src/anbox-ashmem-1
-  cp -rT binder /usr/src/anbox-binder-1
-  dkms install anbox-ashmem/1
-  dkms install anbox-binder/1
-popd
-
-apt-get -q -y install anbox
-
-#apt-get -q -y install software-properties-common
-
-#install -m0644 -v $FILE_FOLDER/anbox.list "/etc/apt/sources.list.d/"
-#apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 21C6044A875B67B7 # anbox
-
-#apt-get -q -y install raspberrypi-kernel-headers anbox-modules-dkms
-
 install -v -o 1000 -g 1000 -m 0755 $FILE_FOLDER/add-ons/anbox-android-img-install.sh "/home/user/add-ons/"
--- install-scripts/8-apps/files/add-ons/anbox-android-img-install.sh
+++ install-scripts/8-apps/files/add-ons/anbox-android-img-install.sh
@@ -1,9 +1,37 @@
 #!/bin/bash -e
 
+sudo apt-get -q -y install android-tools-adb git curl unzip
+sudo apt-get -q -y install dkms
+sudo apt-get -q -y install raspberrypi-kernel-headers
+
+cd ~
+git clone https://github.com/anbox/anbox-modules.git
+
+pushd anbox-modules
+  sudo install -v -m 0644 anbox.conf "/etc/modules-load.d/"
+  sudo install -v -m 0644 99-anbox.rules "/lib/udev/rules.d/"
+  sudo cp -rT ashmem /usr/src/anbox-ashmem-1
+  sudo cp -rT binder /usr/src/anbox-binder-1
+  sudo dkms install anbox-ashmem/1
+  sudo dkms install anbox-binder/1
+popd
+
+sudo apt-get -q -y install anbox
+
+#sudo apt-get -q -y install software-properties-common
+#sudo install -m0644 -v $FILE_FOLDER/anbox.list "/etc/apt/sources.list.d/"
+#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 21C6044A875B67B7 # anbox
+#sudo apt-get -q -y install raspberrypi-kernel-headers anbox-modules-dkms
+
 sudo bash -c 'curl https://iot.bzh/download/public/2020/CESDemo/Anbox/Android_images/android_arm64.img > /var/lib/anbox/android.img'
 
 sudo systemctl enable anbox-container-manager.service
 sudo systemctl start anbox-container-manager.service
+
+sudo adb start-server
+
+anbox wait-ready
+
 
 # See also: https://github.com/anbox/anbox/issues/1214
 
