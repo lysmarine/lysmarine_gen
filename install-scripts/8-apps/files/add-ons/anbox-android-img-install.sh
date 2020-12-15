dac0b4bcdda9c04509d3a94d1ee9311cfd4d3cb4
mgrouch <mgrushinskiy@gmail.com>
2020-12-15 15:38:35 -0500

    anbox

--- install-scripts/8-apps/files/add-ons/anbox-android-img-install.sh
+++ install-scripts/8-apps/files/add-ons/anbox-android-img-install.sh
@@ -5,6 +5,8 @@ sudo bash -c 'curl https://iot.bzh/download/public/2020/CESDemo/Anbox/Android_im
 sudo systemctl enable anbox-container-manager.service
 sudo systemctl start anbox-container-manager.service
 
+# See also: https://github.com/anbox/anbox/issues/1214
+
 # Running:
 # ANBOX_LOG_LEVEL=debug anbox session-manager --gles-driver=translator --single-window --window-size=1024,768 &
 # ANBOX_LOG_LEVEL=debug anbox launch --package=org.anbox.appmgr --component=org.anbox.appmgr.AppViewActivity
