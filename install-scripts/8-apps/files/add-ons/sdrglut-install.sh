1e7ca8b309ec3bf32803161ca90e02f82601ac7e
mgrouch <mgrushinskiy@gmail.com>
2020-12-15 19:29:04 -0500

    fixes

--- install-scripts/8-apps/files/add-ons/sdrglut-install.sh
+++ install-scripts/8-apps/files/add-ons/sdrglut-install.sh
@@ -6,6 +6,6 @@ sudo apt-get -y install build-essential libsoapysdr0.6 libsoapysdr-dev libopenal
  libliquid-dev freeglut3 freeglut3-dev libalut0 libalut-dev librtaudio-dev
 sudo apt-get -y install git
 cd /home/user
-git clone https://github.com/righthalfplane/SdrGlut.git
+git clone https://github.com/bareboat-necessities/SdrGlut
 cd SdrGlut
 make -f makefileRaspbian
