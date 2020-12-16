5c455c7a28c713aa325076c6fe1594ffaa421d99
mgrouch <mgrushinskiy@gmail.com>
2020-12-15 20:13:20 -0500

    more apps

---
+++ install-scripts/8-apps/files/add-ons/nmea-sleuth-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+
+# Dashboard and analysis tool for NMEA data streams from Serial, TCP/IP and File data sources for Chrome browser
+
+chromium https://chrome.google.com/webstore/detail/nmea-sleuth/pkikkfglomloligndkaibecgapljgjok/related?hl=en
+
+# click "add"
