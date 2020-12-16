eeed95780ffaa9011f2e7505b3c9b806aa5bf2cd
mgrouch <mgrushinskiy@gmail.com>
2020-12-16 10:56:08 -0500

    more apps

---
+++ install-scripts/8-apps/files/add-ons/tuktuk-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+# See: https://github.com/vokkim/tuktuk-chart-plotter
+
+#
+# You can install TukTuk chartplotter from inside SignalK AppStore. You would also need to install Charts SignalK plugin.
+#
+
+chromium https://github.com/vokkim/tuktuk-chart-plotter
