3a143ecbd675f666c317d855d37ae301de61fb00
mgrouch <mgrushinskiy@gmail.com>
2020-12-19 11:58:10 -0500

    fixes

--- install-scripts/0-repositories/00-addRepo.sh
+++ install-scripts/0-repositories/00-addRepo.sh
@@ -13,7 +13,7 @@ install -m 0644 -v $FILE_FOLDER/bbn-kplex.list "/etc/apt/sources.list.d/"
 install -m 0644 -v $FILE_FOLDER/bbn-noaa-apt.list "/etc/apt/sources.list.d/"
 #install -m 0644 -v $FILE_FOLDER/bbn-fbpanel.list "/etc/apt/sources.list.d/"
 install -m 0644 -v $FILE_FOLDER/mopidy.list "/etc/apt/sources.list.d/"
-#install -m 0644 -v $FILE_FOLDER/avnav.list "/etc/apt/sources.list.d/"
+install -m 0644 -v $FILE_FOLDER/avnav.list "/etc/apt/sources.list.d/"
 install -m 0644 -v $FILE_FOLDER/mosquitto.list "/etc/apt/sources.list.d/"
 
 ## Prefer opencpn PPA to free-x (for mainly for the opencpn package)
@@ -32,7 +32,7 @@ wget -q -O - https://dl.cloudsmith.io/public/bbn-projects/bbn-kplex/cfg/gpg/gpg.
 wget -q -O - https://dl.cloudsmith.io/public/bbn-projects/bbn-noaa-apt/cfg/gpg/gpg.DB5121F72251E833.key | apt-key add -
 #wget -q -O - https://dl.cloudsmith.io/public/bbn-projects/bbn-fbpanel/cfg/gpg/gpg.89DE2CF06C6908DA.key | apt-key add -
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
-#curl https://open-mind.space/repo/open-mind.space.gpg.key | apt-key add -     # AvNav
+curl https://open-mind.space/repo/open-mind.space.gpg.key | apt-key add -     # AvNav
 wget -q -O - https://repo.mosquitto.org/debian/mosquitto-repo.gpg.key | apt-key add - # Mosquitto
 
 ## Update && Upgrade
