e73a036805a2e8bd9f7d8ed642a5145857f4e07b
mgrouch <mgrushinskiy@gmail.com>
2020-12-21 14:09:58 -0500

    more app

--- install-scripts/0-repositories/00-addRepo.sh
+++ install-scripts/0-repositories/00-addRepo.sh
@@ -14,6 +14,8 @@ install -m 0644 -v $FILE_FOLDER/bbn-noaa-apt.list "/etc/apt/sources.list.d/"
 install -m 0644 -v $FILE_FOLDER/mopidy.list "/etc/apt/sources.list.d/"
 install -m 0644 -v $FILE_FOLDER/avnav.list "/etc/apt/sources.list.d/"
 install -m 0644 -v $FILE_FOLDER/mosquitto.list "/etc/apt/sources.list.d/"
+install -m 0644 -v $FILE_FOLDER/influxdb.list "/etc/apt/sources.list.d/"
+install -m 0644 -v $FILE_FOLDER/grafana.list "/etc/apt/sources.list.d/"
 
 ## Prefer opencpn PPA to free-x (for mainly for the opencpn package)
 install -m 0644 -v $FILE_FOLDER/50-lysmarine.pref "/etc/apt/preferences.d/"
@@ -32,6 +34,8 @@ wget -q -O - https://dl.cloudsmith.io/public/bbn-projects/bbn-noaa-apt/cfg/gpg/g
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
 curl https://open-mind.space/repo/open-mind.space.gpg.key | apt-key add -     # AvNav
 wget -q -O - https://repo.mosquitto.org/debian/mosquitto-repo.gpg.key | apt-key add - # Mosquitto
+wget -q -O - https://repos.influxdata.com/influxdb.key | apt-key add -
+wget -q -O - https://packages.grafana.com/gpg.key | apt-key add -
 
 ## Update && Upgrade
 apt-get update  -y -q
---
+++ install-scripts/4-server/14-grafana.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+apt-get clean
+
+apt-get -y -q install grafana
+
+apt-get clean
+
