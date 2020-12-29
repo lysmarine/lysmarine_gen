81738cd48ce8366ae1198de1d4ea8e873a487f24
mgrouch <mgrushinskiy@gmail.com>
2020-12-28 19:09:47 -0500

    config

--- install-scripts/8-apps/01-stellarium.sh
+++ install-scripts/8-apps/01-stellarium.sh
@@ -4,4 +4,7 @@ apt-get clean
 
 apt-get -y -q install stellarium
 
+install -d -o 1000 -g 1000 -m 0755 "/home/user/.stellarium"
+install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/stellarium-config.ini "/home/user/.stellarium"
+
 apt-get clean
