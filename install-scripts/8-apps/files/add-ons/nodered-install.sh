f6bdf5a84f181452d98ffb39086d49886574e287
mgrouch <mgrushinskiy@gmail.com>
2020-12-29 10:56:27 -0500

    nodered

---
+++ install-scripts/8-apps/files/add-ons/nodered-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+# See: https://nodered.org/docs/getting-started/raspberrypi
+
+bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
