c0afb861bd21d176504e06c4a5d947777ae9b5b6
mgrouch <mgrushinskiy@gmail.com>
2020-12-30 04:34:29 -0500

    -xrdp

--- install-scripts/6-desktop/08-remoteDesktopAccess.sh
+++ install-scripts/6-desktop/08-remoteDesktopAccess.sh
@@ -17,3 +17,6 @@ install -d /etc/systemd/system/xrdp.d
 
 apt-get install -y -q xrdp
 install -v $FILE_FOLDER/Xwrapper.config "/etc/X11/Xwrapper.config"
+
+systemctl disable xrdp
+systemctl disable xrdp-sesman
--- install-scripts/6-desktop/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-install -d /etc/systemd/system/xrdp.d
-
-apt-get install -y -q xrdp
-install -v $FILE_FOLDER/Xwrapper.config "/etc/X11/Xwrapper.config"
