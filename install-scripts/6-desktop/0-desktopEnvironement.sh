e3ba55b7e58b17906e54f58b27d9229eacdfaf84
Frederic Guilbault <2@0464.ca>
2021-01-26 00:55:07 -0500

    switch to awesome

--- install-scripts/6-desktop/0-desktopEnvironement.sh
+++ install-scripts/6-desktop/0-desktopEnvironement.sh
@@ -11,23 +11,33 @@ if [ $LMOS == Armbian ] ;then
 	sudo apt-get -q -y install xserver-xorg-legacy	
 fi
 
+#apt-get install -yq \
+#gstreamer1.0-x gstreamer1.0-omx gstreamer1.0-plugins-base \
+#gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-alsa \
+#gstreamer1.0-libav alsa-utils libavahi-compat-libdnssd-dev git openbox \
+#xbacklight lxappearance xsettingsd xserver-xorg \
+#xinit libgtk2-perl pavucontrol cpanminus perl-base \
+#dialog lxterminal network-manager-gnome  servicemanager \
+#chromium pcmanfm lxterminal mousepad gpsbabel
+
 apt-get install -yq \
-gstreamer1.0-x gstreamer1.0-omx gstreamer1.0-plugins-base \
-gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-alsa \
-gstreamer1.0-libav alsa-utils libavahi-compat-libdnssd-dev git openbox \
-xbacklight lxappearance xsettingsd xserver-xorg \
-xinit libgtk2-perl pavucontrol cpanminus perl-base \
-dialog lxterminal network-manager-gnome evdev-rce servicemanager \
-chromium pcmanfm lxterminal mousepad gpsbabel file-roller
+	xserver-xorg evdev-rce awesome \
+ 	servicemanager pavucontrol network-manager-gnome\
+	chromium sakura mousepad file-roller pcmanfm \
 
-## Openbox
+## DE config files
 install -d -o 1000 -g 1000 /home/user/.local/share/
-install -d -o 1000 -g 1000 "/home/user/.config/openbox"
-install -o 1000 -g 1000  -v $FILE_FOLDER/autostart     "/home/user/.config/openbox/"
 
+install -d -o 1000 -g 1000 "/home/user/.config/"
+
+cp -r $FILE_FOLDER/awesome "/home/user/.config/"
+chown -R 1000:1000 "/home/user/.config/"
 
 ## Make some room for the rest of the build script
 apt-get clean
 
-## Force polkit agent to start with openbox (this is needed for nm-applet hotspot)
+## Force polkit agent to start with openbox (this is needed for nm-applet hotspot configuration)
 sed -i '/^OnlyShowIn=/ s/$/GNOME;/' /etc/xdg/autostart/polkit-gnome-authentication-agent-1.desktop
+
+echo XDG_CURRENT_DESKTOP=Unity >> /etc/environment
+echo QT_QPA_PLATFORMTHEME=gtk2 >> /etc/environment
