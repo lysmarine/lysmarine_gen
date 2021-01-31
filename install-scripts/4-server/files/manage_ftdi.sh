82e1d532687a6c4c188a278ac559f807f9038bb8
mgrouch <mgrushinskiy@gmail.com>
2021-01-31 12:53:12 -0500

    FTDI

---
+++ install-scripts/4-server/files/manage_ftdi.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+{
+  chmod a+r /dev/ttyLYS_ftdi_"$1"
+  if [[ $2 == "remove" ]] ; then
+    logger "The USB device /dev/ttyLYS_ftdi_$1 has been disconnected"
+  else
+    logger "This USB device is known as FTDI and will be linked on /dev/ttyLYS_ftdi_$1"
+    systemctl restart kplex.service
+    sleep 1
+    systemctl restart signalk
+  fi
+}&
