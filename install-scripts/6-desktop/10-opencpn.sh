bcd1be6e67242a383ce125e98f89e334bae0211a
mgrouch <mgrushinskiy@gmail.com>
2021-02-06 22:26:48 -0500

    chart plugins

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -6,3 +6,17 @@ apt-get install -y -q opencpn-plugin-celestial opencpn-plugin-launcher
 
 install -o 1000 -g 1000 -d "/home/user/.opencpn"
 install -o 1000 -g 1000 -v $FILE_FOLDER/opencpn.conf "/home/user/.opencpn/"
+
+apt-get install -y -q oernc-pi
+
+mkdir oe-tmp
+cd oe-tmp
+wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/oesenc-ubuntu-arm64-18.04-tarball/versions/4.2.18.2.0.c89a5c4/oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz
+gzip -cd oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
+cd oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04
+cp -r /usr /
+cp -r /etc /
+cd ../../
+rm -rf oe-tmp
+
+
