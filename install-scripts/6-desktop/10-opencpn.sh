bea47b1d62593a6735a9aa939c32bf04f7dba257
mgrouch <mgrushinskiy@gmail.com>
2021-02-06 22:42:11 -0500

    chart plugins

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -19,4 +19,11 @@ cp -r /etc /
 cd ../../
 rm -rf oe-tmp
 
-
+mkdir s63-tmp
+cd s63-tmp
+wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/s63-ubuntu-arm64-18.04-tarball/versions/1.17.1.0.bcaad82/s63_pi-1.17.1-0_ubuntu-arm64-18.04.tar.gz
+gzip -cd s63_pi-1.17.1-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
+cd s63_pi-1.17.1-0_ubuntu-arm64-18.04
+cp -r /usr /
+cd ../../
+rm -rf s63-tmp
