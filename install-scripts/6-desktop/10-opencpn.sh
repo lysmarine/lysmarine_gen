46ee5bb1ddba22218b43bdff671082485ed4dc7e
mgrouch <mgrushinskiy@gmail.com>
2021-02-07 03:26:34 -0500

    chart plugins

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -14,8 +14,8 @@ cd oe-tmp
 wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/oesenc-ubuntu-arm64-18.04-tarball/versions/4.2.18.2.0.c89a5c4/oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz
 gzip -cd oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
 cd oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04
-cp -r /usr /
-cp -r /etc /
+cp -r usr/ /
+cp -r etc/ /
 cd ../../
 rm -rf oe-tmp
 
@@ -24,6 +24,6 @@ cd s63-tmp
 wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/s63-ubuntu-arm64-18.04-tarball/versions/1.17.1.0.bcaad82/s63_pi-1.17.1-0_ubuntu-arm64-18.04.tar.gz
 gzip -cd s63_pi-1.17.1-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
 cd s63_pi-1.17.1-0_ubuntu-arm64-18.04
-cp -r /usr /
+cp -r usr/ /
 cd ../../
 rm -rf s63-tmp
