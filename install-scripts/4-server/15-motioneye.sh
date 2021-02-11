f07aadb8b9a24f58080b489818878131ac72e1a1
mgrouch <mgrushinskiy@gmail.com>
2021-02-11 14:43:07 -0500

    motioneye

--- install-scripts/4-server/15-motioneye.sh
+++ install-scripts/4-server/15-motioneye.sh
@@ -8,4 +8,14 @@ apt-get -y -q install motion
 
 python3 -m pip install --upgrade motioneye
 
+mkdir -p /etc/motioneye
+cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
+
+mkdir -p /var/lib/motioneye
+
+cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
+#systemctl daemon-reload
+systemctl enable motioneye
+#systemctl start motioneye
+
 apt-get clean
