bb4b9e1b595b9bcd04dc9fef518d43432b16e3cc
mgrouch <mgrushinskiy@gmail.com>
2021-02-12 16:49:03 -0500

    empathy

--- cross-build-release/lib.sh
+++ cross-build-release/lib.sh
@@ -92,8 +92,8 @@ inflateImage() {
     log "Inflating OS image to have enough space to build lysmarine. "
     cp -fv ${imageLocation} $imageLocationInflated
 
-    log "truncate image to 8640M"
-    truncate -s "8640M" $imageLocationInflated
+    log "truncate image to 8168M"
+    truncate -s "8168M" $imageLocationInflated
 
     log "resize last partition to 100%"
     partQty=$(fdisk -l $imageLocationInflated | grep -o "^$imageLocationInflated" | wc -l)
