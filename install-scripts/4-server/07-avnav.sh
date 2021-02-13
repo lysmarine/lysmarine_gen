a7cef96b744461eb1210d18973a7d868eb51cf8f
free-x <oroitburd@gmail.com>
2021-02-13 13:12:48 +0100

    Fix systemd overrides for AvNav

--- install-scripts/4-server/07-avnav.sh
+++ install-scripts/4-server/07-avnav.sh
@@ -10,6 +10,7 @@ apt-get -q -y -o Dpkg::Options::="--force-overwrite" install avnav-oesenc
 
 apt-get -q -y install avnav-mapproxy-plugin
 
+install -o 0 -g 0 -d /usr/lib/systemd/system/avnav.service.d
 install -o 0 -g 0 -m 0644 $FILE_FOLDER/lys-avnav.conf /usr/lib/systemd/system/avnav.service.d/
 install -o 0 -g 0 -d /usr/lib/avnav/lysmarine
 install -o 0 -g 0 -m 0644 $FILE_FOLDER/avnav_server_lysmarine.xml "/usr/lib/avnav/lysmarine/" 
