9aac2fcd0e4516d63ca8cd510e2cd52ab6342d44
Frederic Guilbault <2@0464.ca>
2021-02-12 23:35:25 -0500

    improve cleanup

--- install-scripts/8-cleanup/0-cleanup.sh
+++ install-scripts/8-cleanup/0-cleanup.sh
@@ -2,6 +2,8 @@
 rm -rf  /tmp/empty-cache46
 rm -rvf /home/user/Public /home/user/Templates /home/user/Videos /home/user/Desktop
 
+## Remove unused package
+apt-get purge --auto-remove -yq gnome-keyring # chromium and others install a keyring service but we don't need it
 apt-get -yq remove --auto-remove khmerconverter  || true
 apt-get -yq remove --auto-remove mlterm || true
 apt-get -yq remove --auto-remove debian-reference-common || true
@@ -9,18 +11,16 @@ apt-get -yq remove --auto-remove greybird-gtk-theme  || true
 apt-get -yq remove --auto-remove murrine-themes || true
 apt-get -yq remove --auto-remove rpd-icons || true
 apt-get -yq remove --auto-remove yelp || true
-
+apt-get -yq remove --auto-remove gnome-icon-theme || true
 apt-get -y autoremove
 apt-get clean
 
-## Menu cleanup 
-rm -fv /usr/share/applications/vim.desktop
-rm -fv /usr/share/applications/PyCrust.desktop
-rm -fv /usr/share/applications/XRCed.desktop
-rm -fv /usr/share/applications/yad-icon-browser.desktop
-rm -fv /usr/share/applications/x11vnc.desktop
-rm -fv /usr/share/applications/org.gnome.FileRoller.desktop
-rm -fv /usr/share/applications/mopidy.desktop
-rm -fv /usr/share/applications/mlterm.desktop
+## clean cache of others package managers
+# pip3 cache purge   # require pip version > 20.1
+rm -rf /root/.cache/pip
+npm cache clean --force
+su signalk -c "npm cache clean --force"
 
-#rm -fv /usr/local/share/applications/opencpn.desktop #for amd64 that need a manual builds
+## Remove others cache
+rm -rf /root/.cache/electron
+rm -rf /root/.wget*
