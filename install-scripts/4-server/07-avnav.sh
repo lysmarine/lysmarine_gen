1f60084d4e366174251b0d6e7b8229f5a3959008
free-x <oroitburd@gmail.com>
2021-02-13 13:08:02 +0100

    2. Step: AvNav better integration. systemd

--- install-scripts/4-server/07-avnav.sh
+++ install-scripts/4-server/07-avnav.sh
@@ -2,11 +2,7 @@
 
 apt-get clean
 
-install -o 1000 -g 1000 -d /home/user/AvNavCharts
-install -o 1000 -g 1000 -d /home/user/AvNavCharts/out
-install -o 1000 -g 1000 -d /var/lib/avnav
-
-apt-get -q -y --no-install-recommends install avnav xterm mpg123
+apt-get -q -y --no-install-recommends install avnav mpg123
 
 apt-get -q -y install avnav-history-plugin avnav-ocharts-plugin # avnav-raspi
 
@@ -14,16 +10,13 @@ apt-get -q -y -o Dpkg::Options::="--force-overwrite" install avnav-oesenc
 
 apt-get -q -y install avnav-mapproxy-plugin
 
-adduser avnav audio
-
-usermod -a -G charts avnav
-
-install -m 0644 $FILE_FOLDER/avnav_server_lysmarine.xml "/var/lib/avnav/avnav_server_lysmarine.xml"
-install -m 0644 $FILE_FOLDER/avnav_server_lysmarine.xml "/var/lib/avnav/avnav_server.xml"
+install -o 0 -g 0 -m 0644 $FILE_FOLDER/lys-avnav.conf /usr/lib/systemd/system/avnav.service.d/
+install -o 0 -g 0 -d /usr/lib/avnav/lysmarine
+install -o 0 -g 0 -m 0644 $FILE_FOLDER/avnav_server_lysmarine.xml "/usr/lib/avnav/lysmarine/" 
 
 systemctl enable avnav
 
-install -d -o avnav -g avnav /home/avnav
+#install -d -o avnav -g avnav /home/avnav
 
 apt-get clean
 npm cache clean --force
