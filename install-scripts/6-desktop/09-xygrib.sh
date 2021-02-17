cacd4886e9249c0356e59eef7b21a90b3c3fea24
mgrouch <mgrushinskiy@gmail.com>
2021-02-16 23:01:55 -0500

    grib link

--- install-scripts/6-desktop/09-xygrib.sh
+++ install-scripts/6-desktop/09-xygrib.sh
@@ -4,3 +4,8 @@ apt-get install -y -q xygrib
 
 ## Provide an alternative more meaningful icon for xygrib
 install -o 1000 -g 1000 $FILE_FOLDER/icons/logo_grib.png /usr/share/icons/
+
+pushd /home/user
+  ln -s .xygrib/grib Grib
+  chown user:user Grib
+popd
