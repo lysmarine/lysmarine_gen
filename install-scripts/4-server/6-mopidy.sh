fce266281084d2f7aabbe502277760f6c3f01395
Frederic Guilbault <2@0464.ca>
2021-02-22 08:11:30 -0500

    code refactoring on mopidy stage

--- install-scripts/4-server/6-mopidy.sh
+++ install-scripts/4-server/6-mopidy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -e
-# Strongly Inspired by https://github.com/bareboat-necessities/lysmarine_gen/blob/master/install-scripts/4-server/06-mopidy.sh
 
-apt-get -yq --no-install-recommends install mopidy mopidy-mpd mopidy-tunein xdotool python3-pip
+# Install Mopidy and all dependencies:
+#apt-get -y install mopidy mopidy-mpd mopidy-spotify mopidy-tunein libspotify-dev xdotool
+
+apt-get -yq --no-install-recommends install mopidy mopidy-mpd mopidy-tunein
+
 usermod -a -G video mopidy
 
+# Install Mopidy MusicBox Web Client:
 pip3 install systems Mopidy-MusicBox-Webclient
+
+# Install YouTube support
+apt-get -y install gstreamer1.0-plugins-bad
+pip3 install https://github.com/natumbri/mopidy-youtube/archive/develop.zip
+pip3 install --upgrade requests
+
+# Enable Mopidy service
+systemctl disable mopidy
 
 install -m 644 $FILE_FOLDER/mopidy.conf "/etc/mopidy/"
 
