5ccfdc144040169619bbff9cfe84fdc715a7b34b
mgrouch <mgrushinskiy@gmail.com>
2021-03-05 15:35:56 -0500

    fix

--- install-scripts/8-apps/13-docs.sh
+++ install-scripts/8-apps/13-docs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
+install -d -o 1000 -g 1000 /home/user/Documents/
+
 wget -q -O - https://bareboat-necessities.github.io/my-bareboat/bareboat-os.pdf > /home/user/Documents/bareboat-os.pdf
 #wget -q -O - https://bareboat-necessities.github.io/my-bareboat/index.pdf > /home/user/Documents/my-bareboat.pdf
