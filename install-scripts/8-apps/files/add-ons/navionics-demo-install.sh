d6e31ac60ea6beb57cb5800811d988c0965c16c7
mgrouch <mgrushinskiy@gmail.com>
2021-03-05 02:36:38 -0500

    more add-ons

--- install-scripts/8-apps/files/add-ons/navionics-demo-install.sh
+++ install-scripts/8-apps/files/add-ons/navionics-demo-install.sh
@@ -1,13 +1,28 @@
 #!/bin/bash -e
 
-sudo bash -c 'cat << EOF > /usr/local/share/applications/navionics-demo.desktop
-[Desktop Entry]
-Type=Application
-Name=Navionics Demo
-GenericName=Navionics Demo
-Comment=Navionics Demo
-Exec=chromium https://webapp.navionics.com/
-Terminal=false
-Icon=gnome-globe
-Categories=System;GPS;Geography;Navigation;Chart;
-Keywords=GPS;Navigation;Chart;'
+echo "I agree to Navionics Terms and Conditions viewable at https://www.navionics.com/usa/legal/terms-of-use"
+read -r -p "Accept? [Y/n] " input
+
+case $input in
+ [yY][eE][sS]|[yY])
+    sudo bash -c 'cat << EOF > /usr/local/share/applications/navionics-demo.desktop
+    [Desktop Entry]
+    Type=Application
+    Name=Navionics Demo
+    GenericName=Navionics Demo
+    Comment=Navionics Demo
+    Exec=chromium https://webapp.navionics.com/
+    Terminal=false
+    Icon=gnome-globe
+    Categories=System;GPS;Geography;Navigation;Chart;
+    Keywords=GPS;Navigation;Chart;';;
+ [nN][oO]|[nN])
+    echo "Exit"
+    ;;
+ *)
+    echo "Exit"
+    exit 1
+    ;;
+esac
+
+
