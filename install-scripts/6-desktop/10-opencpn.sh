c5e89312641f190b33ba93db313f5e5eea48ca48
mgrouch <mgrushinskiy@gmail.com>
2021-03-07 12:42:00 -0500

    let's try 32-bit

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -14,35 +14,73 @@ install -o 1000 -g 1000 -v $FILE_FOLDER/opencpn.conf "/home/user/.opencpn/"
 apt-get install -y -q -o Dpkg::Options::="--force-overwrite" oernc-pi
 apt-get install -y -q libsglock
 
-# See https://cloudsmith.io/~david-register/repos/ocpn-plugins-unstable/
-#
-# Chart format plugins
-mkdir oe-tmp
-cd oe-tmp
-wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/oesenc-ubuntu-arm64-18.04-tarball/versions/4.2.18.2.0.c89a5c4/oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz
-gzip -cd oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
-cd oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04
-cp -r usr/ /
-cp -r etc/ /
-cd ../../
-rm -rf oe-tmp
+if [ $LMARCH == 'arm64' ]; then
+  # See https://cloudsmith.io/~david-register/repos/ocpn-plugins-unstable/
+  #
+  # Chart format plugins
+  mkdir oe-tmp
+  cd oe-tmp
+  wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/oesenc-ubuntu-arm64-18.04-tarball/versions/4.2.18.2.0.c89a5c4/oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz
+  gzip -cd oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
+  cd oesenc_pi-4.2.18.2-0_ubuntu-arm64-18.04
+  cp -r usr/ /
+  cp -r etc/ /
+  cd ../../
+  rm -rf oe-tmp
 
-# Chart format plugins
-mkdir s63-tmp
-cd s63-tmp
-wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/s63-ubuntu-arm64-18.04-tarball/versions/1.17.1.0.bcaad82/s63_pi-1.17.1-0_ubuntu-arm64-18.04.tar.gz
-gzip -cd s63_pi-1.17.1-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
-cd s63_pi-1.17.1-0_ubuntu-arm64-18.04
-cp -r usr/ /
-cd ../../
-rm -rf s63-tmp
+  # Chart format plugins
+  mkdir s63-tmp
+  cd s63-tmp
+  wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/s63-ubuntu-arm64-18.04-tarball/versions/1.17.1.0.bcaad82/s63_pi-1.17.1-0_ubuntu-arm64-18.04.tar.gz
+  gzip -cd s63_pi-1.17.1-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
+  cd s63_pi-1.17.1-0_ubuntu-arm64-18.04
+  cp -r usr/ /
+  cd ../../
+  rm -rf s63-tmp
 
-mkdir o-tmp
-cd o-tmp
-wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/o-ubuntu-arm64-18.04-tarball/versions/4.2.18.2.0.ab86266/o-charts_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz
-gzip -cd o-charts_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
-cd o-charts_pi-4.2.18.2-0_ubuntu-arm64-18.04
-cp -r usr/ /
-cp -r etc/ /
-cd ../../
-rm -rf o-tmp
+  mkdir o-tmp
+  cd o-tmp
+  wget https://dl.cloudsmith.io/public/david-register/ocpn-plugins-unstable/raw/names/o-ubuntu-arm64-18.04-tarball/versions/4.2.18.2.0.ab86266/o-charts_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz
+  gzip -cd o-charts_pi-4.2.18.2-0_ubuntu-arm64-18.04.tar.gz | tar xvf -
+  cd o-charts_pi-4.2.18.2-0_ubuntu-arm64-18.04
+  cp -r usr/ /
+  cp -r etc/ /
+  cd ../../
+  rm -rf o-tmp
+fi
+
+if [ $LMARCH == 'armhf' ]; then
+  apt-get install -y -q                                         \
+    oernc-pi                                                    \
+    oesenc-pi                                                   \
+    ofc-pi                                                      \
+    opencpn-doc                                                 \
+    opencpn-plugin-calculator                                   \
+    opencpn-plugin-celestial                                    \
+    opencpn-plugin-chartscale                                   \
+    opencpn-plugin-climatology                                  \
+    opencpn-plugin-climatology-data                             \
+    opencpn-plugin-iacfleet                                     \
+    opencpn-plugin-launcher                                     \
+    opencpn-plugin-logbookkonni                                 \
+    opencpn-plugin-nmeaconverter                                \
+    opencpn-plugin-objsearch                                    \
+    opencpn-plugin-ocpndebugger                                 \
+    opencpn-plugin-plots                                        \
+    opencpn-plugin-polar                                        \
+    opencpn-plugin-pypilot                                      \
+    opencpn-plugin-radar                                        \
+    opencpn-plugin-s63                                          \
+    opencpn-plugin-sar                                          \
+    opencpn-plugin-shipdriver                                   \
+    opencpn-plugin-tactics                                      \
+    opencpn-plugin-vfkaps                                       \
+    opencpn-plugin-watchdog                                     \
+    opencpn-plugin-weatherfax                                   \
+    opencpn-plugin-weatherrouting                               \
+    opencpn-tcdata                                              \
+    opencpn-plugin-draw                                         \
+    opencpn-sglock-arm32                                        \
+    opencpn-plugin-oernc                                        \
+    opencpn-plugin-oesenc
+fi
--- install-scripts/8-apps/files/add-ons/predict-install.sh
+++ install-scripts/8-apps/files/add-ons/predict-install.sh
@@ -3,10 +3,9 @@
 # See: https://github.com/martinber/predict
 
 cd ~
-wget -q -O - https://launchpad.net/ubuntu/+archive/primary/+files/predict_2.2.3-4build2_arm64.deb > predict_2.2.3-4build2_arm64.deb
-wget -q -O - https://launchpad.net/ubuntu/+archive/primary/+files/predict-gsat_2.2.3-4build2_arm64.deb > predict-gsat_2.2.3-4build2_arm64.deb
+wget -q -O - https://launchpad.net/ubuntu/+archive/primary/+files/predict_2.2.3-4build2_${LMARCH}.deb > predict_2.2.3-4build2_${LMARCH}.deb
+wget -q -O - https://launchpad.net/ubuntu/+archive/primary/+files/predict-gsat_2.2.3-4build2_${LMARCH}.deb > predict-gsat_2.2.3-4build2_${LMARCH}.deb
 
 sudo apt install libncurses5 libtinfo5 libforms2
 
-sudo dpkg -i predict_2.2.3-4build2_arm64.deb predict-gsat_2.2.3-4build2_arm64.deb
-
+sudo dpkg -i predict_2.2.3-4build2_${LMARCH}.deb predict-gsat_2.2.3-4build2_${LMARCH}.deb
