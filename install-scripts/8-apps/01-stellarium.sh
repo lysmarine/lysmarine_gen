78d73963b4033dded634cf4220c88f40b786a0cf
mgrouch <mgrushinskiy@gmail.com>
2021-03-12 09:54:21 -0500

    fix

--- install-scripts/8-apps/01-stellarium.sh
+++ install-scripts/8-apps/01-stellarium.sh
@@ -7,4 +7,6 @@ apt-get -y -q install stellarium
 install -d -o 1000 -g 1000 -m 0755 "/home/user/.stellarium"
 install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/stellarium-config.ini "/home/user/.stellarium/config.ini"
 
+install -v -m 0644 $FILE_FOLDER/stellarium.desktop /usr/share/applications/
+
 apt-get clean
