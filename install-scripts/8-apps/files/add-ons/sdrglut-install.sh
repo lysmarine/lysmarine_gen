7e072472b2bb1a300e605772ec62c65373f43a8a
mgrouch <mgrushinskiy@gmail.com>
2021-03-16 18:17:57 -0400

    https://github.com/bareboat-necessities/lysmarine_gen/issues/46

--- install-scripts/8-apps/files/add-ons/sdrglut-install.sh
+++ install-scripts/8-apps/files/add-ons/sdrglut-install.sh
@@ -2,11 +2,18 @@
 
 # See: https://github.com/righthalfplane/SdrGlut
 
+myArch=$(dpkg --print-architecture)
+
 sudo apt-get -y install build-essential libsoapysdr0.6 libsoapysdr-dev libopenal-dev \
  libliquid-dev freeglut3 freeglut3-dev libalut0 libalut-dev librtaudio-dev
 sudo apt-get -y install git
 cd /home/user
-git clone https://github.com/bareboat-necessities/SdrGlut
+rm -rf SdrGlut || true
+if [ "armhf" == "$myArch" ] ; then
+  git clone https://github.com/righthalfplane/SdrGlut
+else
+  git clone https://github.com/bareboat-necessities/SdrGlut
+fi
 cd SdrGlut
 make -f makefileRaspbian
 
