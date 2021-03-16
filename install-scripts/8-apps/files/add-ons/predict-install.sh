ace86d80170a1230a044f1bed68e3c68830954ba
mgrouch <mgrushinskiy@gmail.com>
2021-03-16 10:24:02 -0400

    fixes

--- install-scripts/8-apps/files/add-ons/predict-install.sh
+++ install-scripts/8-apps/files/add-ons/predict-install.sh
@@ -2,10 +2,12 @@
 
 # See: https://github.com/martinber/predict
 
+myArch=$(dpkg --print-architecture)
+
 cd ~
-wget -q -O - https://launchpad.net/ubuntu/+archive/primary/+files/predict_2.2.3-4build2_${LMARCH}.deb > predict_2.2.3-4build2_${LMARCH}.deb
-wget -q -O - https://launchpad.net/ubuntu/+archive/primary/+files/predict-gsat_2.2.3-4build2_${LMARCH}.deb > predict-gsat_2.2.3-4build2_${LMARCH}.deb
+wget -q -O - https://launchpad.net/ubuntu/+archive/primary/+files/predict_2.2.3-4build2_${myArch}.deb > predict_2.2.3-4build2_${myArch}.deb
+wget -q -O - https://launchpad.net/ubuntu/+archive/primary/+files/predict-gsat_2.2.3-4build2_${myArch}.deb > predict-gsat_2.2.3-4build2_${myArch}.deb
 
 sudo apt install libncurses5 libtinfo5 libforms2
 
-sudo dpkg -i predict_2.2.3-4build2_${LMARCH}.deb predict-gsat_2.2.3-4build2_${LMARCH}.deb
+sudo dpkg -i predict_2.2.3-4build2_${myArch}.deb predict-gsat_2.2.3-4build2_${myArch}.deb
