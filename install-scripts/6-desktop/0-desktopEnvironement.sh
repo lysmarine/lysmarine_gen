935e572422ade4f36fbc7f949f56c979583f0cfe
Frederic Guilbault <2@0464.ca>
2021-03-26 12:06:47 -0400

    Reorganising packages list

--- install-scripts/6-desktop/0-desktopEnvironement.sh
+++ install-scripts/6-desktop/0-desktopEnvironement.sh
@@ -3,16 +3,40 @@
 ## Needed to allow the service file start X
 install  -v $FILE_FOLDER/Xwrapper.config "/etc/X11/"
 
-if [ $LMOS == Armbian ] ;then
-	sudo apt-get -q -y install xserver-xorg-legacy	
-fi
+## Xorg server
+apt-get -yq --no-install-recommends install xserver-xorg-legacy xserver-xorg xinit x11-xserver-utils xinput
 
-sudo apt-get -q -y install xserver-xorg-legacy
-apt-get install -yq \
-	xserver-xorg xinit evdev-rce awesome alsa-utils \
- 	servicemanager pavucontrol network-manager-gnome \
-	chromium sakura mousepad file-roller gpsbabel mirage rpi-chromium-mods
-apt-get install -yq --no-install-recommends gvfs-backends gvfs-fuse pcmanfm 
+## Rpi specific packages
+apt-get install -yq --no-install-recommends xserver-xorg-video-fbdev xserver-xorg-video-fbturbo gldriver-test \
+	&& apt-get remove -y xserver-xorg-video-all xserver-xorg-video-amdgpu xserver-xorg-video-ati xserver-xorg-video-nouveau xserver-xorg-video-radeon xserver-xorg-video-vesa \
+	|| true
+
+## Base desktop Env
+apt-get install -yq --no-install-recommends \
+	evdev-rce \
+	alsa-utils pavucontrol pulseaudio rtkit dbus-user-session pulseaudio-module-bluetooth \
+	sakura \
+	network-manager-gnome mobile-broadband-provider-info iso-codes \
+	awesome awesome-extra gir1.2-gtk-3.0 rlwrap # fixme not sure if rlwrap is really needed
+
+# Apps
+apt-get install -yq --no-install-recommends \
+	mirage \
+	gpsbabel gpsbabel-doc \
+	mousepad \
+	pcmanfm gvfs-backends gvfs-fuse  file-roller \
+	vlc nautic
+apt-get install -yq chromium
+
+## Rpi specific packages
+apt-get install -yq --no-install-recommends piclone pigpio libturbojpeg0 raspi-copies-and-fills || true
+
+apt-get install -yq rpi-chromium-mods || apt-get install -yq chromium-sandbox
+
+#sed -i 's/"first_run_tabs":["*"]/"first_run_tabs":["https://lysmarine.org"]/' /etc/chromium/master_preferences
+
+# "first_run_tabs":["https://welcome.raspberrypi.org/raspberry-pi-os?id=UNIDENTIFIED"]
+# rm /etc/chromium/master_preferences
 
 apt-get purge --auto-remove -yq system-config-printer # chromium install cups but we don't need it.
 apt-get purge --auto-remove -yq gnome-keyring # chromium and others install a keyring service but we don't need it
