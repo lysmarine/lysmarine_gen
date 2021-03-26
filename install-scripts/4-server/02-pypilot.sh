a28e2e8a3784e4a1bc5157fc1c4876aa2044f232
mgrouch <mgrushinskiy@gmail.com>
2021-03-26 11:05:14 -0400

    pp

--- install-scripts/4-server/02-pypilot.sh
+++ install-scripts/4-server/02-pypilot.sh
@@ -53,8 +53,8 @@ install -v -m 0644 $FILE_FOLDER/pypilot@.service "/etc/systemd/system/"
 install -v -m 0644 $FILE_FOLDER/pypilot_web.service "/etc/systemd/system/"
 install -v -m 0644 $FILE_FOLDER/pypilot_web.socket "/etc/systemd/system/"
 
-systemctl disable pypilot@pypilot.service
-systemctl enable pypilot_web.service
+systemctl disable pypilot@pypilot.service                              # listens on tcp 20220 and 23322
+systemctl enable pypilot_web.service                                   # listens on tcp 8080
 
 ## Install the user config files
 install -v -o pypilot -g pypilot -m 0755 -d /home/pypilot/.pypilot
