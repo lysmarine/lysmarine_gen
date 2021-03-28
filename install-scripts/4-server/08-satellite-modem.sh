0cd6f543cebd61d9c91a4b27b11d90ab216e6d31
mgrouch <mgrushinskiy@gmail.com>
2021-03-28 12:47:29 -0400

    iridium

--- install-scripts/4-server/08-satellite-modem.sh
+++ install-scripts/4-server/08-satellite-modem.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
-apt-get -y -q install wvdial ppp picocom
+apt-get -y -q install wvdial ppp picocom pppstatus slurm
 
+# See: http://newt.phys.unsw.edu.au/~mcba/iridiumLinux.html
+install -v -m 0644 $FILE_FOLDER/wvdial-iridium.conf "/etc/"
 
+# See https://github.com/tdolby/python-iridium-modem/
+pip3 install python-iridium-modem
 
