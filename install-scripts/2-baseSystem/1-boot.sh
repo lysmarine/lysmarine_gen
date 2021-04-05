9d55f028c24431ec9141783f72201270b26f8fae
Frederic Guilbault <2@0464.ca>
2021-04-05 11:26:40 -0400

    Longer plymouth display

--- install-scripts/2-baseSystem/1-boot.sh
+++ install-scripts/2-baseSystem/1-boot.sh
@@ -12,7 +12,7 @@ fi
 
 ## RaspiOS
 if [ -f /boot/cmdline.txt ] ; then
-	sed -i '$s/$/\ loglevel=1\ splash\ quiet\ logo.nologo\ vt.global_cursor_default=0\ plymouth.ignore-serial-consoles\ console=tty3\ rd.systemd.show_status=false/' /boot/cmdline.txt
+	sed -i '$s/$/\ loglevel=1\ splash\ quiet\ logo.nologo\ plymouth.ignore-serial-consoles\ console=tty3\ rd.systemd.show_status=false/' /boot/cmdline.txt
 fi
 
 ## Armbian
