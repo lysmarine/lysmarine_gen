8798fec9c39c4aa90032be9de3e1a9a5a3c05498
Frederic Guilbault <2@0464.ca>
2021-04-05 11:27:23 -0400

    Fix upstream changes in wpa_supplicant

--- install-scripts/2-baseSystem/2-network.sh
+++ install-scripts/2-baseSystem/2-network.sh
@@ -13,16 +13,16 @@ cat $FILE_FOLDER/hosts >> /etc/hosts
 install -m0600 -v $FILE_FOLDER/lysmarine-hotspot.nmconnection "/etc/NetworkManager/system-connections/"
 systemctl disable dnsmasq
 
-
 ##  NetworkManager provide it's own wpa_supplicant, stop the others to avoid conflicts.
 if service --status-all | grep -Fq 'dhcpcd'; then
 	systemctl disable dhcpcd.service
 fi
 systemctl disable wpa_supplicant.service
-systemctl disable hostapd.service
-
 
 ## Disable some useless networking services.
 systemctl disable NetworkManager-wait-online.service # If we do not boot remote user over the network this is not needed
 systemctl disable ModemManager.service # for 2G/3G/4G
-systemctl disable pppd-dns.service # For dial-up Internet LOL
+systemctl disable pppd-dns.service # For dial-up Internet
+
+#FIXME This is a dirty hack to avoid conflict with NetworkManager. A better solution is probable available in this link link http://downloads.fars-robotics.net/
+echo "" > /etc/wpa_supplicant/wpa_supplicant.conf
