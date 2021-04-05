8e4bb8845411bf94a333fca47ddb6e8ae3554b52
Frederic Guilbault <2@0464.ca>
2021-04-05 11:27:42 -0400

    Cleaner kplex integration

--- install-scripts/4-server/1-kplex.sh
+++ install-scripts/4-server/1-kplex.sh
@@ -2,17 +2,14 @@
 apt-get install -y -q libc6
 
 ## Detect the right package to download.
-	package='kplex_1.4-1_amd64.deb'
-elif [[ $LMARCH == 'armhf' ]]; then
+if [[ $LMARCH == 'arm64' ]]; then
+	apt-get install -yq gcc-8-base:armhf libc6:armhf libgcc1:armhf
 	package='kplex_1.4-1_armhf.deb'
-elif [[ $LMARCH == 'arm64' ]]; then
-	apt-get install -y -q gcc-8-base:armhf libc6:armhf libgcc1:armhf
-	package='kplex_1.4-1_armhf.deb'
+else
+	package="kplex_1.4-1_${LMARCH}.deb"
 fi
 
 ## Download and install the package.
 wget http://www.stripydog.com/download/$package
 dpkg -i $package
 rm $package
-
-systemctl enable kplex
