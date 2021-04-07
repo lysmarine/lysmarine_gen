825eb187145c31b0dc7c0f21152ee38b31f796bc
mgrouch <mgrushinskiy@gmail.com>
2021-04-07 10:43:34 -0400

    fix

--- install-scripts/6-desktop/files/bbn-commands.sh
+++ install-scripts/6-desktop/files/bbn-commands.sh
@@ -7,8 +7,8 @@ ret=$?
 [[ $ret -eq 1 ]] && exit 0
 
 case $action in
-    reboot*) cmd="/sbin/reboot" ;;
-    shutdown*) cmd="/sbin/poweroff" ;;
+    reboot*) cmd='sh -c "kill $(pidof opencpn); /sbin/reboot"' ;;
+    shutdown*) cmd='sh -c "kill $(pidof opencpn); /sbin/poweroff"' ;;
     restartD*) cmd="budgie-panel --replace&" ;;
     restartSK*) cmd="/usr/local/sbin/signalk-restart" ;;
     *) exit 1 ;;
