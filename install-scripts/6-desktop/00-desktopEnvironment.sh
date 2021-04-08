2a6e500a84f76a65aaef58cb77832b4247bd00d0
mgrouch <mgrushinskiy@gmail.com>
2021-04-08 13:55:25 -0400

    fix

--- install-scripts/6-desktop/00-desktopEnvironment.sh
+++ install-scripts/6-desktop/00-desktopEnvironment.sh
@@ -55,6 +55,8 @@ install -o 1000 -g 1000 -v $FILE_FOLDER/openplotter.directory /home/user/.local/
 
 install -m 755 -v $FILE_FOLDER/bbn-commands.sh /usr/local/bin/bbn-commands
 
+install -v $FILE_FOLDER/commands.desktop /usr/local/share/applications/
+
 install -d /etc/budgie-desktop
 install -m 644 -v $FILE_FOLDER/panel.ini /etc/budgie-desktop/
 
