af6ea3edfec0cceed24defc8a977b3bbfad0b58c
mgrouch <mgrushinskiy@gmail.com>
2021-04-11 01:26:00 -0400

    autolocation

--- install-scripts/6-desktop/02-onboard.sh
+++ install-scripts/6-desktop/02-onboard.sh
@@ -9,8 +9,12 @@ install -v $FILE_FOLDER/a11y.dconf "/usr/share/onboard/"
 ## Dconf injection on the first boot + self commenting sed replace.
 install -d -o 1000 -g 1000 /home/user/.config/openbox/
 
-echo "dconf load /org/onboard/ < /usr/share/onboard/onboard.dconf &" >> /home/user/.config/openbox/autostart
-echo "dconf load / < /usr/share/onboard/a11y.dconf &" >> /home/user/.config/openbox/autostart
+{
+  echo "dconf load /org/onboard/ < /usr/share/onboard/onboard.dconf";
+  echo "dconf load / < /usr/share/onboard/a11y.dconf";
+  echo "dconf write /org/gnome/system/location/enabled true";
+  echo "dconf write /org/gnome/Weather/Application/automatic-location true";
+} >> /home/user/.config/openbox/autostart
 
 echo "sed -i 's/^dconf\ /#&/' /home/user/.config/openbox/autostart" >> /home/user/.config/openbox/autostart
 echo "sed -i 's/^sed\ /#&/'   /home/user/.config/openbox/autostart" >> /home/user/.config/openbox/autostart
