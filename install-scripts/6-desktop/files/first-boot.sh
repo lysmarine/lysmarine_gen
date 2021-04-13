f062a7dc720e3b9cbfd46cf6a4f26ec3a97f0050
mgrouch <mgrushinskiy@gmail.com>
2021-04-13 14:09:52 -0400

    first boot

--- install-scripts/6-desktop/files/first-boot.sh
+++ install-scripts/6-desktop/files/first-boot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 #
-# Put commands here to be executed on fisrt system boot
+# Put commands here to be executed on first system boot
 # (Useful for advanced OS image customization)
 #
 
