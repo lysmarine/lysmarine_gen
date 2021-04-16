ac7b7fdcd88ffa6198f353dd1a5d9551b54f72b8
mgrouch <mgrushinskiy@gmail.com>
2021-04-16 11:56:21 -0400

    Lightning Maps

---
+++ install-scripts/8-apps/files/add-ons/lightningmaps-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+sudo bash -c 'cat << EOF > /usr/local/share/applications/lightningmaps.desktop
+[Desktop Entry]
+Type=Application
+Name=Lightning Maps
+GenericName=Lightning Maps
+Comment=Lightning Maps
+Exec=gnome-www-browser https://www.lightningmaps.org/
+Terminal=false
+Icon=gnome-globe
+Categories=WWW;Internet
+EOF'
