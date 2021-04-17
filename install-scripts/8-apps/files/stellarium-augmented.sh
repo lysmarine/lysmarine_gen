30830260a0d797267f2f415972e3ba56b0ff64a9
mgrouch <mgrushinskiy@gmail.com>
2021-04-17 02:54:37 -0400

    stellarium-augmented

--- install-scripts/8-apps/files/stellarium-augmented.sh
+++ install-scripts/8-apps/files/stellarium-augmented.sh
@@ -2,19 +2,18 @@
 
 # hack to control stellarium azimuth by true north heading from signalK
 
-stellarium --startup-script=%f  &
+stellarium "$@"  &
 sleep 5
 
 while [[ -n "$(pidof stellarium)" ]]
 do
-    # magnetic variation
-    MV=$(curl -s http://localhost:80/signalk/v1/api/vessels/self/navigation/magneticVariation/value)
-    # magnetic heading
-    MH=$(curl -s http://localhost:80/signalk/v1/api/vessels/self/navigation/headingMagnetic/value)
-    # true heading
-    HT=$(echo $MH + $MV | bc)
-    # control stellarium azimuth
-    curl -X POST -d "az=$HT" http://localhost:8090/api/main/view
+  # magnetic variation
+  MV=$(curl -s http://localhost:80/signalk/v1/api/vessels/self/navigation/magneticVariation/value)
+  # magnetic heading
+  MH=$(curl -s http://localhost:80/signalk/v1/api/vessels/self/navigation/headingMagnetic/value)
+  # true heading
+  HT=$(echo $MH + $MV | bc)
+  # control stellarium azimuth
+  curl -X POST -d "az=$HT" http://localhost:8090/api/main/view
   sleep 2
 done
-
