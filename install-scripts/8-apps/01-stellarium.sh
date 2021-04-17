d19a859adb3caa651bfd463ed2f4088b871ce2cf
mgrouch <mgrushinskiy@gmail.com>
2021-04-17 02:32:52 -0400

    stellarium-augmented

--- install-scripts/8-apps/01-stellarium.sh
+++ install-scripts/8-apps/01-stellarium.sh
@@ -8,5 +8,6 @@ install -d -o 1000 -g 1000 -m 0755 "/home/user/.stellarium"
 install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/stellarium-config.ini "/home/user/.stellarium/config.ini"
 
 install -v -m 0644 $FILE_FOLDER/stellarium.desktop /usr/share/applications/
+install -v -m 0755 $FILE_FOLDER/stellarium-augmented.sh /usr/local/bin/stellarium-augmented
 
 apt-get clean
