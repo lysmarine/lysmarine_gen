5ae624b4578c28d4e0225054b819487392a8c366
mgrouch <mgrushinskiy@gmail.com>
2021-04-26 02:56:32 -0400

    samba-client

--- install-scripts/4-server/11-samba.sh
+++ install-scripts/4-server/11-samba.sh
@@ -4,7 +4,7 @@ echo "samba-common samba-common/workgroup string WORKGROUP" | sudo debconf-set-s
 echo "samba-common samba-common/dhcp boolean true" | sudo debconf-set-selections
 echo "samba-common samba-common/do_debconf boolean true" | sudo debconf-set-selections
 
-apt-get -y -q install samba samba-common
+apt-get -y -q install samba samba-common samba-client
 
 # systemctl enable smbd
 
