1e63f8e4109652d13f986e15d546430c0b2ad63a
mgrouch <mgrushinskiy@gmail.com>
2021-04-29 20:05:53 -0400

    more sdr/ham

--- install-scripts/8-apps/00-radio-sdr.sh
+++ install-scripts/8-apps/00-radio-sdr.sh
@@ -2,6 +2,18 @@
 
 apt-get clean
 
+install -v -m 0644 $FILE_FOLDER/50-rtl-sdr.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/52-airspy.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/52-airspyhf.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/53-hackrf.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/66-mirics.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/88-nuand-bladerf1.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/88-nuand-bladerf2.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/88-nuand-bootloader.rules "/etc/udev/rules.d/"
+#install -v -m 0644 $FILE_FOLDER/99-com.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/99-direwolf-cmedia.rules "/etc/udev/rules.d/"
+install -v -m 0644 $FILE_FOLDER/99-thumbdv.rules "/etc/udev/rules.d/"
+
 apt-get -y -q install multimon-ng netcat
 apt-get -y -q install cubicsdr
 apt-get -y -q install cutesdr
