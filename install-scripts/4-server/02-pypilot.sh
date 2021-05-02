ce9b64f2896b5f5ba760a7cb4d49e68d9908afda
mgrouch <mgrushinskiy@gmail.com>
2021-05-02 09:04:59 -0400

    trying to fix pypilot_web connection issue for latest pypilot

--- install-scripts/4-server/02-pypilot.sh
+++ install-scripts/4-server/02-pypilot.sh
@@ -16,10 +16,10 @@ systemctl disable watchdog
 install -v -m 0644 $FILE_FOLDER/60-watchdog.rules "/etc/udev/rules.d/60-watchdog.rules"
 
 if [ $LMARCH == 'arm64' ]; then
-  pip3 install pywavefront pyglet gps gevent-websocket "python-socketio<5" "flask-socketio<5"
+  pip3 install pywavefront pyglet gps gevent-websocket "python-socketio" "flask-socketio"
 else
   apt-get install -y -q python3-flask-socketio
-  pip3 install pywavefront pyglet gps gevent-websocket "python-socketio<5"
+  pip3 install pywavefront pyglet gps gevent-websocket "python-socketio"
 fi
 
 #if [ $LMOS == 'Raspbian' ]; then
