858d7c08351a059837f0518033ba1db7f664606f
mgrouch <mgrushinskiy@gmail.com>
2021-05-04 00:34:20 -0400

    flightaware.list

--- install-scripts/8-apps/14-ads-b.sh
+++ install-scripts/8-apps/14-ads-b.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
-apt-get -y -q install piaware dump1090-fa
+apt-get -y -q install dump1090-fa piaware
 
 sed -i 's/= 80/= 8186/' /etc/lighttpd/lighttpd.conf
 
 systemctl disable lighttpd
 systemctl disable dump1090-fa
+systemctl disable piaware
