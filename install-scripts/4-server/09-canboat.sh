23a754ab8b9e92124ac17599d5aaaaf14a99a44b
mgrouch <mgrushinskiy@gmail.com>
2021-05-29 11:41:08 -0400

    for canboat n2kd_monitor

--- install-scripts/4-server/09-canboat.sh
+++ install-scripts/4-server/09-canboat.sh
@@ -2,6 +2,8 @@
 
 apt-get -y -q install canboat can-utils dfu-util
 
+yes | cpan install Config::General # for canboat n2kd_monitor
+
 systemctl disable canboat.service
 
 install -v -m 0644 $FILE_FOLDER/socketcan-interface.service "/etc/systemd/system/socketcan-interface.service"
