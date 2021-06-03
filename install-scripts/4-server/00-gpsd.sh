864008249e05ddd2d045d2f52336c87a6991ee08
mgrouch <mgrushinskiy@gmail.com>
2021-06-02 20:51:48 -0400

    91-pc-navtex.rules

--- install-scripts/4-server/00-gpsd.sh
+++ install-scripts/4-server/00-gpsd.sh
@@ -11,6 +11,7 @@ install -v -m 0644 $FILE_FOLDER/90-lys-ftdi.rules "/etc/udev/rules.d/90-lys-ftdi
 install -v -m 0644 $FILE_FOLDER/90-lys-gps.rules "/etc/udev/rules.d/90-lys-gps.rules"
 install -v -m 0644 $FILE_FOLDER/90-lys-prolific.rules "/etc/udev/rules.d/90-lys-prolific.rules"
 install -v -m 0644 $FILE_FOLDER/70-lys-can.rules "/etc/udev/rules.d/70-lys-can.rules"
+install -v -m 0644 $FILE_FOLDER/91-pc-navtex.rules "/etc/udev/rules.d/91-pc-navtex.rules"
 install -v -m 0644 $FILE_FOLDER/99-zzz-com.rules "/etc/udev/rules.d/99-zzz-com.rules"
 
 install -d /usr/local/bin
