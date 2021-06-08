b8e3fda598a1654e289b7ded3f09ff6010cf46ac
mgrouch <mgrushinskiy@gmail.com>
2021-06-08 11:39:11 -0400

    upd

--- install-scripts/6-desktop/08-remoteDesktopAccess.sh
+++ install-scripts/6-desktop/08-remoteDesktopAccess.sh
@@ -8,8 +8,6 @@ install -v -m 644 $FILE_FOLDER/vnc.service "/etc/systemd/system/vnc.service"
 install -v -d -o 1000 -g 1000 "/home/user/.vnc"
 install -v -o 1000 -g 1000 $FILE_FOLDER/passwd "/home/user/.vnc/passwd"
 
-systemctl enable vnc
-
 ## NOTE: The password file have been generated with the command
 ## x11vnc -storepasswd SECRETPASSWORD /home/user/.vnc/passwd
 
@@ -20,3 +18,5 @@ install -v $FILE_FOLDER/Xwrapper.config "/etc/X11/Xwrapper.config"
 
 systemctl disable xrdp
 systemctl disable xrdp-sesman
+
+systemctl enable vnc
