ee6f882b823888414f2443d46d58a7b42c43e363
mgrouch <mgrushinskiy@gmail.com>
2021-06-21 12:41:29 -0400

    os-guide

--- install-scripts/8-apps/13-docs.sh
+++ install-scripts/8-apps/13-docs.sh
@@ -4,3 +4,16 @@ install -d -o 1000 -g 1000 /home/user/Documents/
 
 wget -q -O - https://bareboat-necessities.github.io/my-bareboat/bareboat-os.pdf > /home/user/Documents/bareboat-os.pdf
 #wget -q -O - https://bareboat-necessities.github.io/my-bareboat/index.pdf > /home/user/Documents/my-bareboat.pdf
+
+
+sudo bash -c 'cat << EOF > /usr/local/share/applications/os-guide.desktop
+[Desktop Entry]
+Type=Application
+Name=OS Guide
+GenericName=OS Guide
+Comment=OS Guide
+Exec=gnome-www-browser /home/user/Documents/bareboat-os.pdf
+Terminal=false
+Icon=help-browser
+Categories=Utility;
+EOF'
