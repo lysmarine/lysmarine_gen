d8e7c4668a4743e5bbe179db1d18019400a9401d
mgrouch <mgrushinskiy@gmail.com>
2021-07-01 13:41:43 -0400

    upd

--- install-scripts/8-apps/files/add-ons/qtvlm-install.sh
+++ install-scripts/8-apps/files/add-ons/qtvlm-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-curl -s https://raw.githubusercontent.com/bareboat-necessities/my-bareboat/master/qtvlm/rpi_qtvlm_install.sh | bash -s
+bash <(curl -sL https://raw.githubusercontent.com/bareboat-necessities/my-bareboat/master/qtvlm/rpi_qtvlm_install.sh)
