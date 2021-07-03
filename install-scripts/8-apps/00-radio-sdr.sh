8dc77a677803ddda5cb01f521e7e9ba700a08555
mgrouch <mgrushinskiy@gmail.com>
2021-07-03 19:59:51 -0400

    fix

--- install-scripts/8-apps/00-radio-sdr.sh
+++ install-scripts/8-apps/00-radio-sdr.sh
@@ -223,9 +223,10 @@ apt-get install -y cmake
 
 pushd /usr/local/share/aisdecoder
   git clone https://github.com/bareboat-necessities/aisdecoder
+  cd aisdecoder
   mkdir build && cd build
-    cmake ../ -DCMAKE_BUILD_TYPE=RELEASE
-    make
-    cp aisdecoder /usr/local/bin/
-  cd ..
+  cmake ../ -DCMAKE_BUILD_TYPE=RELEASE
+  make
+  cp aisdecoder /usr/local/bin/
+  cd ../..
 popd
