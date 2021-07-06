f712a80d3ab6584031bcf9a7e4538c3a76589cc4
mgrouch <mgrushinskiy@gmail.com>
2021-07-06 11:06:25 -0400

    .

--- install-scripts/4-server/13-influxdb.sh
+++ install-scripts/4-server/13-influxdb.sh
@@ -2,7 +2,10 @@
 
 apt-get clean
 
-apt-get -y -q install influxdb
+apt-get -y -q install influxdb chronograf kapacitor
 
 systemctl unmask influxdb
 systemctl disable influxdb
+
+systemctl disable chronograf
+systemctl disable kapacitor
