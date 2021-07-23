aed76af80d2098cc46e832cb60b1b23f6b9b12ed
mgrouch <mgrushinskiy@gmail.com>
2021-07-23 02:35:51 -0400

    switch to realvnc server

--- install-scripts/6-desktop/08-remoteDesktopAccess.sh
+++ install-scripts/6-desktop/08-remoteDesktopAccess.sh
@@ -2,14 +2,19 @@
 
 apt-get clean
 
-apt-get install -y -q x11vnc
+x11vnc=0
+if [ $x11vnc == 1 ]; then
+  apt-get install -y -q x11vnc
 
-install -v -m 644 $FILE_FOLDER/vnc.service "/etc/systemd/system/vnc.service"
-install -v -d -o 1000 -g 1000 "/home/user/.vnc"
-install -v -o 1000 -g 1000 $FILE_FOLDER/passwd "/home/user/.vnc/passwd"
+  install -v -m 644 $FILE_FOLDER/vnc.service "/etc/systemd/system/vnc.service"
+  install -v -d -o 1000 -g 1000 "/home/user/.vnc"
+  install -v -o 1000 -g 1000 $FILE_FOLDER/passwd "/home/user/.vnc/passwd"
 
-## NOTE: The password file have been generated with the command
-## x11vnc -storepasswd SECRETPASSWORD /home/user/.vnc/passwd
+  ## NOTE: The password file have been generated with the command
+  ## x11vnc -storepasswd SECRETPASSWORD /home/user/.vnc/passwd
+else
+  apt-get -y install realvnc-vnc-server
+fi
 
 install -d /etc/systemd/system/xrdp.d
 
@@ -23,4 +28,8 @@ chown xrdp:adm /var/log/xrdp.log
 systemctl disable xrdp
 systemctl disable xrdp-sesman
 
-systemctl enable vnc
+if [ $x11vnc == 1 ]; then
+  systemctl enable vnc
+else
+  systemctl enable vncserver-x11-serviced.service
+fi
