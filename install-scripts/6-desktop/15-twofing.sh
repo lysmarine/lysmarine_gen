40207eb791c91820cf49509d312d11823902e953
mgrouch <mgrushinskiy@gmail.com>
2021-08-30 18:09:33 -0400

    twofing

---
+++ install-scripts/6-desktop/15-twofing.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+# See http://plippo.de/dev_twofing
+# and https://github.com/Plippo/twofing
+# and https://github.com/sjlongland/twofing
+
+apt-get -y install build-essential libx11-dev libxtst-dev libxi-dev x11proto-randr-dev libxrandr-dev \
+  xserver-xorg-input-evdev-dev xserver-xorg-input-evdev git
+
+git clone https://github.com/Plippo/twofing.git
+
+cd twofing || exit 255
+make && cp twofing /usr/local/bin/
+cd .. && rm -rf twofing
+
+bash -c 'cat << EOF > /etc/udev/rules.d/70-touchscreen.rules
+SUBSYSTEMS=="input", KERNEL=="event[0-9]*", ENV{ID_INPUT_TOUCHSCREEN}=="1", SYMLINK+="touchscreen0"
+SUBSYSTEMS=="input", KERNEL=="event[0-9]*", ENV{ID_INPUT_TOUCHSCREEN}=="1", SYMLINK+="twofingtouch", RUN+="/bin/chmod a+r /dev/twofingtouch"
+EOF'
+
+bash -c 'cat << EOF > /etc/udev/rules.d/99-input-tagging.rules
+ACTION=="add", KERNEL=="event*", SUBSYSTEM=="input", TAG+="systemd", , ENV{SYSTEMD_ALIAS}+="/sys/subsystem/input/devices/$env{ID_SERIAL}"
+EOF'
+
