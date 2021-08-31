1f5ea1679570aae6ec6c692c20d4e1746413224c
mgrouch <mgrushinskiy@gmail.com>
2021-08-30 20:33:33 -0400

    twofing

--- install-scripts/6-desktop/15-twofing.sh
+++ install-scripts/6-desktop/15-twofing.sh
@@ -7,7 +7,7 @@
 apt-get -y install build-essential libx11-dev libxtst-dev libxi-dev x11proto-randr-dev libxrandr-dev \
   xserver-xorg-input-evdev-dev xserver-xorg-input-evdev git
 
-git clone https://github.com/Plippo/twofing.git
+git clone https://github.com/bareboat-necessities/twofing.git
 
 cd twofing || exit 255
 make && cp twofing /usr/local/bin/
