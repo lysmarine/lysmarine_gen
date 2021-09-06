ac1d5de561a1a09d55e06bea001f956d7888e0a6
mgrouch <mgrushinskiy@gmail.com>
2021-09-06 16:42:11 -0400

    https://github.com/bareboat-necessities/lysmarine_gen/issues/65

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -82,3 +82,18 @@ if [ $LMARCH == 'armhf' ]; then
 #    opencpn-plugin-oernc                                       \
 #    opencpn-plugin-oesenc
 fi
+
+# Install plugin bundle
+rm -rf tmp-o-bundle-$LMARCH || exit 1
+mkdir tmp-o-bundle-$LMARCH || exit 2
+cd tmp-o-bundle-$LMARCH
+
+wget -O opencpn-plugins-bundle-$LMARCH.tar.gz https://github.com/bareboat-necessities/opencpn-plugins-bundle/blob/main/bundles/opencpn-plugins-bundle-$LMARCH.tar.gz?raw=true
+gzip -cd opencpn-plugins-bundle-$LMARCH.tar.gz | tar xvf -
+
+cp -r -p lib/* /usr/lib/
+cp -r -p bin/* /usr/bin/
+cp -r -p share/* /usr/share/
+
+cd ..
+rm -rf tmp-o-bundle-$LMARCH
