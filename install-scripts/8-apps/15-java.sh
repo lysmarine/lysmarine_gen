04a6a78745a104f23e04ec0ec9ddb97cbb4d53c1
mgrouch <mgrushinskiy@gmail.com>
2021-09-07 13:31:00 -0400

    fix

--- install-scripts/8-apps/15-java.sh
+++ install-scripts/8-apps/15-java.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
-apt-get -y install openjdk-11-jdk openjdk-11-jdk-headless openjdk-11-jre openjdk-11-jre-headless
+if [ $LMARCH == 'armhf' ]; then
+  apt-get -y install openjdk-11-jdk openjdk-11-jdk-headless openjdk-11-jre openjdk-11-jre-headless
 
-apt-get -y remove openjdk-8-jdk openjdk-8-jdk-headless openjdk-8-jre openjdk-8-jre-headless
+  apt-get -y remove openjdk-8-jdk openjdk-8-jdk-headless openjdk-8-jre openjdk-8-jre-headless
+fi
