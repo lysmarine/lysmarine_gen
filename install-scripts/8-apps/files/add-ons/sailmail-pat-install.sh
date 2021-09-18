716315b0e5b181a7cf79ae128f2202b08d5fef57
mgrouch <mgrushinskiy@gmail.com>
2021-09-17 20:40:51 -0400

    pat winlink

--- install-scripts/8-apps/files/add-ons/sailmail-pat-install.sh
+++ install-scripts/8-apps/files/add-ons/sailmail-pat-install.sh
@@ -6,7 +6,7 @@ cd /home/user
 
 mkdir sailmail-pat && cd sailmail-pat
 
-curl https://raw.githubusercontent.com/CarpeNoctem/pat-on-a-pi/main/setup_pat.sh > setup_pat.sh; bash setup_pat.sh
+curl https://raw.githubusercontent.com/bareboat-necessities/pat-on-a-pi/main/setup_pat.sh > setup_pat.sh; bash setup_pat.sh
 
 cd ..
 
