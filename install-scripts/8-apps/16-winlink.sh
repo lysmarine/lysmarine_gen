6c9a4ac8f40118f1efde8c108e258f090c3f3fc5
mgrouch <mgrushinskiy@gmail.com>
2021-09-17 20:21:27 -0400

    pat winlink

---
+++ install-scripts/8-apps/16-winlink.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# See: https://getpat.io/
+
+apt-get -y -q install libhamlib-utils
+
+if [ $LMARCH == 'armhf' ]; then
+  wget https://github.com/la5nta/pat/releases/download/v0.11.0/pat_0.11.0_linux_armhf.deb
+  dpkg -i pat_0.11.0_linux_armhf.deb && rm pat_0.11.0_linux_armhf.deb
+
+  wget -O ardopc http://www.cantab.net/users/john.wiseman/Downloads/Beta/piardopc
+  install ardopc /usr/local/bin && rm ardopc
+  if [ "$(grep 'pcm\.ARDOP' ${HOME}/.asoundrc |wc -l)" -lt "1" ]; then
+    echo 'pcm.ARDOP {type rate slave {pcm "hw:CARD=CODEC,DEV=0" rate 48000}}' >> ${HOME}/.asoundrc;
+  fi
+fi
