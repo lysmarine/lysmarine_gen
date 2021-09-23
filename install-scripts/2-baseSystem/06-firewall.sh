048c61f0d3c6d39817d22f5b58ad012699392819
mgrouch <mgrushinskiy@gmail.com>
2021-09-22 21:15:04 -0400

    firewall

--- install-scripts/2-baseSystem/06-firewall.sh
+++ install-scripts/2-baseSystem/06-firewall.sh
@@ -2,7 +2,17 @@
 
 apt-get -y -q install ufw
 
-ufw default deny
+ufw default deny incoming
+ufw default allow outgoing
+
 ufw allow from 192.0.0.0/8
 ufw allow from 10.0.0.0/8
+
+# Multicast
+ufw allow from 239.0.0.0/8
+ufw allow from 224.0.0.0/8
+
+# For Garmin radar
+ufw allow from 172.0.0.0/8
+
 ufw enable
