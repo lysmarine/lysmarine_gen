a4ce1eaee1651515298b36d3a88b4c76d3925f04
mgrouch <mgrushinskiy@gmail.com>
2021-09-23 09:07:06 -0400

    firewall

--- install-scripts/2-baseSystem/06-firewall.sh
+++ install-scripts/2-baseSystem/06-firewall.sh
@@ -2,29 +2,5 @@
 
 apt-get -y -q install ufw
 
-# The rules are a bit loose, but you are supposed to put your pi behind a separate internet router anyway
-
-ufw default deny incoming
-ufw default allow outgoing
-
-ufw allow from 192.168.0.0/16
-ufw allow from 169.254.0.0/16
-ufw allow from 10.0.0.0/8
-
-# carrier-grade NAT
-ufw allow from 100.64.0.0/10
-
-# For Garmin radar, etc
-ufw allow from 172.16.0.0/12
-
-# IPv6
-ufw allow from fd00::/8
-ufw allow from fe80::/10
-
-# Multicast
-ufw allow in proto udp from 224.0.0.0/4
-
-# IPv6
-ufw allow in proto udp from ff00::/8
-
-ufw enable
+# will be called on first boot
+install -m 755 $FILE_FOLDER/ufw-init.sh "/usr/local/sbin/ufw-init"
---
+++ install-scripts/2-baseSystem/files/ufw-init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+# The rules are a bit loose for multicast
+
+ufw default deny incoming
+ufw default allow outgoing
+
+ufw allow from 192.168.0.0/16
+ufw allow from 169.254.0.0/16
+ufw allow from 10.0.0.0/8
+
+# carrier-grade NAT
+ufw allow from 100.64.0.0/10
+
+# For Garmin radar, etc
+ufw allow from 172.16.0.0/12
+
+# IPv6
+ufw allow from fd00::/8
+ufw allow from fe80::/10
+
+# Multicast
+ufw allow in proto udp from 224.0.0.0/4
+
+# IPv6
+ufw allow in proto udp from ff00::/8
+
+ufw enable
