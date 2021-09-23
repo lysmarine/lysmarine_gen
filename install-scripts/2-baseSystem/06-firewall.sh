135c5b5dd65923efeb0127c7c91d52545aea1e81
mgrouch <mgrushinskiy@gmail.com>
2021-09-22 21:16:21 -0400

    firewall

--- install-scripts/2-baseSystem/06-firewall.sh
+++ install-scripts/2-baseSystem/06-firewall.sh
@@ -10,11 +10,11 @@ ufw default allow outgoing
 ufw allow from 192.0.0.0/8
 ufw allow from 10.0.0.0/8
 
+# For Garmin radar
+ufw allow from 172.0.0.0/8
+
 # Multicast
 ufw allow from 239.0.0.0/8
 ufw allow from 224.0.0.0/8
-
-# For Garmin radar
-ufw allow from 172.0.0.0/8
 
 ufw enable
