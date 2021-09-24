03b501f4907ffd922687d21ff4ef8dbfde9ece48
mgrouch <mgrushinskiy@gmail.com>
2021-09-24 04:14:43 -0400

    fix

--- install-scripts/8-apps/files/stellarium-augmented.sh
+++ install-scripts/8-apps/files/stellarium-augmented.sh
@@ -11,12 +11,16 @@ do
   MV=$(curl -s http://localhost:3000/signalk/v1/api/vessels/self/navigation/magneticVariation/value)
   # magnetic heading
   MH=$(curl -s http://localhost:3000/signalk/v1/api/vessels/self/navigation/headingMagnetic/value)
-  # true heading
-  HT=$(echo $MH + $MV - 3.1415926535 | bc)
-  # control stellarium azimuth
-  curl -X POST -d "az=$HT" 'http://localhost:8090/api/main/view'
-  # control stellarium location  
+  if [ -n "$MH" ] && [ -n "$MV" ]; then
+    # true heading
+    HT=$(echo $MH + $MV - 3.1415926535 | bc)
+    # control stellarium azimuth
+    curl -X POST -d "az=$HT" 'http://localhost:8090/api/main/view'
+  fi
+  # control stellarium location
   POS="$(curl -s http://localhost:3000/signalk/v1/api/vessels/self/navigation/position/ | jq -M -jr '"latitude=",.value.latitude,"&longitude=",.value.longitude')"
-  curl -X POST -d "altitude=0&${POS}&name=Current" 'http://localhost:8090/api/location/setlocationfields'
+  if [ -n "$POS" ] && [ "latitude=null&longitude=null" != "$POS" ]; then
+    curl -X POST -d "altitude=0&${POS}&name=Current" 'http://localhost:8090/api/location/setlocationfields'
+  fi
   sleep 3
 done
