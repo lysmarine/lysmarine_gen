967bbb30442c65c3c335e1d6f0443a7310091599
mgrouch <mgrushinskiy@gmail.com>
2021-09-25 02:15:02 -0400

    # For bluetooth audio skipping

--- install-scripts/8-apps/04-audio-player.sh
+++ install-scripts/8-apps/04-audio-player.sh
@@ -3,4 +3,5 @@
 apt-get -y -q install audacious
 
 # For bluetooth audio skipping
-#sed -i 's/921600/460800/' /usr/bin/btuart || true
+sed -i 's/921600/460800/' /usr/bin/btuart || true
+sed -i 's/3000000/1000000/' /usr/bin/btuart || true
