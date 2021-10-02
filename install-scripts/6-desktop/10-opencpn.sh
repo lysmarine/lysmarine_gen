8cf446101a61dcf381d97bd2590d717ca9f4d7a2
mgrouch <mgrushinskiy@gmail.com>
2021-10-02 09:11:39 -0400

    fix

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -106,6 +106,8 @@ mkdir /home/user/Polars && cd /home/user/Polars
 
 wget https://www.seapilot.com/wp-content/uploads/2018/05/All_polar_files.zip
 unzip All_polar_files.zip
+chown user:user ./*
+chmod 664 ./*
 rm All_polar_files.zip
 
 cd "$BK_DIR"
