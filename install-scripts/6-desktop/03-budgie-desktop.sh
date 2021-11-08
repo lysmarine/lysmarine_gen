5d20b9b85d0264a556d0610c221840950079aaf0
mgrouch <mgrushinskiy@gmail.com>
2021-11-08 14:32:35 -0500

    buster -> bullseye

--- install-scripts/6-desktop/03-budgie-desktop.sh
+++ install-scripts/6-desktop/03-budgie-desktop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-apt-get install -y -q libatk-adaptor libgtk2.0-0 libatk1.0-0 libcairo2 libfontconfig1 libfreetype6 \
-  libgdk-pixbuf2.0-0 libglib2.0-0 libgtk2.0-0 \
+apt-get install -y -q libatk-adaptor libgtk-3-0 libatk1.0-0 libcairo2 libfontconfig1 libfreetype6 \
+  libgdk-pixbuf2.0-0 libglib2.0-0  \
   libpango-1.0-0 libpangocairo-1.0-0 libpangoft2-1.0-0 librsvg2-common libx11-6 menulibre \
-  gvfs-fuse gvfs-backends gnome-bluetooth ibus
+  gvfs-fuse gvfs-backends gnome-bluetooth ibus openbox
 
 install -o 1000 -g 1000 -d /home/user/.config/openbox
 
