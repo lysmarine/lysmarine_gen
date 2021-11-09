fda9258b7f7430918193464c0b919f6c08494bf9
mgrouch <mgrushinskiy@gmail.com>
2021-11-09 04:22:38 -0500

    buster -> bullseye

--- install-scripts/4-server/07-avnav.sh
+++ install-scripts/4-server/07-avnav.sh
@@ -6,7 +6,7 @@ apt-get -q -y --no-install-recommends install avnav xterm mpg123
 
 apt-get -q -y install avnav-history-plugin avnav-more-nmea-plugin avnav-mapproxy-plugin # avnav-raspi TODO: avnav-ocharts-plugin
 
-apt-get -q -y -o Dpkg::Options::="--force-overwrite" install avnav-oesenc
+# TODO: apt-get -q -y -o Dpkg::Options::="--force-overwrite" install avnav-oesenc
 
 install -o 0 -g 0 -d /usr/lib/systemd/system/avnav.service.d
 install -o 0 -g 0 -m 0644 $FILE_FOLDER/lys-avnav.conf /usr/lib/systemd/system/avnav.service.d/
