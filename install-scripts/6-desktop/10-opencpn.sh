631bee9a4465482da641e8bfcd7bcbdf31caa381
mgrouch <mgrushinskiy@gmail.com>
2021-11-10 11:47:17 -0500

    buster -> bullseye

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -11,7 +11,7 @@ install -o 1000 -g 1000 -d "/home/user/.opencpn"
 install -o 1000 -g 1000 -v $FILE_FOLDER/opencpn.conf "/home/user/.opencpn/"
 install -o 1000 -g 1000 -v $FILE_FOLDER/opencpn.conf "/home/user/.opencpn/opencpn.conf-bbn"
 
-apt-get install -y -q -o Dpkg::Options::="--force-overwrite" oernc-pi
+# TODO: apt-get install -y -q -o Dpkg::Options::="--force-overwrite" oernc-pi
 
 if [ $LMARCH == 'arm64' ]; then
   # See https://cloudsmith.io/~david-register/repos/ocpn-plugins-unstable/
@@ -49,8 +49,6 @@ fi
 
 if [ $LMARCH == 'armhf' ]; then
   apt-get install -y -q                                         \
-    oernc-pi                                                    \
-    oesenc-pi                                                   \
     ofc-pi                                                      \
     opencpn-doc                                                 \
     opencpn-plugin-calculator                                   \
@@ -73,6 +71,8 @@ if [ $LMARCH == 'armhf' ]; then
     opencpn-sglock-arm32
 
 # TODO:
+#    oernc-pi                                                    \
+#    oesenc-pi                                                   \
 #    opencpn-plugin-celestial                                    \
 #    opencpn-plugin-iacfleet                                     \
 #    opencpn-plugin-launcher                                     \
