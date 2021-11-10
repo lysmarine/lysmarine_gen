8a5c4b3cac62fdcdb8b7a2d1abeb243864eaec01
mgrouch <mgrushinskiy@gmail.com>
2021-11-10 11:36:21 -0500

    buster -> bullseye

--- install-scripts/4-server/12-mosquitto.sh
+++ install-scripts/4-server/12-mosquitto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-apt-get -y -q install mosquitto mosquitto-clients
-
-install -d -o mosquitto -g mosquitto "/var/log/mosquitto/"
-
-systemctl disable mosquitto
+#apt-get -y -q install mosquitto mosquitto-clients
+#
+#install -d -o mosquitto -g mosquitto "/var/log/mosquitto/"
+#
+#systemctl disable mosquitto
