6928d66576a5c69b332013311907a8cd1589868f
mgrouch <mgrushinskiy@gmail.com>
2021-11-10 17:26:16 -0500

    buster -> bullseye

--- install-scripts/8-apps/14-ads-b.sh
+++ install-scripts/8-apps/14-ads-b.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
-if [ $LMARCH != 'arm64' ]; then
-
-  apt-get -y -q install dump1090-fa piaware
-
-  sed -i 's/= 80/= 8186/' /etc/lighttpd/lighttpd.conf
-
-  systemctl disable lighttpd
-  systemctl disable dump1090-fa
-  systemctl disable piaware
-fi
+# TODO:
+#if [ $LMARCH != 'arm64' ]; then
+#
+#  apt-get -y -q install dump1090-fa piaware
+#
+#  sed -i 's/= 80/= 8186/' /etc/lighttpd/lighttpd.conf
+#
+#  systemctl disable lighttpd
+#  systemctl disable dump1090-fa
+#  systemctl disable piaware
+#fi
