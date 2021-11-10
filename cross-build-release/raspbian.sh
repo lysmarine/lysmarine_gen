df3c2e84e24f61de132f3a06271710c06458e095
mgrouch <mgrushinskiy@gmail.com>
2021-11-10 13:34:06 -0500

    buster -> bullseye

--- cross-build-release/raspbian.sh
+++ cross-build-release/raspbian.sh
@@ -57,7 +57,7 @@
   mount --rbind $myCache/stageCache $mkRoot/install-scripts/stageCache
   mount --rbind /run/shm $mkRoot/run/shm
   chroot $mkRoot /bin/bash -xe <<EOF
-    set -x; set -e; cd /install-scripts; export LMBUILD="raspios"; ls; chmod +x *.sh; ./install.sh 0 2  8 a; exit
+    set -x; set -e; cd /install-scripts; export LMBUILD="raspios"; ls; chmod +x *.sh; ./install.sh 0 2 4 8 a; exit
 EOF
 
   # Unmount
