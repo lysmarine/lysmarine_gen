a5767201fd1310c86e1a62fffab5ef9d84d64be3
mgrouch <mgrushinskiy@gmail.com>
2021-11-10 21:37:20 -0500

    buster -> bullseye

--- install-scripts/4-server/15-motioneye.sh
+++ install-scripts/4-server/15-motioneye.sh
@@ -2,24 +2,25 @@
 
 apt-get clean
 
-# See https://github.com/ccrisan/motioneye/wiki/Install-On-Raspbian
-
-apt-get -y -q install libcurl4-openssl-dev libssl-dev ffmpeg libmariadb3 libpq5 libmicrohttpd12 motion \
- python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
-
-pip install --upgrade motioneye
-
-mkdir -p /etc/motioneye
-cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
-
-mkdir -p /var/lib/motioneye
-
-cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
-#systemctl daemon-reload
-systemctl enable motioneye
-#systemctl start motioneye
-
-apt-get clean
+# TODO:
+## See https://github.com/ccrisan/motioneye/wiki/Install-On-Raspbian
+#
+#apt-get -y -q install libcurl4-openssl-dev libssl-dev ffmpeg libmariadb3 libpq5 libmicrohttpd12 motion \
+# python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev
+#
+#pip install --upgrade motioneye
+#
+#mkdir -p /etc/motioneye
+#cp /usr/local/share/motioneye/extra/motioneye.conf.sample /etc/motioneye/motioneye.conf
+#
+#mkdir -p /var/lib/motioneye
+#
+#cp /usr/local/share/motioneye/extra/motioneye.systemd-unit-local /etc/systemd/system/motioneye.service
+##systemctl daemon-reload
+#systemctl enable motioneye
+##systemctl start motioneye
+#
+#apt-get clean
 
 # http://localhost:8765/
 # default user admin
