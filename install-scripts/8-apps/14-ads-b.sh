532ba758d7d105e82d355921d17898c3a5260b08
mgrouch <mgrushinskiy@gmail.com>
2021-11-13 14:16:01 -0500

    Revert "buster -> bullseye"
    This reverts commit 6928d665

--- install-scripts/8-apps/14-ads-b.sh
+++ install-scripts/8-apps/14-ads-b.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
 
-# TODO:
-#if [ $LMARCH != 'arm64' ]; then
-#
-#  apt-get -y -q install dump1090-fa piaware
-#
-#  sed -i 's/= 80/= 8186/' /etc/lighttpd/lighttpd.conf
-#
-#  systemctl disable lighttpd
-#  systemctl disable dump1090-fa
-#  systemctl disable piaware
-#fi
+if [ $LMARCH != 'arm64' ]; then
+
+  apt-get -y -q install dump1090-fa piaware
+
+  sed -i 's/= 80/= 8186/' /etc/lighttpd/lighttpd.conf
+
+  systemctl disable lighttpd
+  systemctl disable dump1090-fa
+  systemctl disable piaware
+fi
