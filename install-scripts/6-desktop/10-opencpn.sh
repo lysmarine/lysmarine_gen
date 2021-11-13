269c7f2be5400bff450b30a3f779eb766ce6e87f
mgrouch <mgrushinskiy@gmail.com>
2021-11-13 14:19:47 -0500

    Revert "Update 10-opencpn.sh"
    This reverts commit cf1ec5fe

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -53,24 +53,25 @@ if [ $LMARCH == 'armhf' ]; then
     ofc-pi                                                      \
     opencpn-doc                                                 \
     opencpn-plugin-calculator                                   \
+    opencpn-plugin-celestial                                    \
     opencpn-plugin-chartscale                                   \
     opencpn-plugin-climatology                                  \
     opencpn-plugin-climatology-data                             \
-                                        \
-                                        \
+    opencpn-plugin-iacfleet                                     \
+    opencpn-plugin-launcher                                     \
     opencpn-plugin-logbookkonni                                 \
     opencpn-plugin-nmeaconverter                                \
-                                       \
+    opencpn-plugin-objsearch                                    \
     opencpn-plugin-ocpndebugger                                 \
     opencpn-plugin-plots                                        \
     opencpn-plugin-polar                                        \
-                                         \
+    opencpn-plugin-pypilot                                      \
     opencpn-plugin-radar                                        \
     opencpn-plugin-s63                                          \
     opencpn-plugin-sar                                          \
     opencpn-plugin-shipdriver                                   \
     opencpn-plugin-tactics                                      \
-                                          \
+    opencpn-plugin-vfkaps                                       \
     opencpn-plugin-watchdog                                     \
     opencpn-plugin-weatherfax                                   \
     opencpn-plugin-weatherrouting                               \
