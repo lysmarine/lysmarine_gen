0be39cc79a150f5633bb2394dfe2bba7a4086932
mgrouch <mgrushinskiy@gmail.com>
2021-11-13 14:16:24 -0500

    Revert "buster -> bullseye"
    This reverts commit 0909ced4

--- install-scripts/4-server/13-influxdb.sh
+++ install-scripts/4-server/13-influxdb.sh
@@ -2,10 +2,10 @@
 
 apt-get clean
 
-apt-get -y -q install influxdb chronograf # TODO: kapacitor
+apt-get -y -q install influxdb chronograf kapacitor
 
 systemctl unmask influxdb
 systemctl disable influxdb
 
 systemctl disable chronograf
-# TODO: systemctl disable kapacitor
+systemctl disable kapacitor
