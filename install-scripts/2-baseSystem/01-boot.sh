de61dbeaef9ed29222d464b4101db4fa8090c45f
mgrouch <mgrushinskiy@gmail.com>
2021-11-14 00:48:13 -0500

    remove duped arm_64bit=1 on arm64

--- install-scripts/2-baseSystem/01-boot.sh
+++ install-scripts/2-baseSystem/01-boot.sh
@@ -7,6 +7,9 @@ install -v -m0644 $FILE_FOLDER/skip-prompt.conf "/etc/systemd/system/getty@tty1.
 
 ## RaspOS
 if [ -f /boot/config.txt ]; then
+  if [ $LMARCH == 'armhf' ]; then
+    echo "arm_64bit=1" >> /boot/config.txt
+  fi
 	cat $FILE_FOLDER/appendToConfig.txt >> /boot/config.txt
 	sed -i 's/kms-v3d$/kms-v3d,cma-256/' /boot/config.txt
 fi
