4ba96b6fd55d3b3d624b972da3215689c84004dd
mgrouch <mgrushinskiy@gmail.com>
2021-12-01 11:14:33 -0500

    fix import

--- install-scripts/4-server/02-pypilot.sh
+++ install-scripts/4-server/02-pypilot.sh
@@ -81,6 +81,8 @@ sed -i 's/_http._tcp.local./_signalk-http._tcp.local./' "$(find /usr/local/lib -
 sed -i 's/ttyAMA0/serial1/' "$(find /usr/local/lib -name serialprobe.py)" || true
 sed -i "s/'ttyAMA'//" "$(find /usr/local/lib -name serialprobe.py)" || true
 
+cp $FILE_FOLDER/wind.py "$(find /usr/local/lib -name wind.py)" || true
+
 systemctl disable pypilot_boatimu.service
 systemctl enable pypilot_hat.service
 systemctl enable pypilot@pypilot.service                               # listens on tcp 20220 and 23322
