6a04cc7fcfce87cadffa3aa81d86b70ff7766961
mgrouch <mgrushinskiy@gmail.com>
2021-12-30 15:41:58 -0500

    fix

--- .circleci/build-ci.sh
+++ .circleci/build-ci.sh
@@ -25,7 +25,7 @@ fi
 WORK_DIR=$(pwd):/ci-source
 
 docker run --privileged --cap-add=ALL --security-opt="seccomp=unconfined" -d -ti -e "container=docker" -v $WORK_DIR:rw $DOCKER_IMAGE /bin/bash
-DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}')
+DOCKER_CONTAINER_ID=$(docker ps --last 4 | grep $CONTAINER_DISTRO | awk '{print $1}' | head -1)
 
 docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get update
 docker exec --privileged -ti $DOCKER_CONTAINER_ID apt-get -y install dpkg-dev debhelper devscripts equivs pkg-config apt-utils fakeroot \
