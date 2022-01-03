900c26f4bbb632dd2ab0c557956750d2d2f3b457
mgrouch <mgrushinskiy@gmail.com>
2022-01-03 11:34:08 -0500

    fix

--- install-scripts/8-apps/01-stellarium.sh
+++ install-scripts/8-apps/01-stellarium.sh
@@ -2,7 +2,20 @@
 
 apt-get clean
 
-apt-get -y -q install geographiclib-tools
+apt-get -y -q install geographiclib-tools \
+   libqt5concurrent5 \
+   libqt5core5a \
+   libqt5gui5 \
+   libqt5svg5 \
+   libqt5opengl5 \
+   libqt5multimedia5 \
+   libqt5multimediawidgets5 \
+   libqt5network5 \
+   libqt5positioning5 \
+   libqt5printsupport5 \
+   libqt5script5 \
+   libqt5serialport5 \
+   libqt5widgets5
 
 wget https://launchpad.net/~stellarium/+archive/ubuntu/stellarium-releases/+files/stellarium_0.21.2-upstream1~ubuntu18.04.1_${LMARCH}.deb
 wget https://launchpad.net/~stellarium/+archive/ubuntu/stellarium-releases/+files/stellarium-data_0.21.2-upstream1~ubuntu18.04.1_all.deb
