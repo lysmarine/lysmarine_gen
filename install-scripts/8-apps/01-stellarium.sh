3051e627e59bd7c9448a4436121f0a12b28df186
mgrouch <mgrushinskiy@gmail.com>
2022-01-02 23:06:19 -0500

    fix

--- install-scripts/8-apps/01-stellarium.sh
+++ install-scripts/8-apps/01-stellarium.sh
@@ -2,7 +2,7 @@
 
 apt-get clean
 
-apt-get -y -q install stellarium geographiclib-tools
+apt-get -y -q install stellarium libqt5webengine5 geographiclib-tools
 
 install -d -o 1000 -g 1000 -m 0755 "/home/user/.stellarium"
 install -v -o 1000 -g 1000 -m 0644 $FILE_FOLDER/stellarium-config.ini "/home/user/.stellarium/config.ini"
