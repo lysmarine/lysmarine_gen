aeaa6380735435d36dd9e15dd6919311dd0b6d6f
mgrouch <mgrushinskiy@gmail.com>
2022-01-04 16:36:48 -0500

    not needed for bt

--- install-scripts/8-apps/04-audio-player.sh
+++ install-scripts/8-apps/04-audio-player.sh
@@ -1,7 +1,3 @@
 #!/bin/bash -e
 
 apt-get -y -q install audacious
-
-# For bluetooth audio skipping
-sed -i 's/921600/460800/' /usr/bin/btuart || true
-sed -i 's/3000000/1000000/' /usr/bin/btuart || true
