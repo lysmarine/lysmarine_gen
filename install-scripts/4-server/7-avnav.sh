a3cb636d27a3b503de99710bf7c46e22e4686d9e
Frederic Guilbault <2@0464.ca>
2022-11-08 17:08:47 -0500

    Fix avnav

--- install-scripts/4-server/7-avnav.sh
+++ install-scripts/4-server/7-avnav.sh
@@ -1,12 +1,8 @@
 #!/bin/bash -e
-
 apt-get -q -y --no-install-recommends install avnav # mpg123
-
-apt-get -q -y install avnav-history-plugin avnav-ocharts-plugin
-
-apt-get -q -y -o Dpkg::Options::="--force-overwrite" install avnav-oesenc
-
-apt-get -q -y install avnav-mapproxy-plugin
+apt-get -q -y install avnav-history-plugin || true  # avnav-ocharts-plugin
+apt-get -q -y -o Dpkg::Options::="--force-overwrite" install avnav-oesenc || true
+apt-get -q -y install avnav-mapproxy-plugin || true
 
 install -o 0 -g 0 -d /usr/lib/systemd/system/avnav.service.d
 install -o 0 -g 0 -m 0644 $FILE_FOLDER/lys-avnav.conf /usr/lib/systemd/system/avnav.service.d/
