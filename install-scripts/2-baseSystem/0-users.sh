ef5aedbf4c6da55630700c7a3f0a710491762f2a
Frederic Guilbault <2@0464.ca>
2022-11-08 15:43:52 -0500

    change default password

--- install-scripts/2-baseSystem/0-users.sh
+++ install-scripts/2-baseSystem/0-users.sh
@@ -5,7 +5,7 @@ apt-get -y -q install sudo policykit-1
 sed -i "s/XKBLAYOUT=.*/XKBLAYOUT=\"us\"/g" /etc/default/keyboard
 
 ## Set root password.
-echo 'root:changeme' | chpasswd
+echo 'root:9000' | chpasswd
 
 ## Remove default user (if any).
 oldUser=$(cat /etc/passwd | grep 1000:1000 | cut -f1 -d:) 
@@ -18,9 +18,12 @@ fi
 
 ## Add default user.
 adduser --uid 1000 --home /home/user --quiet --disabled-password -gecos "lysmarine" user
-echo 'user:changeme' | chpasswd
+echo 'user:9000' | chpasswd
 echo "user ALL=(ALL:ALL) ALL" >> /etc/sudoers
 usermod -a -G netdev,tty,sudo,video,input,plugdev user
+echo "export XDG_RUNTIME_DIR=/run/user/1000" >> /home/user/.bashrc
+install -d -o 1000 -g 1000 "/home/user/.local"
+install -d -o 1000 -g 1000 "/home/user/.local/share"
 
 ## Create signalk user to run the server.
 if [ ! -d /home/signalk ] ; then
@@ -47,10 +50,8 @@ if ! grep -q charts /etc/group ; then
 	usermod -a -G charts root;
 fi
 
-## Create the special charts folder.
-install -v -d -m 6775 -o signalk -g charts /srv/charts; 
-
-## Link the chart folder to home for convenience.
+## Create and Link the special charts folder.
+install -v -d -m 6775 -o signalk -g charts /srv/charts;
 if [ ! -f /home/user/charts ] ; then
 	su user -c "ln -s /srv/charts /home/user/charts;"
 fi
@@ -60,7 +61,6 @@ if [[ -d /etc/polkit-1 ]]; then
 	install -v $FILE_FOLDER/all_all_users_to_shutdown_reboot.pkla "/etc/polkit-1/localauthority/50-local.d/"
 	install -v $FILE_FOLDER/mount.pkla "/etc/polkit-1/localauthority/50-local.d/"
 	install -d "/etc/polkit-1/localauthority/10-vendor.d"
-	install -v $FILE_FOLDER/org.freedesktop.NetworkManager.pkla  "/etc/polkit-1/localauthority/10-vendor.d/"
 fi
 
 ## Remove the raspbian no-pwd sudo to user pi.
