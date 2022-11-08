6097c4e0d8de06a05c48a7de4c65b14c8c8f34ba
Frederic Guilbault <2@0464.ca>
2022-11-08 15:45:20 -0500

    it's not needed to skip prompt since simplelogin implementation

--- install-scripts/2-baseSystem/1-boot.sh
+++ install-scripts/2-baseSystem/1-boot.sh
@@ -1,26 +1,22 @@
 #!/bin/bash -e
 apt-get install -yq plymouth;
 
-## This override the default tty1 behaviour to make it more discrete during the boot process
-install -v -d "/etc/systemd/system/getty@tty1.service.d"
-install -v -m0644 $FILE_FOLDER/skip-prompt.conf "/etc/systemd/system/getty@tty1.service.d/"
-
-## RaspiOS
+## RaspiOS boot config
 if [ -f /boot/config.txt  ] ;then
 	cat $FILE_FOLDER/appendToConfig.txt >> /boot/config.txt
 fi
 
-## RaspiOS
+## RaspiOS boot config
 if [ -f /boot/cmdline.txt ] ; then
 	sed -i '$s/$/\ loglevel=1\ splash\ quiet\ logo.nologo\ plymouth.ignore-serial-consoles\ console=tty3\ rd.systemd.show_status=false/' /boot/cmdline.txt
 fi
 
-## Armbian
+## Armbian boot config
 if [ -f /boot/armbianEnv.txt ] ; then
 	echo "console=serial" >> /boot/armbianEnv.txt
 fi
 
-## Debian
+## Debian grub settings
 if [ -f /etc/default/grub ] ; then
   install -m0644 -v $FILE_FOLDER/grub "/etc/default/grub"
   install -m0644 -v $FILE_FOLDER/background.png "/boot/grub/background.png"
@@ -34,9 +30,9 @@ install -v $FILE_FOLDER/ascii_logo.txt "/etc/motd"
 cp -r $FILE_FOLDER/dreams "/usr/share/plymouth/themes/"
 plymouth-set-default-theme dreams
 
-## Armbian
+## Armbian specific
 rm /etc/issue || true
 rm /etc/issue.net || true
 
-## Raspbian enable this to intercept keystroke during the boot process, (for ondemand cup freq management.) Lysmarine don't want to set it that way.
+## Raspios: disable on boot management options for ondemand cup freq management. Lysmarine don't want to set it that way.
 systemctl disable triggerhappy.service || /bin/true
