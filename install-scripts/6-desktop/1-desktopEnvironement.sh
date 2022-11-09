5d31dec2e35a9a0f39eaeb95656ca407088d8917
Frederic Guilbault <2@0464.ca>
2022-11-09 14:26:10 -0500

    Install desktop environement

---
+++ install-scripts/6-desktop/1-desktopEnvironement.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+## Base desktop Env
+apt-get -yq	--no-install-recommends install plasma-phone-components plasma-phone-settings
+apt-get -yq install	plasma-nm kde-config-mobile-networking plasma-settings
