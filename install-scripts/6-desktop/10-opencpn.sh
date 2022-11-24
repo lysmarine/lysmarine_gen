40a22d2c9de326f41973750947e4107e601d95c6
Frederic Guilbault <2@0464.ca>
2022-11-23 19:26:23 -0500

    opencpn wondow placement rules for KDE

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -1,5 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
+
 apt-get install -y -q opencpn
 
 install -o 1000 -g 1000 -d "/home/user/.opencpn"
 install -o 1000 -g 1000 -v $FILE_FOLDER/opencpn.conf "/home/user/.opencpn/"
+
+
+## Add specifics window placement rules for opencpn in plasma-mobile.
+install -o 1000 -g 1000 -d -m 755 /home/user/.config
+install -v -o 1000 -g 1000 -m 600 $FILE_FOLDER/kwinrulesrc "/home/user/.config/"
