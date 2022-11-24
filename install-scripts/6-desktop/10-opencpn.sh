58b96df4bde8fdd2c51f246a9488747b6ca3a35a
Frederic Guilbault <2@0464.ca>
2022-11-23 19:27:19 -0500

    trivial cleanup

--- install-scripts/6-desktop/10-opencpn.sh
+++ install-scripts/6-desktop/10-opencpn.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
+#!/bin/bash -e
 apt-get install -y -q opencpn
 
 install -o 1000 -g 1000 -d "/home/user/.opencpn"
 install -o 1000 -g 1000 -v $FILE_FOLDER/opencpn.conf "/home/user/.opencpn/"
-
 
 ## Add specifics window placement rules for opencpn in plasma-mobile.
 install -o 1000 -g 1000 -d -m 755 /home/user/.config
