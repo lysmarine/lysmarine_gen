d119418451e105ec2eb74b9b82f3d2136bd481dd
Frederic Guilbault <2@0464.ca>
2022-11-25 21:15:29 -0500

    dummy the krunner plasmoid

--- install-scripts/6-desktop/3-desktopTheming.sh
+++ install-scripts/6-desktop/3-desktopTheming.sh
@@ -13,3 +13,9 @@ install -v -o 1000 -g 1000 -m 600 $FILE_FOLDER/powermanagementprofilesrc "/home/
 #sed -i 's/"first_run_tabs":["*"]/"first_run_tabs":["https://lysmarine.org"]/' /etc/chromium/master_preferences
 # "first_run_tabs":["https://welcome.raspberrypi.org/raspberry-pi-os?id=UNIDENTIFIED"]
 # rm /etc/chromium/master_preferences
+
+## Copy and dummy the krunner plasmoid to disable it without breaking the debian package.
+install -o 1000 -g 1000 -d -m 0755 /home/user/.local/share/plasma/plasmoids/org.kde.phone.krunner/
+cp -r /usr/share/plasma/plasmoids/org.kde.phone.krunner /home/user/.local/share/plasma/plasmoids/
+chown -R user:user /home/user/.local/share/plasma/plasmoids/org.kde.phone.krunner
+install -v -o 1000 -g 1000 -m 600 $FILE_FOLDER/main.qml "/home/user/.local/share/plasma/plasmoids/org.kde.phone.krunner/contents/ui/"
---
+++ install-scripts/6-desktop/files/main.qml
@@ -0,0 +1,7 @@
+import QtQuick 2.1
+import org.kde.plasma.core 2.0 as PlasmaCore
+import org.kde.plasma.plasmoid 2.0
+
+Item {
+    Plasmoid.backgroundHints: PlasmaCore.Types.ShadowBackground | PlasmaCore.Types.ConfigurableBackground
+}
