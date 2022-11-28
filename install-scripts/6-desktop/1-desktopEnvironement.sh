bf2fdefbb615c4f2102a7677685bb38f5ae55e48
Frederic Guilbault <2@0464.ca>
2022-11-28 15:58:53 -0500

    Remove firmware package

--- install-scripts/6-desktop/1-desktopEnvironement.sh
+++ install-scripts/6-desktop/1-desktopEnvironement.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 ## Base desktop Env
-apt-get -yq	--no-install-recommends install plasma-phone-components plasma-phone-settings
+apt-get -yq	--no-install-recommends install plasma-phone-components plasma-phone-settings firmware-ti-connectivity-
 apt-get -yq install	plasma-nm kde-config-mobile-networking plasma-settings kdeconnect-
