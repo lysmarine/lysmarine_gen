e77970d7fa0c32f7d5c79b45f33e5fe84a2747f2
Frederic Guilbault <2@0464.ca>
2022-11-30 11:48:42 -0500

    Add Avnav webapp

--- install-scripts/6-desktop/6-nativfierApps.sh
+++ install-scripts/6-desktop/6-nativfierApps.sh
@@ -3,12 +3,14 @@
 ## Install dependencies
 apt-get -yq install gir1.2-webkit2-4.0
 pip3 install pywebview
+pip3 install PyQtWebEngine
+pip3 install qtpy
 
 ## Copy files
 # install -v -m 0755 $FILE_FOLDER/Freeboard-sk.py "/usr/local/bin/Freeboard-sk"
 install -v -m 0755 $FILE_FOLDER/Pypilot_webapp.py "/usr/local/bin/Pypilot_webapp"
-install -v -m 0755 $FILE_FOLDER/SignalK.py "/usr/local/bin/SignalK"
-install -v -m 0755 $FILE_FOLDER/SpeedSample.py "/usr/local/bin/SpeedSample"
+install -v -m 0755 $FILE_FOLDER/signalk_webapp.py "/usr/local/bin/signalk_webapp"
+install -v -m 0755 $FILE_FOLDER/avnav_webapp.py "/usr/local/bin/avnav_webapp"
 install -v -m 0755 $FILE_FOLDER/wdash.py "/usr/local/bin/wdash"
 
 ## todo for icon support
@@ -21,3 +23,4 @@ install -v -o 1000 -g 1000 -m 644 $FILE_FOLDER/freeboard-sk.png "/home/user/.loc
 install -v -o 1000 -g 1000 -m 644 $FILE_FOLDER/signalk.png "/home/user/.local/share/icons/"
 install -d /usr/local/share/applications
 install -v $FILE_FOLDER/signalk.desktop "/usr/local/share/applications/"
+install -v $FILE_FOLDER/avnav_webapp.desktop "/usr/local/share/applications/"
