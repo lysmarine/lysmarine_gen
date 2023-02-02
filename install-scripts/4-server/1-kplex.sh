33541cb743d9ad1b7cdc02548f5837c822350cca
Frederic Guilbault <2@0464.ca>
2023-02-02 17:48:47 -0500

    Bump gcc version

--- install-scripts/4-server/1-kplex.sh
+++ install-scripts/4-server/1-kplex.sh
@@ -3,7 +3,7 @@ apt-get install -y -q libc6
 
 ## Detect the right package to download.
 if [[ $LMARCH == 'arm64' ]]; then
-	apt-get install -yq gcc-8-base:armhf libc6:armhf libgcc1:armhf
+	apt-get install -yq gcc-10-base:armhf libc6:armhf libgcc1:armhf
 	package='kplex_1.4-1_armhf.deb'
 else
 	package="kplex_1.4-1_${LMARCH}.deb"
