7e8e0844fffee7e6a176c471cd3b44f35b7ae4d7
Frederic Guilbault <2@0464.ca>
2023-02-05 23:01:56 -0500

    Fix webapp and remove titlebar

--- install-scripts/6-desktop/6-nativfierApps.sh
+++ install-scripts/6-desktop/6-nativfierApps.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
 ## Install dependencies
-apt-get -yq install gir1.2-webkit2-4.0
-pip3 install pywebview
-pip3 install PyQtWebEngine
-pip3 install qtpy
+apt-get -yq install gir1.2-webkit2-4.0 qt5-qmake pyqt5-dev
+
+apt-get -yq install python3-pyqt5.qtwebengine
+
+# p   python3-pyqt5.qtwebengine                                                                                      - Python 3 bindings for Qt5's WebEngine module                                                                             
+# p   python3-pyside2.qtwebengine                                                                                    - Python bindings for Qt5 Web Engine (Python 3)                                                                            
+# p   python3-pyside2.qtwebenginecore                                                                                - Python bindings for Qt5 WebEngineCore module (Python 3)                                                                  
+# p   python3-pyside2.qtwebenginewidgets                                                                             - Python bindings for Qt5 WebEngineWidgets module (Python 3) 
+#  /QtWebEngineCoremod.sip: line 25: column 9: 'QtCore/QtCoremod.sip' could not be found
+# /QtWebEngineCoremod.sip: line 26: column 9: 'QtGui/QtGuimod.sip' could not be found
+#  /QtWebEngineCoremod.sip: line 27: column 9: 'QtNetwork/QtNetworkmod.sip' could not be found
+
+
+pip3 install --upgrade pip
+
+pip3 install setuptools
+pip install pywebview
+pip install qtpy
+
+# pip3 install pyqt5-dev 
+#pip3 install pyqt5-sip
+#pip3 install pyqt5 
+
+# pip3 install pyqt5-sip-dev
+# pip3 install pywebview PyQtWebEngine 
 
 ## Copy files
 # install -v -m 0755 $FILE_FOLDER/Freeboard-sk.py "/usr/local/bin/Freeboard-sk"
@@ -24,3 +45,11 @@ install -v -o 1000 -g 1000 -m 644 $FILE_FOLDER/signalk.png "/home/user/.local/sh
 install -d /usr/local/share/applications
 install -v $FILE_FOLDER/signalk.desktop "/usr/local/share/applications/"
 install -v $FILE_FOLDER/avnav_webapp.desktop "/usr/local/share/applications/"
+
+install -d "/usr/local/share/icons/"
+install -m 644  $FILE_FOLDER/signalk.png "/usr/local/share/icons/"
+
+install -d /usr/local/share/applications
+install -v $FILE_FOLDER/pypilot_calibration.desktop "/usr/local/share/applications/"
+install -v $FILE_FOLDER/pypilot_webapp.desktop "/usr/local/share/applications/" # NOTE Depend on stage 6.6
+
